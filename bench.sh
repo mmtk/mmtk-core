d412262399b1f2ae3d2b77608320d32607b6f1ba
Zixian Cai <u5937495@anu.edu.au>
2017-11-24 09:13:53 +1100

    Have a separate bench folder

--- bench.sh
+++ bench.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Using malloc"
-clang -lmmtk -Ltarget/release -o bench -Iapi ./api/bench.c
+clang -lmmtk -Ltarget/release -o bench -Iapi ./bench/bench.c
 export LD_LIBRARY_PAT=target/release
 time ./bench
 
 echo "Using bump point allocator"
-clang -lmmtk -Ltarget/release -o bench -D TEST -Iapi ./api/bench.c
+clang -lmmtk -Ltarget/release -o bench -D TEST -Iapi ./bench/bench.c
 export LD_LIBRARY_PAT=target/release
 time ./bench
