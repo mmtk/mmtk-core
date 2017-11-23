019c8cd449b9167eb94c4b788c61481fa1f6d630
Zixian Cai <u5937495@anu.edu.au>
2017-11-23 17:25:28 +1100

    Allocate various sizes

--- api/bench.c
+++ api/bench.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include "mmtk.h"
 
-//#define TEST
-
 #ifdef TEST
     #define ALLOC(x) alloc(x, 1, 0)
-    #define INIT gc_init(1024*1024*1024)
+    #define INIT gc_init(1700*1024*1024)
 #else
     #define ALLOC(x) malloc(x)
     #define INIT __asm__("nop")
@@ -15,7 +13,19 @@
 int main() {
     volatile void * tmp;
     INIT;
-    for (int i=0; i<1024*1024*100; i++) {
+    for (int i=0; i<1024*1024*25; i++) {
+        tmp = ALLOC(16);
+        if (!tmp) {
+            puts("Ran out of heap space :(\n");
+        }
+        tmp = ALLOC(8);
+        if (!tmp) {
+            puts("Ran out of heap space :(\n");
+        }
+        tmp = ALLOC(32);
+        if (!tmp) {
+            puts("Ran out of heap space :(\n");
+        }
         tmp = ALLOC(8);
         if (!tmp) {
             puts("Ran out of heap space :(\n");
--- bench.sh
+++ bench.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Using malloc"
 clang -lmmtk -Ltarget/release -o bench -Iapi ./api/bench.c
+export LD_LIBRARY_PAT=target/release
+time ./bench
+
+echo "Using bump point allocator"
+clang -lmmtk -Ltarget/release -o bench -D TEST -Iapi ./api/bench.c
 export LD_LIBRARY_PAT=target/release
 time ./bench
