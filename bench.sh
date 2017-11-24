f84b6bd34738a63bd2b1549152b63ee5da9b8a2e
Pavel Zakopaylo <zakopaylo2@gmail.com>
2017-11-24 12:57:02 +1100

    Fixed benchmarking script to always use -O3

--- bench.sh
+++ bench.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Using malloc"
-clang -lmmtk -Ltarget/release -o bench-exe -Iapi ./bench/bench.c
+clang -O3 -lmmtk -Ltarget/release -o bench-exe -Iapi ./bench/bench.c
 export LD_LIBRARY_PATH=target/release
 time ./bench-exe
 
 echo "Using Rust bump pointer allocator"
-clang -lmmtk -Ltarget/release -o bench-exe -D TEST -Iapi ./bench/bench.c
+clang -O3 -lmmtk -Ltarget/release -o bench-exe -D TEST -Iapi ./bench/bench.c
 export LD_LIBRARY_PATH=target/release
 time ./bench-exe
 
 echo "Using C bump pointer allocator"
-clang -shared -fPIC -o bench/libmmtk.so bench/bump_allocator.c
-clang -lmmtk -Lbench -o bench-exe -D TEST -Iapi ./bench/bench.c
+clang -O3 -shared -fPIC -o bench/libmmtk.so bench/bump_allocator.c
+clang -O3 -lmmtk -Lbench -o bench-exe -D TEST -Iapi ./bench/bench.c
 export LD_LIBRARY_PATH=bench
 time ./bench-exe
