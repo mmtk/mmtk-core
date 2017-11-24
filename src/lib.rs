adc08eeab8157f729edd6eb6362586f3f812fa70
Pavel Zakopaylo <zakopaylo2@gmail.com>
2017-11-24 12:44:26 +1100

    Rewrote allocation to not use unwraps in fast path

--- src/lib.rs
+++ src/lib.rs
@@ -27,40 +27,53 @@ pub struct Space {
     heap_start: Address,
     heap_cursor: Address,
     heap_end: Address,
-    address_range: MmapMut,
+    address_range: Option<MmapMut>, // we do not access this in fast path
 }
 
 lazy_static! {
-    static ref IMMORTAL_SPACE: NotThreadSafe<Option<Space>> = NotThreadSafe { value: UnsafeCell::new(None) };
+    static ref IMMORTAL_SPACE: NotThreadSafe<Space> = NotThreadSafe {value: UnsafeCell::new(Space::new()) };
 }
 
 impl Space {
-    pub unsafe fn new(heap_size: usize) -> Self {
+    pub fn new() -> Self {
+        unsafe {
+            Space {
+                heap_start: Address::zero(),
+                heap_cursor: Address::zero(),
+                heap_end: Address::zero(),
+                address_range: None,
+            }
+        }
+    }
+
+    pub fn init(&mut self, heap_size: usize) {
         let address_range = MmapMut::map_anon(heap_size + SPACE_ALIGN).
             expect("Unable to allocate memory");
 
-        let heap_start: Address = Address::from_ptr::<u8>(address_range.as_ptr())
+        self.heap_start = Address::from_ptr::<u8>(address_range.as_ptr())
             .align_up(SPACE_ALIGN);
 
-        Space {
-            heap_start: heap_start,
-            heap_cursor: heap_start,
-            heap_end: heap_start + heap_size,
-            address_range: address_range,
-        }
+        self.heap_cursor = self.heap_start;
+        self.heap_end = self.heap_start + heap_size;
+
+        self.address_range = Some(address_range);
     }
 }
 
 #[no_mangle]
 pub extern fn gc_init(heap_size: usize) {
-    unsafe { *IMMORTAL_SPACE.value.get() = Some(Space::new(heap_size)) };
+    unsafe {
+        (*IMMORTAL_SPACE.value.get()).init(heap_size);
+    }
 }
 
 #[inline(always)]
 fn align_allocation(region: Address, align: usize, offset: usize) -> Address {
     let region_isize = region.as_usize() as isize;
+    let offset_isize = offset as isize;
+
     let mask = (align - 1) as isize; // fromIntSignExtend
-    let neg_off = -region_isize; // fromIntSignExtend
+    let neg_off = -offset_isize; // fromIntSignExtend
     let delta = (neg_off - region_isize) & mask;
 
     region + delta
@@ -68,13 +81,13 @@ fn align_allocation(region: Address, align: usize, offset: usize) -> Address {
 
 #[no_mangle]
 pub extern fn alloc(size: usize, align: usize, offset: usize) -> ObjectReference {
-    let space: &mut Option<Space> = unsafe { &mut *IMMORTAL_SPACE.get() };
-    let old_cursor = space.as_ref().unwrap().heap_cursor;
+    let space: &mut Space = unsafe { &mut *IMMORTAL_SPACE.get() };
+    let old_cursor = space.heap_cursor;
     let new_cursor = align_allocation(old_cursor + size, align, offset);
-    if new_cursor > space.as_ref().unwrap().heap_end {
+    if new_cursor > space.heap_end {
         unsafe { Address::zero().to_object_reference() }
     } else {
-        space.as_mut().unwrap().heap_cursor = new_cursor;
+        space.heap_cursor = new_cursor;
         unsafe { old_cursor.to_object_reference() }
     }
 }
