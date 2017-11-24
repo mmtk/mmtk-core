d50e08e7d31e4ab58235307cd66c92d5dd33e853
Zixian Cai <u5937495@anu.edu.au>
2017-11-24 17:07:30 +1100

    Benchmarking allocation only and optionally with storing

--- bench/bench.c
+++ bench/bench.c
@@ -3,42 +3,13 @@
 #include <stdlib.h>
 #include "mmtk.h"
 
-/*
-// Use mach_absolute_time on macOS
-typedef uint64_t timestamp_t;
-#include <mach/mach_time.h>
-
-static inline void get_timestamp(timestamp_t* tstamp) {
-    *tstamp = mach_absolute_time();
-}
-static double get_elapsed_time(timestamp_t* t0, timestamp_t* t1) {
-    mach_timebase_info_data_t tb;
-    timestamp_t elapsed;
-    uint64_t elapsed_nano;
-
-    mach_timebase_info(&tb);
-    elapsed = *t1 - *t0;
-    elapsed_nano = elapsed * tb.numer / tb.denom;
-    return ((double)elapsed_nano) * 1e-9;
-}*/
-
-#ifdef TEST
-    #define ALLOC(x) alloc(x, 1, 0)
-    #define INIT gc_init(1024*1024*1024)
-#else
-    #define ALLOC(x) malloc(x)
-    #define INIT __asm__("nop")
-#endif
-
 int main() {
     volatile uint64_t * tmp;
-    INIT;
-    timestamp_t t0, t1;
-    get_timestamp(&t0);
+    gc_init(1024*1024*1024);
     for (int i=0; i<1024*1024*100; i++) {
-        tmp = ALLOC(8);
-        *tmp = 42;
+        tmp = alloc(8, 1, 0);
+        #ifdef STORE
+            *tmp = 42;
+        #endif
     }
-    get_timestamp(&t1);
-    printf("%lf\n", get_elapsed_time(&t0, &t1));
 }
