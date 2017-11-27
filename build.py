8e80bc04d7e2df54004a5b70712641ff0fa18f62
Zixian Cai <u5937495@anu.edu.au>
2017-11-28 10:48:52 +1100

    Update build script

---
+++ build.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import platform
+import subprocess
+import shutil
+import os
+
+
+def exec_and_redirect(cmd, env=None):
+    p = subprocess.Popen(cmd,
+                         stdout=subprocess.PIPE,
+                         bufsize=1,  # line buffered
+                         env=env)
+    for line in p.stdout:
+        print line.strip()
+
+
+system = platform.system()
+assert system == "Darwin" or system == "Linux"
+
+SUFFIX = ".so"
+if system == "Darwin":
+    SUFFIX = ".dylib"
+elif system == "Linux":
+    SUFFIX = ".so"
+
+LIBRARY_PATH = "LD_LIBRARY_PATH"
+if system == "Darwin":
+    LIBRARY_PATH = "DYLD_LIBRARY_PATH"
+elif system == "Linux":
+    LIBRARY_PATH = "LD_LIBRARY_PATH"
+
+exec_and_redirect(["cargo", "build"])
+exec_and_redirect(["cargo", "build", "--release"])
+shutil.copyfile("target/release/libmmtk{}".format(SUFFIX),
+                "./libmmtk{}".format(SUFFIX))
+
+exec_and_redirect([
+    "clang",
+    "-shared",
+    "-fPIC",
+    "-o", "libmmtkc{}".format(SUFFIX),
+    "-O3",
+    "bench/bump_allocator.c"])
+exec_and_redirect([
+    "clang",
+    "-lmmtk",
+    "-L.",
+    "-Iapi",
+    "-O3",
+    "-o",
+    "test_mmtk",
+    "./api/main.c"])
+exec_and_redirect([
+    "clang",
+    "-lmmtkc",
+    "-L.",
+    "-Iapi",
+    "-O3",
+    "-o",
+    "test_mmtkc",
+    "./api/main.c"])
+exec_and_redirect(["./test_mmtk"], env={LIBRARY_PATH: "."})
+exec_and_redirect(["./test_mmtkc"], env={LIBRARY_PATH: "."})
+os.remove("./test_mmtk")
+os.remove("./test_mmtkc")
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clang -lmmtk -Ltarget/debug -o main -Iapi ./api/main.c
-LD_LIBRARY_PATH=target/debug ./main
-
-clang -lmmtk -Ltarget/release -o main -Iapi ./api/main.c
-LD_LIBRARY_PATH=target/release ./main
