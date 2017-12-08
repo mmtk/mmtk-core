49f3dd4d66a5654133990258cec9f1fa52e26426
Pavel Zakopaylo <zakopaylo2@gmail.com>
2017-12-08 11:18:08 +1100

    Reverted workaround to alignment issue
    JikesRVM is capable of specifying the correct alignment.

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -2,8 +2,6 @@ use libc::{mmap, PROT_READ, PROT_WRITE, PROT_EXEC, MAP_PRIVATE, MAP_ANON, c_void
 use ::util::address::Address;
 use std::ptr::null_mut;
 
-use std::mem::uninitialized;
-
 const SPACE_ALIGN: usize = 1 << 19;
 
 #[derive(Debug)]
@@ -16,15 +14,12 @@ pub struct Space {
 
 impl Space {
     pub fn new() -> Self {
-        // FIXME: We need to find a better solution here.
-        //
-        // This is a workaround for what appears to be a compiler bug.
-        // Essentially, it tries to initialize multiple fields at once
-        // using a `movapd` but fails because the alignment is wrong.
-        //
-        // This is (relatively) safe to do because the fields only get
-        // properly initialized in `init`, not here.
-        unsafe { uninitialized() }
+        Space {
+            mmap_start: 0,
+            mmap_len: 0,
+            heap_cursor: unsafe { Address::zero() },
+            heap_limit: unsafe { Address::zero() },
+        }
     }
 
     pub fn init(&mut self, heap_size: usize) {
