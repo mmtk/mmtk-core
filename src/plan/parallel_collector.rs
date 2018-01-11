dcd5ca7bab3edaf9f0c262d10c22d83038361f1a
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-01-11 19:38:23 +1100

    Added struct for parallel worker groups

--- src/plan/parallel_collector.rs
+++ src/plan/parallel_collector.rs
@@ -3,10 +3,16 @@ use super::Phase;
 use super::TraceLocal;
 
 pub trait ParallelCollector: CollectorContext {
-    fn collect();
-    fn collection_phase(phase_id: Phase, primary: bool);
+    fn collect(&self);
+    fn collection_phase(&self, phase_id: Phase, primary: bool);
     fn get_current_trace<T: TraceLocal>() -> T;
-    fn parallel_worker_count() -> usize;
-    fn parallel_worker_ordinal() -> usize;
-    fn rendezvous() -> usize;
+    fn parallel_worker_count(&self) -> usize;
+    fn parallel_worker_ordinal(&self) -> usize;
+    fn rendezvous(&self) -> usize;
+
+    fn get_last_trigger_count(&self) -> usize;
+    fn set_last_trigger_count(&self, val: usize);
+    fn increment_last_trigger_count(&self) {
+        self.set_last_trigger_count(self.get_last_trigger_count() + 1);
+    }
 }
