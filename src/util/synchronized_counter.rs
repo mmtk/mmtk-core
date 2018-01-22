a9fd30a94e88711c395f5c9c0ae15492ccdcf4a9
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-01-22 18:05:59 +1100

    Fixed minor error in spec for SynchronizedCounter

--- src/util/synchronized_counter.rs
+++ src/util/synchronized_counter.rs
@@ -10,9 +10,9 @@ impl SynchronizedCounter {
         self.count.swap(0, Ordering::Relaxed)
     }
 
-    pub fn increment(&self) {
+    pub fn increment(&self) -> usize {
         debug_assert!(self.count.load(Ordering::Acquire) != usize::MAX);
-        self.count.fetch_add(1, Ordering::Relaxed);
+        self.count.fetch_add(1, Ordering::Relaxed)
     }
 
     pub fn peek(&self) -> usize {
