050a2ea379e38d0bc2fa4810814ebafe957cfbc6
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-01-24 11:29:31 +1100

    Implemented scanning of statics for JikesRVM

--- src/vm/jikesrvm/scan_statics.rs
+++ src/vm/jikesrvm/scan_statics.rs
@@ -2,7 +2,7 @@ use super::jtoc::*;
 use super::JTOC_BASE;
 use super::scheduling::VMScheduling;
 use ::util::Address;
-use ::plan::{TraceLocal};
+use ::plan::{TraceLocal, SelectedPlan, Plan, ParallelCollector};
 
 static mut REF_SLOT_SIZE: usize = 0;
 static mut CHUNK_SIZE_MASK: usize = 0;
@@ -15,6 +15,37 @@ pub unsafe fn set_ref_slot_size(thread_id: usize) {
 pub fn scan_statics<T: TraceLocal>(trace: &mut T, thread_id: usize) {
     unsafe {
         let slots = JTOC_BASE;
+
         let thread = VMScheduling::thread_from_id(thread_id);
+        let system_thread = Address::from_usize(
+            (thread + SYSTEM_THREAD_FIELD_JTOC_OFFSET).load::<usize>());
+        let cc = &*((system_thread + WORKER_INSTANCE_FIELD_JTOC_OFFSET)
+            .load::<*const <SelectedPlan as Plan>::CollectorT>());
+
+        let number_of_collectors: usize = cc.parallel_worker_count();
+        let number_of_references: usize = jtoc_call!(GET_NUMBER_OF_REFERENCE_SLOTS_METHOD_JTOC_OFFSET,
+            thread_id);
+        let chunk_size: usize = (number_of_references / number_of_collectors) & CHUNK_SIZE_MASK;
+        let thread_ordinal = cc.parallel_worker_ordinal();
+
+        let start: usize = if thread_ordinal == 0 {
+            REF_SLOT_SIZE
+        } else {
+            thread_ordinal * chunk_size
+        };
+
+        let end: usize = if thread_ordinal + 1 == number_of_collectors {
+            number_of_references
+        } else {
+            (thread_ordinal + 1) * chunk_size
+        };
+
+        let mut slot = start;
+        while (slot < end) {
+            let slot_offset = slot * 4;
+            // TODO: check_reference?
+            trace.process_root_edge(slots + slot_offset, true);
+            slot += REF_SLOT_SIZE;
+        }
     }
 }
