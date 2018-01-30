d669969aa9d6d5c875812327ddbd0ede51376d3f
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-01-30 14:36:47 +1100

    Implemented is_mutator in ActivePlan and fixed compile error

--- src/vm/jikesrvm/active_plan.rs
+++ src/vm/jikesrvm/active_plan.rs
@@ -2,6 +2,7 @@ use ::vm::ActivePlan;
 use ::plan::{Plan, SelectedPlan};
 use ::util::Address;
 use super::entrypoint::*;
+use super::scheduling::VMScheduling;
 
 pub struct VMActivePlan<> {}
 
@@ -16,14 +17,17 @@ impl<'a> ActivePlan<'a> for VMActivePlan {
             let system_thread = Address::from_usize(
                 (thread + SYSTEM_THREAD_FIELD_OFFSET).load::<usize>());
             let cc = &mut *((system_thread + WORKER_INSTANCE_FIELD_OFFSET)
-                .load::<*const <SelectedPlan as Plan>::CollectorT>());
+                .load::<*mut <SelectedPlan as Plan>::CollectorT>());
 
             cc
         }
     }
 
     fn is_mutator(thread_id: usize) -> bool {
-        unimplemented!()
+        unsafe {
+            let thread = VMScheduling::thread_from_id(thread_id);
+            !(thread + IS_COLLECTOR_FIELD_OFFSET).load::<bool>()
+        }
     }
 
     fn mutator(thread_id: usize) -> &'a mut <SelectedPlan<'a> as Plan>::MutatorT {
