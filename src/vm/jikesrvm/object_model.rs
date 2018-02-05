b844ed7a51bd4772b796062970dcdc4bf878a266
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-02-05 17:02:13 +1100

    Implemented more of JikesRVM object model

--- src/vm/jikesrvm/object_model.rs
+++ src/vm/jikesrvm/object_model.rs
@@ -3,9 +3,14 @@ use super::java_header_constants::{ADDRESS_BASED_HASHING, GC_HEADER_OFFSET, DYNA
     HASHCODE_BYTES};
 use super::java_header::*;
 use super::memory_manager_constants::*;
+use super::tib_layout_constants::*;
+use super::entrypoint::*;
+use super::unboxed_size_constants::*;
+use super::JTOC_BASE;
 
 use ::vm::object_model::ObjectModel;
 use ::util::{Address, ObjectReference};
+use ::util::constants::*;
 use ::plan::Allocator;
 use std::mem::size_of;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -38,11 +43,38 @@ impl ObjectModel for VMObjectModel {
     }
 
     fn get_current_size(object: ObjectReference) -> usize {
-        unimplemented!()
+        unsafe {
+            let tib = Address::from_usize(object.to_address().load::<usize>());
+            let rvm_type = Address::from_usize((tib + TIB_TYPE_INDEX * BYTES_IN_ADDRESS)
+                .load::<usize>());
+
+            let mut size = if (rvm_type + IS_CLASS_TYPE_FIELD_OFFSET).load::<bool>() {
+                (rvm_type + INSTANCE_SIZE_FIELD_OFFSET).load::<usize>()
+            } else {
+                let num_elements = Self::get_array_length(object);
+                ARRAY_HEADER_SIZE
+                    + (num_elements << (rvm_type + LOG_ELEMENT_SIZE_FIELD_OFFSET).load::<usize>())
+            };
+
+            if MOVES_OBJECTS {
+                if ADDRESS_BASED_HASHING {
+                    let hash_state = (object.value() as isize + STATUS_OFFSET) as usize
+                        & HASH_STATE_MASK;
+                    if hash_state == HASH_STATE_HASHED_AND_MOVED {
+                        size += HASHCODE_BYTES;
+                    }
+                }
+            }
+
+            size
+        }
     }
 
     fn get_next_object(object: ObjectReference) -> ObjectReference {
-        unimplemented!()
+        unsafe {
+            // XXX: It can't be this simple..
+            Self::get_object_from_start_address(Self::get_object_end_address(object))
+        }
     }
 
     unsafe fn get_object_from_start_address(start: Address) -> ObjectReference {
@@ -57,7 +89,30 @@ impl ObjectModel for VMObjectModel {
     }
 
     fn get_object_end_address(object: ObjectReference) -> Address {
-        unimplemented!()
+        unsafe {
+            let tib = Address::from_usize(object.to_address().load::<usize>());
+            let rvm_type = Address::from_usize((tib + TIB_TYPE_INDEX * BYTES_IN_ADDRESS)
+                .load::<usize>());
+
+            let mut size = if (rvm_type + IS_CLASS_TYPE_FIELD_OFFSET).load::<bool>() {
+                (rvm_type + INSTANCE_SIZE_FIELD_OFFSET).load::<usize>()
+            } else {
+                let num_elements = Self::get_array_length(object);
+                ARRAY_HEADER_SIZE
+                    + (num_elements << (rvm_type + LOG_ELEMENT_SIZE_FIELD_OFFSET).load::<usize>())
+            };
+
+            if ADDRESS_BASED_HASHING && DYNAMIC_HASH_OFFSET {
+                let hash_state = (object.value() as isize + STATUS_OFFSET) as usize
+                    & HASH_STATE_MASK;
+                if hash_state == HASH_STATE_HASHED_AND_MOVED {
+                    size += HASHCODE_BYTES;
+                }
+            }
+            object.to_address()
+                + Address::from_usize(size).align_up(BYTES_IN_INT).as_usize()
+                - OBJECT_REF_OFFSET
+        }
     }
 
     fn get_type_descriptor(reference: ObjectReference) -> &'static [i8] {
@@ -65,11 +120,24 @@ impl ObjectModel for VMObjectModel {
     }
 
     fn is_array(object: ObjectReference) -> bool {
-        unimplemented!()
+        unsafe {
+            let tib = Address::from_usize(object.to_address().load::<usize>());
+            let rvm_type = Address::from_usize((tib + TIB_TYPE_INDEX * BYTES_IN_ADDRESS)
+                .load::<usize>());
+            (rvm_type + IS_ARRAY_TYPE_FIELD_OFFSET).load::<bool>()
+        }
     }
 
     fn is_primitive_array(object: ObjectReference) -> bool {
-        unimplemented!()
+        unsafe {
+            // XXX: Is it OK to compare references like this?
+            object.value() == (JTOC_BASE + LONG_ARRAY_FIELD_OFFSET).load::<usize>()
+                || object.value() == (JTOC_BASE + INT_ARRAY_FIELD_OFFSET).load::<usize>()
+                || object.value() == (JTOC_BASE + BYTE_ARRAY_FIELD_OFFSET).load::<usize>()
+                || object.value() == (JTOC_BASE + INT_ARRAY_FIELD_OFFSET).load::<usize>()
+                || object.value() == (JTOC_BASE + DOUBLE_ARRAY_FIELD_OFFSET).load::<usize>()
+                || object.value() == (JTOC_BASE + FLOAT_ARRAY_FIELD_OFFSET).load::<usize>()
+        }
     }
 
     fn get_array_length(object: ObjectReference) -> usize {
