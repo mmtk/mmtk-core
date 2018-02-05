44187bf3753f0a8d1fa9797faa4bd98033035814
Pavel Zakopaylo <zakopaylo2@gmail.com>
2018-02-05 12:39:44 +1100

    Refactored options to enable faster reads (no HashMap)

--- src/util/options/option_closure.rs
+++ src/util/options/option_closure.rs
@@ -3,126 +3,113 @@ use std::cell::UnsafeCell;
 use std::collections::HashMap;
 use num_cpus;
 
+use libc::c_void;
+
 pub struct UnsafeOptionsWrapper {
-    inner_map: UnsafeCell<HashMap<CLIOptionKey, CLIOption>>
+    inner_map: UnsafeCell<Options>
 }
 
 unsafe impl Sync for UnsafeOptionsWrapper {}
 
 impl UnsafeOptionsWrapper {
-    unsafe fn len(&self) -> usize {
-        (&mut *self.inner_map.get()).len()
-    }
-
-    fn get(&self, name: &str) -> Option<&CLIOption> {
-        let key = &CLIOptionKey::from_camelcase_str(name);
+    pub fn get(&self) -> &Options {
         unsafe {
-            (&mut *self.inner_map.get()).get(key)
+            (&*self.inner_map.get())
         }
     }
 
     pub unsafe fn process(&self, name: &str, value: &str) -> bool {
-        trace!("Trying to process option pair: ({}, {})", name, value);
-        let k = &CLIOptionKey::from_camelcase_str(name);
-        let v = (&mut *self.inner_map.get()).get_mut(k).unwrap();
-        match *v {
-            IntOption(ref mut o) => {
-                o.set(value)
-            },
-            BoolOption(ref mut o) => o.set(value)
-        }
+        (&mut *self.inner_map.get()).set_from_camelcase_str(name, value)
     }
 }
 
-pub trait CLIOptionTrait<T> {
+pub trait CLIOption<T: Default> {
+    fn new(default: T, validator: Option<fn(T) -> bool>) -> Self;
     fn get(&self) -> T;
-    fn set(&mut self, value_str: &str) -> bool;
+    fn set(&mut self, value: &str) -> bool;
 }
 
-pub struct CLIOptionType<T, F> {
-    value: T,
-    validator: F,
+pub struct IntOption {
+    value: usize,
+    validator: Option<fn(usize) -> bool>
 }
 
-impl<T, F> CLIOptionType<T, F>
-    where T: FromStr,
-          T: Copy,
-          F: Fn(&T) -> bool
-{
-    fn new(default_value: T, validator: F) -> Self {
-        CLIOptionType {
-            value: default_value,
+impl CLIOption<usize> for IntOption {
+    fn new(default: usize, validator: Option<fn(usize) -> bool>) -> Self {
+        Self {
+            value: default,
             validator,
         }
     }
-}
 
-impl<T, F> CLIOptionTrait<T> for CLIOptionType<T, F>
-    where T: FromStr,
-          T: Copy,
-          F: Fn(&T) -> bool
-{
-    fn set(&mut self, value_str: &str) -> bool {
-        let value = match value_str.parse() {
-            Ok(v) => v,
-            Err(_) => return false
-        };
-        if (self.validator)(&value) {
-            trace!("value: {} passed validation", value_str);
-            self.value = value;
-            return true;
-        }
-        trace!("value: {} failed validation", value_str);
-        false
-    }
-
-    fn get(&self) -> T {
+    fn get(&self) -> usize {
         self.value
     }
+
+    fn set(&mut self, value: &str) -> bool {
+        if let Ok(dval) = value.parse() {
+            let succ = self.validator.unwrap_or(|_| true)(dval);
+            if succ { self.value = dval; }
+            succ
+        } else {
+            false
+        }
+    }
 }
 
-pub enum CLIOption {
-    IntOption(&'static mut CLIOptionTrait<usize>),
-    BoolOption(&'static mut CLIOptionTrait<bool>),
+pub struct BoolOption {
+    value: bool,
+    validator: Option<fn(bool) -> bool>
 }
 
-use self::CLIOption::*;
+impl CLIOption<bool> for BoolOption {
+    fn new(default: bool, validator: Option<fn(bool) -> bool>) -> Self {
+        Self {
+            value: default,
+            validator,
+        }
+    }
 
-#[derive(Hash, Eq, PartialEq)]
-pub enum CLIOptionKey {
-    Threads,
-    UseShortStackScans,
-    UseReturnBarrier,
-    EagerCompleteSweep,
+    fn get(&self) -> bool {
+        self.value
+    }
+
+    fn set(&mut self, value: &str) -> bool {
+        if let Ok(dval) = value.parse() {
+            let succ = self.validator.unwrap_or(|_| true)(dval);
+            if succ { self.value = dval; }
+            succ
+        } else {
+            false
+        }
+    }
 }
 
-impl CLIOptionKey {
-    fn from_camelcase_str(s: &str) -> CLIOptionKey {
+pub struct Options {
+    pub threads: IntOption,
+    pub use_short_stack_scans: BoolOption,
+    pub use_return_barrier: BoolOption,
+    pub eager_complete_sweep: BoolOption,
+}
+
+impl Options {
+    fn set_from_camelcase_str(&mut self, s: &str, val: &str) -> bool {
         match s {
-            "threads" => Threads,
-            "useShortStackScans" => UseShortStackScans,
-            "useReturnBarrier" => UseReturnBarrier,
-            "eagerCompleteSweep" => EagerCompleteSweep,
+            "threads" => self.threads.set(val),
+            "useShortStackScans" => self.use_short_stack_scans.set(val),
+            "useReturnBarrier" => self.use_return_barrier.set(val),
+            "eagerCompleteSweep" => self.eager_complete_sweep.set(val),
             _ => panic!("Invalid CLIOptionKey")
         }
     }
 }
 
-use self::CLIOptionKey::*;
-
 lazy_static! {
-    pub static ref OptionMap: UnsafeOptionsWrapper = UnsafeOptionsWrapper { inner_map: UnsafeCell::new({
-        let mut map = HashMap::new();
-        unsafe {
-            map.insert(Threads, IntOption(&mut *Box::into_raw(Box::new(
-                       CLIOptionType::new(num_cpus::get(), |v| *v > 0)))));
-            map.insert(UseShortStackScans, BoolOption(&mut *Box::into_raw(Box::new(
-                       CLIOptionType::new(false, |v| true)))));
-            map.insert(UseReturnBarrier, BoolOption(&mut *Box::into_raw(Box::new(
-                       CLIOptionType::new(false, |v| true)))));
-            map.insert(EagerCompleteSweep, BoolOption(&mut *Box::into_raw(Box::new(
-                       CLIOptionType::new(false, |v| true)))));
-        }
-        map
-    })};
+    pub static ref OptionMap: UnsafeOptionsWrapper = UnsafeOptionsWrapper { inner_map: UnsafeCell::new(
+        Options {
+            threads: IntOption::new(num_cpus::get(), Some(|v| v > 0)),
+            use_short_stack_scans: BoolOption::new(false, None),
+            use_return_barrier: BoolOption::new(false, None),
+            eager_complete_sweep: BoolOption::new(false, None),
+        })};
 }
