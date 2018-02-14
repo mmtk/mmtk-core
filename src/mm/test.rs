290a04d5bd3cd587fe93c5a1c6e100e0ebb40fb7
Zixian Cai <u5937495@anu.edu.au>
2018-02-14 17:05:12 +1100

    Use info instead of debug

--- src/mm/test.rs
+++ src/mm/test.rs
@@ -1,24 +1,23 @@
-
 #[no_mangle]
 #[cfg(feature = "jikesrvm")]
 pub extern fn test_stack_alignment() {
-    trace!{"Entering stack alignment test with no args passed"}
+    info!("Entering stack alignment test with no args passed");
     unsafe {
         asm!("movaps %xmm1, (%esp)" : : : "sp", "%xmm1", "memory");
     }
-    trace!{"Exiting stack alignment test"}
+    info!("Exiting stack alignment test");
 }
 
 #[no_mangle]
 #[cfg(feature = "jikesrvm")]
 pub extern fn test_stack_alignment1(a: usize, b: usize, c: usize, d: usize, e: usize) -> usize {
-    trace!{"Entering stack alignment test"}
-    trace!{"a:{} , b:{}, c:{}, d:{}, e:{}",
-           a, b, c, d, e}
+    info!("Entering stack alignment test");
+    info!("a:{} , b:{}, c:{}, d:{}, e:{}",
+           a, b, c, d, e);
     unsafe {
         asm!("movaps %xmm1, (%esp)" : : : "sp", "%xmm1", "memory");
     }
     let result = a + b * 2 + c * 3  + d * 4 + e * 5;
-    trace!{"Exiting stack alignment test"}
+    info!("Exiting stack alignment test");
     result
 }
