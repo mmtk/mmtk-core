0d048f2c336f58f8af36ec40b01e65303b04ec17
Brenda Wang <u6374399@anu.edu.au>
2018-02-27 13:17:08 +1100

    Only run tests when in debug mode

--- src/mm/memory_manager.rs
+++ src/mm/memory_manager.rs
@@ -30,10 +30,9 @@ pub unsafe extern fn jikesrvm_gc_init(jtoc: *mut c_void, heap_size: usize) {
     ::util::logger::init().unwrap();
     JTOC_BASE = Address::from_mut_ptr(jtoc);
     selected_plan::PLAN.gc_init(heap_size);
-    ::vm::JikesRVM::test1();
-    info!("{}", ::vm::JikesRVM::test(44));
-    info!("{}", ::vm::JikesRVM::test2(45, 67));
-    info!("{}", ::vm::JikesRVM::test3(21, 34, 9, 8));
+    debug_assert!(54 == ::vm::JikesRVM::test(44));
+    debug_assert!(112 == ::vm::JikesRVM::test2(45, 67));
+    debug_assert!(731 == ::vm::JikesRVM::test3(21, 34, 9, 8));
 }
 
 #[no_mangle]
