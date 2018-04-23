437053a7690a4c3b4f4462c931287082d7970833
Zixian Cai <u5937495@anu.edu.au>
2018-04-23 14:59:21 +1000

    Implemented DumpLinearScan

--- src/util/alloc/mod.rs
+++ src/util/alloc/mod.rs
@@ -2,6 +2,7 @@ mod bumpallocator;
 pub mod allocator;
 pub mod embedded_meta_data;
 pub mod linear_scan;
+pub mod dump_linear_scan;
 
 pub use self::allocator::Allocator;
 pub use self::bumpallocator::BumpAllocator;
