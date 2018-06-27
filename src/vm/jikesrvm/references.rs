872f93d96f1052b381f5ffc92a9ff585959d9fb9
Zixian Cai <u5937495@anu.edu.au>
2018-06-27 17:05:04 +1000

    Passing int instead of bool across boundary

--- src/vm/jikesrvm/references.rs
+++ src/vm/jikesrvm/references.rs
@@ -11,28 +11,28 @@ impl References for VMReferences {
     fn forward_refs<T: TraceLocal>(trace: &mut T, thread_id: usize) {
         let trace_ptr = trace as *mut T;
         unsafe {
-            jtoc_call!(PROCESS_REFERENCE_TYPES_METHOD_OFFSET, thread_id, trace_ptr, false);
+            jtoc_call!(PROCESS_REFERENCE_TYPES_METHOD_OFFSET, thread_id, trace_ptr, 0);
         }
     }
 
     fn scan_weak_refs<T: TraceLocal>(trace: &mut T, thread_id: usize) {
         let trace_ptr = trace as *mut T;
         unsafe {
-            jtoc_call!(SCAN_WEAK_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, false);
+            jtoc_call!(SCAN_WEAK_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, 0);
         }
     }
 
     fn scan_soft_refs<T: TraceLocal>(trace: &mut T, thread_id: usize) {
         let trace_ptr = trace as *mut T;
         unsafe {
-            jtoc_call!(SCAN_SOFT_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, false);
+            jtoc_call!(SCAN_SOFT_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, 0);
         }
     }
 
     fn scan_phantom_refs<T: TraceLocal>(trace: &mut T, thread_id: usize) {
         let trace_ptr = trace as *mut T;
         unsafe {
-            jtoc_call!(SCAN_PHANTOM_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, false);
+            jtoc_call!(SCAN_PHANTOM_REFERENCE_TYPE_METHOD_OFFSET, thread_id, trace_ptr, 0);
         }
     }
 }
