1659398220db58ced3d2acf7db35821a71cabfd3
Zixian Cai <u5937495@anu.edu.au>
2018-09-13 18:31:21 +1000

    Add comments on racy usage of work-stealing queues

--- src/util/global_pool.rs
+++ src/util/global_pool.rs
@@ -2,6 +2,9 @@ use std::thread;
 use std::sync::mpsc;
 use crossbeam_deque::{self as deque, Steal, Stealer};
 
+// FIXME we have a race condition here that after GC workers check that the global queue is empty,
+// the coordinator thread might still add work to the queue
+// Thus, we might have residual work after all GC threads finish the cycle
 pub fn new<T: 'static>(name: &'static str) -> (Stealer<T>, mpsc::Sender<T>) where T: Send {
     let (w, s) = deque::fifo();
     let (tx, rx) = mpsc::channel();
