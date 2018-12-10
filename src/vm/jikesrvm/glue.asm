73ae969719b35ccd71f0d33d7e1a2b9039ef6c07
Zixian Cai <u5937495@anu.edu.au>
2018-12-10 18:20:08 +1100

    Store and process callee-save non-volatile regs in glue frames

--- src/vm/jikesrvm/glue.asm
+++ src/vm/jikesrvm/glue.asm
@@ -4,13 +4,21 @@
 jikesrvm_alloc_slow:
     pushl 0x100(%esi)
     mov %esp, 0x100(%esi)
-    push $0xffffffff
-    pushl 0x1c(%esp)
-    pushl 0x1c(%esp)
-    pushl 0x1c(%esp)
-    pushl 0x1c(%esp)
-    pushl 0x1c(%esp)
+    pushl $0xffffffff #method id
+    pushl $42 #fingerprint
+    push %ebx
+    push %edi
+    push %ebp
+    pushl 0x2c(%esp)
+    pushl 0x2c(%esp)
+    pushl 0x2c(%esp)
+    pushl 0x2c(%esp)
+    pushl 0x2c(%esp)
     call alloc_slow
-    add $0x18, %esp
+    add $0x14, %esp #shrink stack for args
+    popl %ebp
+    popl %edi
+    popl %ebx
+    add $0x8, %esp #shrink stack for method id
     popl 0x100(%esi)
     ret
