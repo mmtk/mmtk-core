75243ed8d4736752b1c271484ceb5ae392b7ff90
Zixian Cai <u5937495@anu.edu.au>
2018-12-13 15:17:06 +1100

    Update the glue code to reflect the latest workaround

--- src/vm/jikesrvm/glue.asm
+++ src/vm/jikesrvm/glue.asm
@@ -5,21 +5,13 @@ jikesrvm_alloc_slow:
     pushl 0x100(%esi)
     mov %esp, 0x100(%esi)
     pushl $0xffffffff #method id
-    pushl $42 #fingerprint
-    push %ebx
-    push %edi
-    push %ebp
-    pushl 0x2c(%esp)
-    pushl 0x2c(%esp)
-    pushl 0x2c(%esp)
-    pushl 0x2c(%esp)
-    pushl 0x2c(%esp)
+    pushl 0x1c(%esp)
+    pushl 0x1c(%esp)
+    pushl 0x1c(%esp)
+    pushl 0x1c(%esp)
+    pushl 0x1c(%esp)
     call alloc_slow
-    add $0x14, %esp #shrink stack for args
-    popl %ebp
-    popl %edi
-    popl %ebx
-    add $0x8, %esp #shrink stack for method id
+    add $0x18, %esp #shrink stack for args and method id
     popl 0x100(%esi)
     ret
 
@@ -27,16 +19,8 @@ jikesrvm_handle_user_collection_request:
     pushl 0x100(%esi)
     mov %esp, 0x100(%esi)
     pushl $0xffffffff #method id
-    pushl $42 #fingerprint
-    push %ebx
-    push %edi
-    push %ebp
-    pushl 0x2c(%esp)
+    pushl 0x1c(%esp)
     call handle_user_collection_request
-    add $0x4, %esp #shrink stack for args
-    popl %ebp
-    popl %edi
-    popl %ebx
-    add $0x8, %esp #shrink stack for method id
+    add $0x8, %esp #shrink stack for args and method id
     popl 0x100(%esi)
     ret
