4000482960fbb353278175ae22290310c8daf6b3
Wenyu Zhao <wenyuzhaox@gmail.com>
2019-01-03 17:00:01 +1100

    Add `VM_MAP.descriptor_map` support

--- src/vm/memory.rs
+++ src/vm/memory.rs
@@ -1,5 +1,11 @@
 use ::util::Address;
+use libc;
 
 pub trait Memory {
   fn dzmmap(start: Address, size: usize) -> i32;
+  fn zero(start: Address, len: usize) {
+    unsafe {
+      libc::memset(start.to_ptr_mut() as *mut libc::c_void, 0, len);
+    }
+  }
 }
