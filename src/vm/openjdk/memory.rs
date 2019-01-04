1b2cab1cc57ef63c1c85fea8fbf56895a80f7dbf
Wenyu Zhao <wenyuzhaox@gmail.com>
2019-01-04 14:32:22 +1100

    Change `MAP_ANONYMOUS` to `MAP_ANON`

--- src/vm/openjdk/memory.rs
+++ src/vm/openjdk/memory.rs
@@ -7,7 +7,7 @@ pub struct VMMemory;
 impl Memory for VMMemory {
   fn dzmmap(start: Address, size: usize) -> i32 {
     let prot = libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC;
-    let flags = libc::MAP_ANONYMOUS | libc::MAP_PRIVATE | libc::MAP_FIXED;
+    let flags = libc::MAP_ANON | libc::MAP_PRIVATE | libc::MAP_FIXED;
     let result = unsafe { Address::from_usize(libc::mmap(start.0 as _, size, prot, flags, -1, 0) as _) };
     if result == start {
       0
