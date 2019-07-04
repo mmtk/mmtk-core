c5c6095afcf6e4ce1cdaca1f8321702cd1f553a4
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2019-07-04 14:29:21 +1000

    Change GC_HEADER_WORDS to 0

--- src/plan/gencopy/ssconstraints.rs
+++ src/plan/gencopy/ssconstraints.rs
@@ -3,5 +3,5 @@ pub use ::plan::plan_constraints::*;
 
 pub const MOVES_OBJECTS: bool = true;
 pub const GC_HEADER_BITS: usize = 2;
-pub const GC_HEADER_WORDS: usize = 1;
+pub const GC_HEADER_WORDS: usize = 0;
 pub const NUM_SPECIALIZED_SCANS: usize = 1;
