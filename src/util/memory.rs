8134c95a3d45a12694b5640abe9a16781f7414c8
Yi Lin <yi.lin@anu.edu.au>
2020-02-24 14:24:46 +1100

    Added unit tests fro ByteMapMmapper

--- src/util/memory.rs
+++ src/util/memory.rs
@@ -1,5 +1,6 @@
 use util::Address;
-use libc::c_void;
+use std::io::{Result, Error};
+use libc::{PROT_READ, PROT_WRITE, PROT_EXEC, PROT_NONE, c_void};
 
 pub fn zero(start: Address, len: usize) {
     unsafe {
@@ -7,17 +8,35 @@ pub fn zero(start: Address, len: usize) {
     }
 }
 
-pub fn dzmmap(start: Address, size: usize) -> i32 {
+pub fn dzmmap(start: Address, size: usize) -> Result<Address> {
     let prot = libc::PROT_READ | libc::PROT_WRITE | libc::PROT_EXEC;
     let flags = libc::MAP_ANON | libc::MAP_PRIVATE | libc::MAP_FIXED;
-    let result: *mut c_void = unsafe { libc::mmap(start.to_mut_ptr::<c_void>(), size, prot, flags, -1, 0) };
+    let result: *mut c_void = unsafe { libc::mmap(start.to_mut_ptr(), size, prot, flags, -1, 0) };
     if Address::from_mut_ptr(result) == start {
-        0
+        Ok(Address::from_mut_ptr(result))
     } else {
         assert!(result as usize <= 127,
                 "mmap with MAP_FIXED has unexpected behavior: demand zero mmap with MAP_FIXED on {:?} returned some other address {:?}",
                 start, result
         );
-        result as _
+        Err(Error::from_raw_os_error(result as _))
+    }
+}
+
+pub fn munprotect(start: Address, size: usize) -> Result<()> {
+    let result = unsafe { libc::mprotect(start.to_mut_ptr(), size, PROT_READ | PROT_WRITE | PROT_EXEC) };
+    if result == 0 {
+        Ok(())
+    } else {
+        Err(Error::from_raw_os_error(result))
+    }
+}
+
+pub fn mprotect(start: Address, size: usize) -> Result<()> {
+    let result = unsafe { libc::mprotect(start.to_mut_ptr(), size, PROT_NONE) };
+    if result == 0 {
+        Ok(())
+    } else {
+        Err(Error::from_raw_os_error(result))
     }
 }
