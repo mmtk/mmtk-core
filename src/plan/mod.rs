79379a3967a0affe16803352a6ec5eb0f8408a97
Yi Lin <qinsoon@gmail.com>
2020-04-01 13:56:24 +1300

    Fix for rustfmt

--- src/plan/mod.rs
+++ src/plan/mod.rs
@@ -1,22 +1,22 @@
 pub mod collector_context;
 pub mod controller_collector_context;
+pub mod global;
 pub mod mutator_context;
 pub mod parallel_collector;
 mod parallel_collector_group;
 pub mod phase;
-pub mod global;
 pub mod plan_constraints;
 mod trace;
 pub mod tracelocal;
 pub mod transitive_closure;
 
 pub use self::collector_context::CollectorContext;
+pub use self::global::Allocator;
+pub use self::global::Plan;
 pub use self::mutator_context::MutatorContext;
 pub use self::parallel_collector::ParallelCollector;
 pub use self::parallel_collector_group::ParallelCollectorGroup;
 pub use self::phase::Phase;
-pub use self::global::Allocator;
-pub use self::global::Plan;
 pub use self::tracelocal::TraceLocal;
 pub use self::transitive_closure::TransitiveClosure;
 
--- src/plan/nogc/mod.rs
+++ src/plan/nogc/mod.rs
@@ -1,16 +1,16 @@
-mod global;
 mod collector;
 pub mod constraints;
+mod global;
 mod mutator;
 mod tracelocal;
 
-pub use self::global::NoGC;
 pub use self::collector::NoGCCollector;
+pub use self::global::NoGC;
 pub use self::mutator::NoGCMutator;
 pub use self::tracelocal::NoGCTraceLocal;
 
-pub use self::global::SelectedPlan;
 pub use self::collector::NoGCCollector as SelectedCollector;
 pub use self::constraints as SelectedConstraints;
+pub use self::global::SelectedPlan;
 pub use self::mutator::NoGCMutator as SelectedMutator;
 pub use self::tracelocal::NoGCTraceLocal as SelectedTraceLocal;
