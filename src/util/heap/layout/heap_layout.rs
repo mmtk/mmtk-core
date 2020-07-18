888222ecafc1b67c480c5f3f5c9805c98f4a954f
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-07-18 23:17:16 +1000

    Remove fixmes

--- src/util/heap/layout/heap_layout.rs
+++ src/util/heap/layout/heap_layout.rs
@@ -8,9 +8,6 @@ use crate::util::heap::layout::map64::Map64;
 #[cfg(all(target_pointer_width = "64", not(feature = "force_32bit_heap_layout")))]
 use crate::util::heap::layout::FragmentedMapper;
 
-// FIXME: Use FragmentMmapper for 64-bit heaps
-// FIXME: Use Map64 for 64-bit heaps
-
 #[cfg(any(target_pointer_width = "32", feature = "force_32bit_heap_layout"))]
 pub type VMMap = Map32;
 #[cfg(all(target_pointer_width = "64", not(feature = "force_32bit_heap_layout")))]
