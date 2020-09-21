9f7b34c6d962621c87070fae62075a338f7fba56
Yi Lin <qinsoon@gmail.com>
2020-09-21 11:33:52 +1000

    Run the tests in CI

--- .github/scripts/ci-test.sh
+++ .github/scripts/ci-test.sh
@@ -3,3 +3,7 @@ set -xe
 cargo test --features nogc
 cargo test --features semispace
 python examples/build.py
+
+cd vmbindings/dummyvm
+cargo test fixed_live --features nogc
+cargo test gcbench --features nogc
