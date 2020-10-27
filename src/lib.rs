a710307437ba2b40a13430397610005e6f3e5921
Yi Lin <qinsoon@gmail.com>
2020-10-28 12:59:25 +1300

    Fix for cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,25 @@
 #![feature(maybe_uninit_ref)]
 #![feature(maybe_uninit_extra)]
 
-//! Memory Management ToolKit (MMTk) is a portable and high performance memory manager 
-//! that includes various garbage collection algorithms and provides clean and efficient 
+//! Memory Management ToolKit (MMTk) is a portable and high performance memory manager
+//! that includes various garbage collection algorithms and provides clean and efficient
 //! interfaces to cooperate with language implementations. MMTk features highly modular
 //! and highly reusable designs. It includes components such as allocators, spaces and
-//! work packets that GC implementers can choose from to compose their own GC plan easily. 
-//! 
+//! work packets that GC implementers can choose from to compose their own GC plan easily.
+//!
 //! Logically this crate includes these major parts:
 //! * GC components
 //!   * [Allocators](util/alloc/allocator/trait.Allocator.html): handlers of allocation requests and allocates objects to the bound space.
 //!   * [Spaces](policy/space/trait.Space.html): memory regions in the heap with defined semantics and behaviors.
 //!   * [Work packets](scheduler/work/trait.GCWork.html): the unit of GC works scheduled by the MMTk's scheduler.
-//! * [GC plans](plan/global/trait.Plan.html): GC algorithms composed from components. 
-//!   *Note that currently the choice of plans is through Rust features, which is a build-time config, so only one plan is present in the generated binary 
+//! * [GC plans](plan/global/trait.Plan.html): GC algorithms composed from components.
+//!   *Note that currently the choice of plans is through Rust features, which is a build-time config, so only one plan is present in the generated binary
 //!   and in this documentation. We plan to make this a run-time config so that users can choose GC plans at boot time.*
 //! * [Heap implementations](util/heap/index.html): the foundation to support spaces.
 //! * [Scheduler](scheduler/scheduler/struct.Scheduler.html): the MMTk scheduler to allow flexible and parallel execution of GC works.
-//! * Interfaces: a bi-direcitonal interface between MMTk and a language implementation, 
-//!   i.e. [the memory manager API](memory_manager/index.html) that allows a language's memory manager to use MMTk 
-//!   and [the VMBinding trait](vm/trait.VMBinding.html) that allows MMTk to call the language implementation. 
+//! * Interfaces: a bi-direcitonal interface between MMTk and a language implementation,
+//!   i.e. [the memory manager API](memory_manager/index.html) that allows a language's memory manager to use MMTk
+//!   and [the VMBinding trait](vm/trait.VMBinding.html) that allows MMTk to call the language implementation.
 
 #[macro_use]
 extern crate custom_derive;
