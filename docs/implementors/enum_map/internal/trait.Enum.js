901cb17c9c027eec612f8fcbdae2cb81458472ea
qinsoon <qinsoon@users.noreply.github.com>
2020-11-03 21:56:30 +0000

    deploy: aa518f13e04441ef6f344c88fe7f7464e6a3a73a

--- docs/implementors/enum_map/internal/trait.Enum.js
+++ docs/implementors/enum_map/internal/trait.Enum.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mmtk"] = [{"text":"impl&lt;V&gt; Enum&lt;V&gt; for <a class=\"enum\" href=\"mmtk/enum.Allocator.html\" title=\"enum mmtk::Allocator\">Allocator</a>","synthetic":false,"types":["mmtk::plan::global::Allocator"]}];
+implementors["mmtk"] = [{"text":"impl&lt;V&gt; Enum&lt;V&gt; for <a class=\"enum\" href=\"mmtk/enum.AllocationSemantics.html\" title=\"enum mmtk::AllocationSemantics\">AllocationSemantics</a>","synthetic":false,"types":["mmtk::plan::global::AllocationSemantics"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
