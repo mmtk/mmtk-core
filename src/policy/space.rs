6d48536d3181026005dd4fe18a0645697d06f502
Yi Lin <qinsoon@gmail.com>
2020-11-09 19:51:16 +1100

    Fix clippy errors

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -133,7 +133,28 @@ impl SFTMap {
         unsafe { &*res }
     }
 
-    fn trace_print_map(&self) {
+    // #[allow(clippy::not_unsafe_ptr_arg_deref)]
+    fn log_update(&self, space: *const (dyn SFT + Sync), start: Address, chunks: usize) {
+        let first = start.chunk_index();
+        let end = start + (chunks << LOG_BYTES_IN_CHUNK);
+        debug!(
+            "Update SFT for [{}, {}) as {}",
+            start,
+            end,
+            unsafe { &(*space) }.name()
+        );
+        let start_chunk = chunk_index_to_address(first);
+        let end_chunk = chunk_index_to_address(first + chunks);
+        debug!(
+            "Update SFT for {} chunks of [{} #{}, {} #{})",
+            chunks,
+            start_chunk,
+            first,
+            end_chunk,
+            first + chunks
+        );
+
+        // print the entire SFT map
         const SPACE_PER_LINE: usize = 10;
         for i in (0..self.sft.len()).step_by(SPACE_PER_LINE) {
             let max = if i + SPACE_PER_LINE > self.sft.len() {
@@ -156,24 +177,7 @@ impl SFTMap {
             self.set(chunk, space);
         }
         if DEBUG_SFT {
-            let end = start + (chunks << LOG_BYTES_IN_CHUNK);
-            debug!(
-                "Update SFT for [{}, {}) as {}",
-                start,
-                end,
-                unsafe { &(*space) }.name()
-            );
-            let start_chunk = chunk_index_to_address(first);
-            let end_chunk = chunk_index_to_address(first + chunks);
-            debug!(
-                "Update SFT for {} chunks of [{} #{}, {} #{})",
-                chunks,
-                start_chunk,
-                first,
-                end_chunk,
-                first + chunks
-            );
-            self.trace_print_map();
+            self.log_update(space, start, chunks);
         }
     }
 
@@ -193,9 +197,9 @@ impl SFTMap {
          */
         let self_mut: &mut Self = unsafe { self.mut_self() };
         // It is okay to set empty to valid, or set valid to empty. It is wrong if we overwrite a valid value with another valid value.
-        assert!(
-            (self_mut.sft[chunk] == (&EMPTY_SPACE_SFT) as *const _)
-                || (sft == (&EMPTY_SPACE_SFT) as *const _),
+        debug_assert!(
+            (unsafe { self_mut.sft[chunk].as_ref() }.unwrap().name() == "empty")
+                || (unsafe { sft.as_ref() }.unwrap().name() == "empty"),
             "attempt to overwrite a non-empty chunk in SFT map"
         );
         self_mut.sft[chunk] = sft;
