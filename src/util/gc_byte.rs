d7ed637fc650fd10075dcea837aa1aadf250c0ab
Javad Amiri <javad.amiri@anu.edu.au>
2020-11-16 11:51:20 +1100

    fix bug in negative GC_BYTE_OFFSET

--- src/util/gc_byte.rs
+++ src/util/gc_byte.rs
@@ -1,6 +1,7 @@
 use crate::util::ObjectReference;
 use crate::vm::ObjectModel;
 use crate::vm::VMBinding;
+use std::mem;
 use std::sync::atomic::{AtomicU8, Ordering};
 
 /// Return the GC byte of an object.
@@ -26,6 +27,10 @@ pub fn get_gc_byte_value<VM: VMBinding>(object: ObjectReference) -> u8 {
     get_gc_byte::<VM>(object).load(Ordering::SeqCst)
 }
 
-pub fn get_gc_byte_offset<VM: VMBinding>() -> isize {
-    VM::VMObjectModel::GC_BYTE_OFFSET
+pub fn get_relative_offset<VM: VMBinding>() -> isize {
+    #[cfg(target_pointer_width = "64")]
+    let sys_ptr_width = 64;
+    #[cfg(target_pointer_width = "32")]
+    let sys_ptr_width = 32;
+    (VM::VMObjectModel::GC_BYTE_OFFSET).rem_euclid(sys_ptr_width)
 }
