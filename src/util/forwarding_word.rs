b7f566644eba72db054b98aacf0319af6125d387
Javad Amiri <javad.amiri@anu.edu.au>
2020-12-09 14:51:18 +1100

    typo

--- src/util/forwarding_word.rs
+++ src/util/forwarding_word.rs
@@ -227,7 +227,7 @@ pub(super) fn gc_byte_offset_in_forwarding_word<VM: VMBinding>() -> Option<isize
 #[cfg(debug_assertions)]
 pub(crate) fn check_alloc_size<VM: VMBinding>(size: usize) {
     debug_assert!(
-        if VM::VMObjectModel::HAS_GC_BYTE ||
+        if !VM::VMObjectModel::HAS_GC_BYTE ||
          gc_byte_offset_in_forwarding_word::<VM>().is_some() {
             size >= constants::BYTES_IN_WORD
         } else {
