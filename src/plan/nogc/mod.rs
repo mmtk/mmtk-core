2d2e947e59f758e46cf4df67400578e5653d6a45
Yi Lin <qinsoon@gmail.com>
2021-01-13 12:53:07 +1100

    Remove features for plans

--- src/plan/nogc/mod.rs
+++ src/plan/nogc/mod.rs
@@ -1,8 +1,4 @@
-pub mod constraints;
 mod global;
 mod mutator;
 
 pub use self::global::NoGC;
-
-pub use self::constraints as SelectedConstraints;
-pub use self::global::SelectedPlan;
--- src/plan/semispace/mod.rs
+++ src/plan/semispace/mod.rs
@@ -1,9 +1,5 @@
-pub mod constraints;
 mod gc_works;
 mod global;
 mod mutator;
 
 pub use self::global::SemiSpace;
-
-pub use self::constraints as SelectedConstraints;
-pub use self::global::SelectedPlan;
