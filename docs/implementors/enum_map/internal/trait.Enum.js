d8024c1d935c6abd9b3c370a022dfb5730fbb9ed
wenyuzhao <wenyuzhao@users.noreply.github.com>
2021-01-28 01:35:12 +0000

    deploy: f93f50fe8e48c2f4765e84f02f58b370bf4f4348

--- docs/implementors/enum_map/internal/trait.Enum.js
+++ docs/implementors/enum_map/internal/trait.Enum.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mmtk"] = [{"text":"impl&lt;V&gt; Enum&lt;V&gt; for AllocationSemantics","synthetic":false,"types":[]}];
+implementors["mmtk"] = [{"text":"impl&lt;V&gt; Enum&lt;V&gt; for AllocationSemantics","synthetic":false,"types":[]},{"text":"impl&lt;V&gt; Enum&lt;V&gt; for WorkBucketStage","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
