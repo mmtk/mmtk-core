adb89e6b97b9735a978b724a43ab8a28ca8ec056
paigereeves <paigereeves006@gmail.com>
2021-01-28 15:52:51 +1100

    Explanation for malloc implementation and updated hoard-sys dependency.

--- src/util/malloc.rs
+++ src/util/malloc.rs
@@ -6,8 +6,6 @@ pub use mimalloc_sys::{
     mi_calloc as calloc, mi_free as free, mi_malloc_usable_size as malloc_usable_size,
 };
 
-// Hoard links dynamically; you must define LD_LIBRARY_PATH
-// export LD_LIBRARY_PATH=./../../mmtk/target/release/build/hoard-sys-982b9ad0d5b55f98/out/Hoard/src
 #[cfg(feature = "malloc_hoard")]
 pub use hoard_sys::{calloc, free, malloc_usable_size};
 
