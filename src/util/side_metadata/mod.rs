2982b1c2e5c3b3b374ad9d86c9166480ee7a0536
Javad Amiri <javad.amiri@anu.edu.au>
2021-02-04 17:50:04 +1100

    update module comments

--- src/util/side_metadata/mod.rs
+++ src/util/side_metadata/mod.rs
@@ -1,6 +1,6 @@
 //! This is a generic module to work with side metadata (vs. in-object metadata)
 //!
-//! This module enables the implementation of a wide range of GC algorithms for VMs which do not provide (any/sufficient) in-object space for GC-specific metadata (e.g. marking bits, etc.).
+//! This module enables the implementation of a wide range of GC algorithms for VMs which do not provide (any/sufficient) in-object space for GC-specific metadata (e.g. marking bits, logging bit, etc.).
 //!
 //!
 //! # Design
@@ -9,45 +9,56 @@
 //!
 //! It aims to support two categories of side metadata:
 //!
-//! 1. **Global** metadata bits which are per-object metadata and are common to all policies, and
-//! 2. **Policy-specific** bits which are only used by certain policies and are not necessarily per-object.
+//! 1. **Global** metadata bits which are plan-specific but common to all policies, and
+//! 2. **Policy-specific** bits which are only used exclusively by certain policies.
 //!
 //! To support these categories, MMTk side metadata provides the following features:
 //!
-//! 1. The granularity of the source data is configurable to $2^n$ bytes, where $n >= 0$.
-//! 2. The number of metadata bits per source data unit is configurable to to $2^m$ bits, where $m >= 0$.
-//! 3. The total number of metadata bit-sets is only constrained by the amount of available memory.
-//! 4. For each metadata bit-set, the metadata space is only allocated for the range of heap addresses it chooses to cover.
-//! 5. Bulk-zeroing of metadata bits should be possible. For this, the memory space for each metadata bit-set is contiguous.
+//! 1. The granularity of the source data (minimum data size) is configurable to $2^n$ bytes, where $n >= 0$.
+//! 2. The number of metadata bits per source data unit is configurable to $2^m$ bits, where $m >= 0$.
+//! 3. The total number of metadata bit-sets is constrained by the worst-case ratio of global and policy-specific metadata.
+//! 4. Metadata space is only allocated on demand.
+//! 5. Bulk-zeroing of metadata bits should be possible. For this, the memory space for each metadata bit-set is contiguous per chunk.
 //!
 //!
 //! # How to Use
 //!
-//! For each side metadata bit-set, first request the metadata bits by calling:
+//! For each global side metadata bit-set, a constant object of the `SideMetadataSpec` struct should be created.
+//!
+//! For the first global side metadata bit-set:
 //!
 //! ```
-//! SideMetadata::request_meta_bits(number_of_bits: usize, align: usize) -> SideMetadataID
+//! const GLOBAL_META_1: SideMetadataSpec = SideMetadataSpec {
+//!    scope: SideMetadataScope::Global,
+//!    offset: 0,
+//!    log_num_of_bits: b,
+//!    log_min_obj_size: s,
+//! };
 //! ```
 //!
-//! The returned ID is used for future references to this side metadata.
+//! the offset is zero, the number of bits per data is $2^b$, and the minimum object size is $2^s$.
 //!
-//! Requesting the bits does not allocate any metadata space.
-//! So, the next step is to announce the data space you want the metadata to cover, by calling:
+//! Not, to add a second side metadata bit-set, offset needs to be calculated based-on the first global bit-set:
 //!
 //! ```
-//! SideMetadata::ensure_meta_space_is_mapped(
-//!     start: Address,
-//!     size: usize,
-//!     metadata_id: SideMetadataID
-//! ) -> bool
+//! const GLOBAL_META_2: SideMetadataSpec = SideMetadataSpec {
+//!    scope: SideMetadataScope::Global,
+//!    offset: meta_bytes_per_chunk(s, b),
+//!    log_num_of_bits: bb,
+//!    log_min_obj_size: ss,
+//! };
 //! ```
 //!
-//! On success, this function returns `true`.
+//! where `meta_bytes_per_chunk` is a const function which calculates the offset based-on `s` and `b` from the first global bit-set.
 //!
-//! NOTE-1: A return value of `false` may mean you need to use a different memory space for your data)
+//! So far, no metadata space is allocated.
 //!
-//! NOTE-2: As you allocate more memory, you may need to map more metadata space at runtime.
+//! For this purpose, each plan should override `fn global_side_metadata_per_chuck(&self) -> usize;` to return the size of the global side metadata it needs per chunk. This can be calculated using the `meta_bytes_per_chunk` function.
 //!
+//! For the local metadata bit-sets, each policy needs to follow the same pattern as the global metadata, with two differences:
+//!
+//! 1. scope should be `SideMetadataScope::PolicySpecific`,
+//! 2. each policy needs to override `fn local_side_metadata_per_chuck(&self) -> usize;
 //!
 //! After mapping the metadata space, the following operations can be performed on the metadata:
 //!
