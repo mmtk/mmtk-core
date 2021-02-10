3ea1c4331560fee21eb982b6b5c974a784d012ae
Yi Lin <qinsoon@gmail.com>
2021-02-10 13:44:54 +1100

    Remove test skip in ci-test (the bug was fixd in master)

--- .github/scripts/ci-test.sh
+++ .github/scripts/ci-test.sh
@@ -4,9 +4,7 @@ for_all_features "cargo test"
 
 # For x86_64-linux, also check for i686
 if [[ $arch == "x86_64" && $os == "linux" ]]; then
-    # Skipping one test - see Issue #229. Remove the skip part when the bug is fixed.
-    # After the bug is fixed, change the following line to 'for_all_features "cargo test --target i686-unknown-linux-gnu"'
-    cargo test --target i686-unknown-linux-gnu -- --skip test_side_metadata_try_mmap_metadata_chunk
+    for_all_features "cargo test --target i686-unknown-linux-gnu"
 fi
 
 python examples/build.py
