b0848fc6e71381364d3e8f833e7203b61a00f15d
Javad Amiri <javad.amiri@anu.edu.au>
2021-02-11 14:34:24 +1100

    cargo fmt

--- src/util/side_metadata/helpers.rs
+++ src/util/side_metadata/helpers.rs
@@ -35,7 +35,8 @@ pub(crate) fn address_to_meta_address(
 #[inline(always)]
 pub(crate) const fn meta_bytes_per_chunk(log_min_obj_size: usize, log_num_of_bits: usize) -> usize {
     1usize
-        << (LOG_BYTES_IN_CHUNK - (constants::LOG_BITS_IN_BYTE as usize) - log_min_obj_size + log_num_of_bits)
+        << (LOG_BYTES_IN_CHUNK - (constants::LOG_BITS_IN_BYTE as usize) - log_min_obj_size
+            + log_num_of_bits)
 }
 
 #[inline(always)]
