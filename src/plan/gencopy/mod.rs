9099a40ae08d3c78e49a0a76d7fb6f34918e28b4
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-02-18 10:04:46 +1100

    No slow path

--- src/plan/gencopy/mod.rs
+++ src/plan/gencopy/mod.rs
@@ -4,8 +4,8 @@ pub(super) mod mutator;
 
 pub use self::global::GenCopy;
 
-pub const FULL_NURSERY_GC: bool = false;
-pub const NO_SLOW: bool = false;
+pub const FULL_NURSERY_GC: bool = true;
+pub const NO_SLOW: bool = true;
 
 pub use self::global::GENCOPY_CONSTRAINTS;
 
