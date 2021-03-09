8481a56d51c95fbaa58a07cc2943829524c010a2
Yi Lin <qinsoon@gmail.com>
2021-03-09 13:26:54 +1100

    Properly do align and offset for MallocAllocator

--- src/util/alloc/malloc_allocator.rs
+++ src/util/alloc/malloc_allocator.rs
@@ -4,6 +4,7 @@ use crate::policy::space::Space;
 use crate::util::alloc::Allocator;
 use crate::util::Address;
 use crate::util::OpaquePointer;
+use crate::util::alloc::allocator;
 use crate::vm::VMBinding;
 
 #[repr(C)]
@@ -21,7 +22,8 @@ impl<VM: VMBinding> Allocator<VM> for MallocAllocator<VM> {
         self.plan
     }
     fn alloc(&mut self, size: usize, align: usize, offset: isize) -> Address {
-        self.alloc_slow(size, align, offset)
+        let cell = self.alloc_slow(size, align, offset);
+        allocator::align_allocation::<VM>(cell, align, offset, VM::MIN_ALIGNMENT, true)
     }
 
     fn get_tls(&self) -> OpaquePointer {
@@ -29,10 +31,8 @@ impl<VM: VMBinding> Allocator<VM> for MallocAllocator<VM> {
     }
 
     fn alloc_slow_once(&mut self, size: usize, align: usize, offset: isize) -> Address {
-        // TODO: We currently ignore the offset field. This is wrong.
-        // assert!(offset == 0);
-        assert!(align <= 16);
-        let ret = self.space.unwrap().alloc(size);
+        let maxbytes = allocator::get_maximum_aligned_size::<VM>(size, align, VM::MIN_ALIGNMENT);
+        let ret = self.space.unwrap().alloc(maxbytes);
         trace!(
             "MallocSpace.alloc size = {}, align = {}, offset = {}, res = {}",
             size,
