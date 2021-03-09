9ed0048b55994022d3dd23c94d2a6d356a7e2d85
Yi Lin <qinsoon@gmail.com>
2021-03-09 14:24:28 +1100

    Remove unused imports

--- src/util/alloc/malloc_allocator.rs
+++ src/util/alloc/malloc_allocator.rs
@@ -1,7 +1,6 @@
 use crate::plan::global::Plan;
 use crate::policy::mallocspace::MallocSpace;
 use crate::policy::space::Space;
-use crate::util::alloc::allocator;
 use crate::util::alloc::Allocator;
 use crate::util::Address;
 use crate::util::OpaquePointer;
