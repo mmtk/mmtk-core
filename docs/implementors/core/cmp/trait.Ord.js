44f62d26fbd399b744d6c36ebbc3735a5bdf3668
qinsoon <qinsoon@users.noreply.github.com>
2021-04-12 00:42:55 +0000

    deploy: c2132e36c0f0db18819ab2ae7b88a099e2225aaa

---
+++ docs/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["mmtk"] = [{"text":"impl Ord for Address","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
