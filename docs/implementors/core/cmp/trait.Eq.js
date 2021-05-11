9dd49bd5f4073280ec88402eb9066577c20e9b4e
qinsoon <qinsoon@users.noreply.github.com>
2021-05-11 07:28:15 +0000

    deploy: e750dfe52c92ac2916690f45237b90b93e032020

--- docs/implementors/core/cmp/trait.Eq.js
+++ docs/implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mmtk"] = [{"text":"impl Eq for Address","synthetic":false,"types":[]},{"text":"impl Eq for ObjectReference","synthetic":false,"types":[]},{"text":"impl Eq for OpaquePointer","synthetic":false,"types":[]},{"text":"impl Eq for WorkBucketStage","synthetic":false,"types":[]}];
+implementors["mmtk"] = [{"text":"impl Eq for Address","synthetic":false,"types":[]},{"text":"impl Eq for ObjectReference","synthetic":false,"types":[]},{"text":"impl Eq for OpaquePointer","synthetic":false,"types":[]},{"text":"impl Eq for VMThread","synthetic":false,"types":[]},{"text":"impl Eq for VMMutatorThread","synthetic":false,"types":[]},{"text":"impl Eq for VMWorkerThread","synthetic":false,"types":[]},{"text":"impl Eq for WorkBucketStage","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
