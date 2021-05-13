0526559cb74121dcfb5325816dbccb1b3edf95f7
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-05-13 13:43:22 +1000

    Fix new_chunk

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -332,10 +332,10 @@ pub trait Space<VM: VMBinding>: 'static + SFT + Sync + Downcast {
             bytes,
             new_chunk
         );
-        // debug_assert!(
-        //     (new_chunk && start.is_aligned_to(BYTES_IN_CHUNK)) || !new_chunk,
-        //     "should only grow space for new chunks at chunk-aligned start address"
-        // );
+        debug_assert!(
+            (new_chunk && start.is_aligned_to(BYTES_IN_CHUNK)) || !new_chunk,
+            "should only grow space for new chunks at chunk-aligned start address"
+        );
         if new_chunk {
             let chunks = conversions::bytes_to_chunks_up(bytes);
             SFT_MAP.update(self.as_sft(), start, chunks);
