6f87962191fd7a1c06a1c56511e60b79846a1d9b
Yi Lin <qinsoon@gmail.com>
2021-05-13 11:38:28 +1000

    Fix doc

--- src/plan/mod.rs
+++ src/plan/mod.rs
@@ -2,15 +2,15 @@
 //!
 //! This module provides various GC plans, each of which implements a GC algorithm.
 //! Generally a plan consists of a few parts:
-//! * A plan type that implements the [`Plan`](crate::plan::global::Plan) trait, which defines
+//! * A plan type that implements the [`Plan`](crate::plan::Plan) trait, which defines
 //!   spaces used in the plan, and their behaviors in GC and page accounting.
 //! * A mutator definition, which describes the mapping between allocators and allocation semantics,
 //!   and the mapping between allocators and spaces. If the plan needs barrier, the barrier definition is
 //!   also included here.
-//! * A constant for [`PlanConstraints`](crate::plan::plan_constraints::PlanConstraints), which defines
+//! * A constant for [`PlanConstraints`](crate::plan::PlanConstraints), which defines
 //!   plan-specific constants.
 //! * Plan-specific [`GCWork`](crate::scheduler::GCWork), which is scheduled during GC. If the plan
-//!   implements a copying GC, a [`CopyContext`](crate::plan::global::CopyContext) also needs to be provided.
+//!   implements a copying GC, a [`CopyContext`](crate::plan::CopyContext) also needs to be provided.
 //!
 //! For more about implementing a plan, it is recommended to read the [MMTk tutorial](/docs/tutorial/Tutorial.md).
 
