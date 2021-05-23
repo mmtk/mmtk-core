c0425b40152032c523358b86c863094049438add
paigereeves <paigereeves006@gmail.com>
2021-05-23 22:31:34 +1000

    Interact with mimalloc

--- src/util/alloc/mod.rs
+++ src/util/alloc/mod.rs
@@ -1,22 +1,25 @@
 pub(crate) mod allocator;
 pub use allocator::fill_alignment_gap;
-pub use allocator::Allocator;
 
 pub(crate) mod allocators;
 pub use allocators::AllocatorSelector;
 
 mod bumpallocator;
-pub use bumpallocator::BumpAllocator;
+pub mod dump_linear_scan;
+pub mod embedded_meta_data;
+pub mod large_object_allocator;
+pub mod linear_scan;
+pub mod malloc_allocator;
+pub mod mimalloc;
 
-mod large_object_allocator;
-pub use large_object_allocator::LargeObjectAllocator;
+pub use self::allocator::Allocator;
+pub use self::bumpallocator::BumpAllocator;
+pub use self::large_object_allocator::LargeObjectAllocator;
+pub use self::malloc_allocator::MallocAllocator;
 
-mod malloc_allocator;
-pub use malloc_allocator::MallocAllocator;
+pub use crate::policy::mallocspace::metadata::is_alloced_by_malloc;
+pub use self::mimalloc::do_something;
+pub use self::mimalloc::mimalloc_dzmmap;
 
 pub mod immix_allocator;
 pub use self::immix_allocator::ImmixAllocator;
-
-pub(crate) mod dump_linear_scan;
-pub(crate) mod embedded_meta_data;
-pub(crate) mod linear_scan;
