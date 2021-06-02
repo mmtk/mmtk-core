163e8860fc4e4f0c34a5faf0aa9b1d01b8153737
Yi Lin <qinsoon@gmail.com>
2021-06-02 11:51:05 +1000

    Revert "Revert changes about quarantine and copyspace reset"
    This reverts commit 94e000e5e93b45bc6d1d390d58c1391ae30e8449.

--- src/util/heap/layout/fragmented_mapper.rs
+++ src/util/heap/layout/fragmented_mapper.rs
@@ -14,7 +14,7 @@ const MMAP_NUM_CHUNKS: usize = 1 << (33 - LOG_MMAP_CHUNK_BYTES);
 // 36 = 128G - physical memory larger than this is uncommon
 // 40 = 2T. Increased to 2T. Though we probably won't use this much memory, we allow quarantine memory range,
 // and that is usually used to quarantine a large amount of memory.
-const LOG_MAPPABLE_BYTES: usize = 36;
+const LOG_MAPPABLE_BYTES: usize = 40;
 
 /*
  * Size of a slab.  The value 10 gives a slab size of 1GB, with 1024
@@ -80,35 +80,35 @@ impl Mmapper for FragmentedMapper {
         }
     }
 
-    fn quarantine_address_range(&self, mut _start: Address, _pages: usize) -> Result<()> {
-        // let end = start + conversions::pages_to_bytes(pages);
-        // // Iterate over the slabs covered
-        // while start < end {
-        //     let base = Self::slab_align_down(start);
-        //     let high = if end > Self::slab_limit(start) && !Self::slab_limit(start).is_zero() {
-        //         Self::slab_limit(start)
-        //     } else {
-        //         end
-        //     };
+    fn quarantine_address_range(&self, mut start: Address, pages: usize) -> Result<()> {
+        let end = start + conversions::pages_to_bytes(pages);
+        // Iterate over the slabs covered
+        while start < end {
+            let base = Self::slab_align_down(start);
+            let high = if end > Self::slab_limit(start) && !Self::slab_limit(start).is_zero() {
+                Self::slab_limit(start)
+            } else {
+                end
+            };
 
-        //     let slab = Self::slab_align_down(start);
-        //     let start_chunk = Self::chunk_index(slab, start);
-        //     let end_chunk = Self::chunk_index(slab, conversions::mmap_chunk_align_up(high));
+            let slab = Self::slab_align_down(start);
+            let start_chunk = Self::chunk_index(slab, start);
+            let end_chunk = Self::chunk_index(slab, conversions::mmap_chunk_align_up(high));
 
-        //     let mapped = self.get_or_allocate_slab_table(start);
+            let mapped = self.get_or_allocate_slab_table(start);
 
-        //     /* Iterate over the chunks within the slab */
-        //     for (chunk, entry) in mapped.iter().enumerate().take(end_chunk).skip(start_chunk) {
-        //         if matches!(entry.load(Ordering::Relaxed), MapState::Quarantined) {
-        //             continue;
-        //         }
+            /* Iterate over the chunks within the slab */
+            for (chunk, entry) in mapped.iter().enumerate().take(end_chunk).skip(start_chunk) {
+                if matches!(entry.load(Ordering::Relaxed), MapState::Quarantined) {
+                    continue;
+                }
 
-        //         let mmap_start = Self::chunk_index_to_address(base, chunk);
-        //         let _guard = self.lock.lock().unwrap();
-        //         MapState::transition_to_quarantined(entry, mmap_start).unwrap();
-        //     }
-        //     start = high;
-        // }
+                let mmap_start = Self::chunk_index_to_address(base, chunk);
+                let _guard = self.lock.lock().unwrap();
+                MapState::transition_to_quarantined(entry, mmap_start).unwrap();
+            }
+            start = high;
+        }
         Ok(())
     }
 
