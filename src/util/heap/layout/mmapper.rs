4b6aa4b7049c0121b9e00eb6f60de89d7135f12b
Yi Lin <qinsoon@gmail.com>
2021-06-09 11:25:49 +1000

    Add comments for quanrantine_address_range(), reformat comments to rustdoc

--- src/util/heap/layout/mmapper.rs
+++ src/util/heap/layout/mmapper.rs
@@ -4,57 +4,58 @@ use crate::util::Address;
 use atomic::{Atomic, Ordering};
 use std::io::Result;
 
+/// Generic mmap and protection functionality
 pub trait Mmapper {
-    /****************************************************************************
-     * Generic mmap and protection functionality
-     */
-
-    /**
-     * Given an address array describing the regions of virtual memory to be used
-     * by MMTk, demand zero map all of them if they are not already mapped.
-     *
-     * @param spaceMap An address array containing a pairs of start and end
-     * addresses for each of the regions to be mappe3d
-     */
+    /// Given an address array describing the regions of virtual memory to be used
+    /// by MMTk, demand zero map all of them if they are not already mapped.
+    ///
+    /// Arguments:
+    /// * `spaceMap`: An address array containing a pairs of start and end
+    ///   addresses for each of the regions to be mapped
     fn eagerly_mmap_all_spaces(&self, space_map: &[Address]);
 
-    /**
-     * Mark a number of pages as mapped, without making any
-     * request to the operating system.  Used to mark pages
-     * that the VM has already mapped.
-     * @param start Address of the first page to be mapped
-     * @param bytes Number of bytes to ensure mapped
-     */
+    /// Mark a number of pages as mapped, without making any
+    /// request to the operating system.  Used to mark pages
+    /// that the VM has already mapped.
+    ///
+    /// Arguments:
+    /// * `start`: Address of the first page to be mapped
+    /// * `bytes`: Number of bytes to ensure mapped
     fn mark_as_mapped(&self, start: Address, bytes: usize);
 
+    /// Quarantine/reserve address range. We mmap from the OS with no reserve and with PROT_NONE,
+    /// which should be little overhead. This ensures that we can reserve certain address range that
+    /// we can use if needed. Quarantined memory needs to be mapped before it can be used.
+    ///
+    /// Arguments:
+    /// * `start`: Address of the first page to be quarantined
+    /// * `bytes`: Number of bytes to quarantine from the start
     fn quarantine_address_range(&self, start: Address, pages: usize) -> Result<()>;
 
-    /**
-     * Ensure that a range of pages is mmapped (or equivalent).  If the
-     * pages are not yet mapped, demand-zero map them. Note that mapping
-     * occurs at chunk granularity, not page granularity.<p>
-     *
-     * NOTE: There is a monotonicity assumption so that only updates require lock
-     * acquisition.
-     * TODO: Fix the above to support unmapping.
-     *
-     * @param start The start of the range to be mapped.
-     * @param pages The size of the range to be mapped, in pages
-     */
+    /// Ensure that a range of pages is mmapped (or equivalent).  If the
+    /// pages are not yet mapped, demand-zero map them. Note that mapping
+    /// occurs at chunk granularity, not page granularity.<p>
+    ///
+    /// Argumetns:
+    /// `start`: The start of the range to be mapped.
+    /// `pages`: The size of the range to be mapped, in pages
+    // NOTE: There is a monotonicity assumption so that only updates require lock
+    // acquisition.
+    // TODO: Fix the above to support unmapping.
     fn ensure_mapped(&self, start: Address, pages: usize) -> Result<()>;
 
-    /**
-     * Is the page pointed to by this address mapped ?
-     * @param addr Address in question
-     * @return {@code true} if the page at the given address is mapped.
-     */
+    /// Is the page pointed to by this address mapped? Returns true if
+    /// the page at the given address is mapped.
+    ///
+    /// Arguments:
+    /// * `addr`: Address in question
     fn is_mapped_address(&self, addr: Address) -> bool;
 
-    /**
-     * Mark a number of pages as inaccessible.
-     * @param start Address of the first page to be protected
-     * @param pages Number of pages to be protected
-     */
+    /// Mark a number of pages as inaccessible.
+    ///
+    /// Arguments:
+    /// * `start`: Address of the first page to be protected
+    /// * `pages`: Number of pages to be protected
     fn protect(&self, start: Address, pages: usize);
 }
 
