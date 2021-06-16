50357ebc321866e47e3861c4865fc20ca578f295
paigereeves <paigereeves006@gmail.com>
2021-06-16 21:02:30 +1000

    free list allocator and mark sweep space skeletons

---
+++ src/policy/marksweepspace.rs
@@ -0,0 +1,120 @@
+use crate::{TransitiveClosure, util::{Address, ObjectReference, constants::CARD_META_PAGES_PER_REGION, heap::{FreeListPageResource, HeapMeta, VMRequest, layout::heap_layout::{Mmapper, VMMap}}, side_metadata::{SideMetadataContext, SideMetadataSpec}}, vm::VMBinding};
+
+use super::space::{CommonSpace, SFT, Space, SpaceOptions};
+const META_DATA_PAGES_PER_REGION: usize = CARD_META_PAGES_PER_REGION;
+
+pub struct MarkSweepSpace<VM: VMBinding> {
+    common: CommonSpace<VM>,
+    pr: FreeListPageResource<VM>    
+}
+
+impl<VM: VMBinding> SFT for MarkSweepSpace<VM> {
+    fn name(&self) -> &str {
+        todo!()
+    }
+
+    fn is_live(&self, object: crate::util::ObjectReference) -> bool {
+        todo!()
+    }
+
+    fn is_movable(&self) -> bool {
+        todo!()
+    }
+
+    fn is_sane(&self) -> bool {
+        todo!()
+    }
+
+    fn initialize_object_metadata(&self, object: crate::util::ObjectReference, alloc: bool) {
+        todo!()
+    }
+}
+
+impl<VM: VMBinding> Space<VM> for MarkSweepSpace<VM> {
+    fn as_space(&self) -> &dyn Space<VM> {
+        todo!()
+    }
+
+    fn as_sft(&self) -> &(dyn SFT + Sync + 'static) {
+        todo!()
+    }
+
+    fn get_page_resource(&self) -> &dyn crate::util::heap::PageResource<VM> {
+        &self.pr
+    }
+
+    fn init(&mut self, vm_map: &'static crate::util::heap::layout::heap_layout::VMMap) {
+        todo!()
+    }
+
+    fn common(&self) -> &CommonSpace<VM> {
+        &self.common
+    }
+
+    fn release_multiple_pages(&mut self, start: crate::util::Address) {
+        todo!()
+    }
+}
+
+impl<VM: VMBinding> MarkSweepSpace<VM> {
+    fn new(
+        name: &'static str,
+        zeroed: bool,
+        vmrequest: VMRequest,
+        local_side_metadata_specs: Vec<SideMetadataSpec>,
+        vm_map: &'static VMMap,
+        mmapper: &'static Mmapper,
+        heap: &mut HeapMeta,
+    ) -> MarkSweepSpace<VM> {
+        let common = CommonSpace::new(
+            SpaceOptions {
+                name,
+                movable: false,
+                immortal: false,
+                zeroed,
+                vmrequest,
+                side_metadata_specs: SideMetadataContext {
+                    global: vec![],
+                    local: local_side_metadata_specs
+                },
+            },
+            vm_map,
+            mmapper,
+            heap,
+        );
+        MarkSweepSpace {
+            common,
+            pr: if vmrequest.is_discontiguous() {
+                FreeListPageResource::new_discontiguous(META_DATA_PAGES_PER_REGION, vm_map)
+            } else {
+                FreeListPageResource::new_contiguous(common.start, common.extent, META_DATA_PAGES_PER_REGION, vm_map)
+            }
+        }
+    }
+
+    pub fn trace_object<T: TransitiveClosure>(
+        &self,
+        trace: &mut T,
+        object: ObjectReference,
+    ) -> ObjectReference {
+        todo!()
+    }
+
+    fn mark_object(
+        &self,
+        object: ObjectReference,
+    ) {
+        todo!()
+    }
+
+    pub fn acquire_block() -> Address {
+        // acquire 64kB block from the global pool
+        todo!()
+    }
+
+    pub fn return_block() {
+        // return freed 64kB block
+        todo!()
+    }
+
+}
