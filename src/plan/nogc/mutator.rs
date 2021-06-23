20b090797630aa68f59ba81ba4cec13a6eb2ef43
paigereeves <paigereeves006@gmail.com>
2021-06-23 12:26:03 +1000

    Send large objects to the immortal space

--- src/plan/nogc/mutator.rs
+++ src/plan/nogc/mutator.rs
@@ -49,11 +49,10 @@ pub fn create_nogc_mutator<VM: VMBinding>(
         space_mapping: box vec![(
             AllocatorSelector::FreeList(0),
             &plan.downcast_ref::<NoGC<VM>>().unwrap().nogc_space,
-        )],
+        ),],
         prepare_func: &nogc_mutator_noop,
         release_func: &nogc_mutator_noop,
     };
-    eprintln!("create mutator");
     Mutator {
         allocators: Allocators::<VM>::new(mutator_tls, plan, &config.space_mapping),
         barrier: Box::new(NoBarrier),
