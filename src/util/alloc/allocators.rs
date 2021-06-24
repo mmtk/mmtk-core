c3240bb9bc3b08ba57af2d256b7acdf8e1a30372
paigereeves <paigereeves006@gmail.com>
2021-06-24 10:34:57 +1000

    boxed allocator

--- src/util/alloc/allocators.rs
+++ src/util/alloc/allocators.rs
@@ -27,7 +27,7 @@ pub struct Allocators<VM: VMBinding> {
     pub bump_pointer: [MaybeUninit<BumpAllocator<VM>>; MAX_BUMP_ALLOCATORS],
     pub large_object: [MaybeUninit<LargeObjectAllocator<VM>>; MAX_LARGE_OBJECT_ALLOCATORS],
     pub malloc: [MaybeUninit<MallocAllocator<VM>>; MAX_MALLOC_ALLOCATORS],
-    pub free_list: [MaybeUninit<FreeListAllocator<VM>>; MAX_FREE_LIST_ALLOCATORS],
+    pub free_list: [MaybeUninit<Box<FreeListAllocator<VM>>>; MAX_FREE_LIST_ALLOCATORS],
 }
 
 impl<VM: VMBinding> Allocators<VM> {
@@ -45,7 +45,7 @@ impl<VM: VMBinding> Allocators<VM> {
                 self.malloc[index as usize].assume_init_ref()
             }
             AllocatorSelector::FreeList(index) => {
-                self.free_list[index as usize].assume_init_ref()
+                &**self.free_list[index as usize].assume_init_ref()
             }
         }
     }
@@ -67,7 +67,7 @@ impl<VM: VMBinding> Allocators<VM> {
                 self.malloc[index as usize].assume_init_mut()
             }
             AllocatorSelector::FreeList(index) => {
-                self.free_list[index as usize].assume_init_mut()
+                &mut **self.free_list[index as usize].assume_init_mut()
             }
         }
     }
@@ -108,11 +108,11 @@ impl<VM: VMBinding> Allocators<VM> {
                     ));
                 }
                 AllocatorSelector::FreeList(index) => {
-                    ret.free_list[index as usize].write(FreeListAllocator::new(
+                    ret.free_list[index as usize].write(Box::new(FreeListAllocator::new(
                         mutator_tls.0,
                         space.downcast_ref::<MarkSweepSpace<VM>>().unwrap(),
                         plan,
-                    ));
+                    )));
                 }
             }
         }
