9d7305ee7d8386066c518673321a03489cd06e15
Zixian Cai <caizixian@users.noreply.github.com>
2021-06-26 20:40:32 +1000

    Add perf event support (#315)
    * Add perfmon to Stats
    * Configure perf events with MMTk options
    * Include perf_counter feature in CI

--- src/scheduler/work_counter.rs
+++ src/scheduler/work_counter.rs
@@ -133,3 +133,84 @@ impl WorkCounter for WorkDuration {
         &mut self.base
     }
 }
+
+#[cfg(feature = "perf_counter")]
+mod perf_event {
+    //! Measure the perf events of work packets
+    //!
+    //! This is built on top of libpfm4.
+    //! The events to measure are parsed from MMTk option `perf_events`
+    use super::*;
+    use libc::{c_int, pid_t};
+    use pfm::PerfEvent;
+    use std::fmt;
+
+    /// Work counter for perf events
+    #[derive(Clone)]
+    pub struct WorkPerfEvent {
+        base: WorkCounterBase,
+        running: bool,
+        event_name: String,
+        pe: PerfEvent,
+    }
+
+    impl WorkPerfEvent {
+        /// Create a work counter
+        ///
+        /// See `perf_event_open` for more details on `pid` and `cpu`
+        /// Examples:
+        /// 0, -1 measures the calling thread on all CPUs
+        /// -1, 0 measures all threads on CPU 0
+        /// -1, -1 is invalid
+        pub fn new(name: &str, pid: pid_t, cpu: c_int) -> WorkPerfEvent {
+            let mut pe = PerfEvent::new(name)
+                .unwrap_or_else(|_| panic!("Failed to create perf event {}", name));
+            pe.open(pid, cpu)
+                .unwrap_or_else(|_| panic!("Failed to open perf event {}", name));
+            WorkPerfEvent {
+                base: Default::default(),
+                running: false,
+                event_name: name.to_string(),
+                pe,
+            }
+        }
+    }
+
+    impl fmt::Debug for WorkPerfEvent {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("WorkPerfEvent")
+                .field("base", &self.base)
+                .field("running", &self.running)
+                .field("event_name", &self.event_name)
+                .finish()
+        }
+    }
+
+    impl WorkCounter for WorkPerfEvent {
+        fn start(&mut self) {
+            self.running = true;
+            self.pe.reset();
+            self.pe.enable();
+        }
+        fn stop(&mut self) {
+            self.running = true;
+            let perf_event_value = self.pe.read().unwrap();
+            self.base.merge_val(perf_event_value.value as f64);
+            // assert not multiplexing
+            assert_eq!(perf_event_value.time_enabled, perf_event_value.time_running);
+            self.pe.disable();
+        }
+        fn name(&self) -> String {
+            self.event_name.to_owned()
+        }
+        fn get_base(&self) -> &WorkCounterBase {
+            &self.base
+        }
+        fn get_base_mut(&mut self) -> &mut WorkCounterBase {
+            &mut self.base
+        }
+    }
+}
+
+#[cfg(feature = "perf_counter")]
+pub(super) use perf_event::WorkPerfEvent;
