f5c03cca1f570636fc4457bda110d196026d31b1
paigereeves <paigereeves006@gmail.com>
2021-06-29 21:37:47 +1000

    _mi_bin

--- src/util/alloc/free_list_allocator.rs
+++ src/util/alloc/free_list_allocator.rs
@@ -113,10 +113,6 @@ pub(crate) const BLOCK_LISTS_EMPTY: [BlockList; MI_BIN_HUGE + 1] = [
     BlockList::new(MI_LARGE_OBJ_WSIZE_MAX + 1 /* 655360, Huge queue */),
 ];
 
-const BYTES_IN_BLOCK: usize = 1 << LOG_BYTES_IN_BLOCK;
-const LOG_BYTES_IN_BLOCK: usize = 16;
-const MI_BIN_HUGE: u8 = 73;
-
 pub struct FreeListAllocator<VM: VMBinding> {
     pub tls: VMThread,
     space: &'static MarkSweepSpace<VM>,
