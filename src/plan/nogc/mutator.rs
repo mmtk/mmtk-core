9d30dfac51d487a16d924161b1c5ba931a039736
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-06-29 15:56:34 +1000

    Fix NoGC allocators

--- src/plan/nogc/mutator.rs
+++ src/plan/nogc/mutator.rs
@@ -12,7 +12,12 @@ use enum_map::EnumMap;
 
 lazy_static! {
     pub static ref ALLOCATOR_MAPPING: EnumMap<AllocationType, AllocatorSelector> = enum_map! {
-        AllocationType::Default | AllocationType::Immortal | AllocationType::Code | AllocationType::LargeCode | AllocationType::ReadOnly | AllocationType::Los => AllocatorSelector::BumpPointer(0),
+        AllocationType::Default => AllocatorSelector::BumpPointer(0),
+        AllocationType::Immortal => AllocatorSelector::BumpPointer(1),
+        AllocationType::ReadOnly => AllocatorSelector::BumpPointer(2),
+        AllocationType::Code => AllocatorSelector::BumpPointer(3),
+        AllocationType::LargeCode => AllocatorSelector::BumpPointer(4),
+        AllocationType::Los => AllocatorSelector::LargeObject(0),
     };
 }
 
@@ -24,12 +29,17 @@ pub fn create_nogc_mutator<VM: VMBinding>(
     mutator_tls: VMMutatorThread,
     plan: &'static dyn Plan<VM = VM>,
 ) -> Mutator<VM> {
+    let nogc = plan.downcast_ref::<NoGC<VM>>().unwrap();
     let config = MutatorConfig {
         allocator_mapping: &*ALLOCATOR_MAPPING,
-        space_mapping: box vec![(
-            AllocatorSelector::BumpPointer(0),
-            &plan.downcast_ref::<NoGC<VM>>().unwrap().nogc_space,
-        )],
+        space_mapping: box vec![
+            (AllocatorSelector::BumpPointer(0), &nogc.nogc_space),
+            (AllocatorSelector::BumpPointer(1), nogc.common.get_immortal()),
+            (AllocatorSelector::BumpPointer(2), &nogc.base().ro_space),
+            (AllocatorSelector::BumpPointer(3), &nogc.base().code_space),
+            (AllocatorSelector::BumpPointer(4), &nogc.base().code_lo_space),
+            (AllocatorSelector::LargeObject(0), nogc.common.get_los()),
+        ],
         prepare_func: &nogc_mutator_noop,
         release_func: &nogc_mutator_noop,
     };
