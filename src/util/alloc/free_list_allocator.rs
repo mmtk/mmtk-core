4aec820d0540f997032d7fcece6e903c17db35a1
paigereeves <paigereeves006@gmail.com>
2021-07-01 13:25:33 +1000

    start on unoptimised blocks array

--- src/util/alloc/free_list_allocator.rs
+++ src/util/alloc/free_list_allocator.rs
@@ -6,20 +6,49 @@ use super::Allocator;
 
 const BYTES_IN_BLOCK: usize = 1 << LOG_BYTES_IN_BLOCK;
 const LOG_BYTES_IN_BLOCK: usize = 16;
-const MI_BIN_HUGE: u8 = 73;
+const MI_BIN_HUGE: usize = 73;
 const MI_INTPTR_SHIFT: usize = 3;
 const MI_INTPTR_SIZE: usize = 1 << MI_INTPTR_SHIFT;
-const MI_LARGE_OBJ_SIZE_MAX: usize = 1 << 21;
+pub const MI_LARGE_OBJ_SIZE_MAX: usize = 1 << 21;
 const MI_LARGE_OBJ_WSIZE_MAX: usize = MI_LARGE_OBJ_SIZE_MAX/MI_INTPTR_SIZE;
 const MI_INTPTR_BITS: usize = MI_INTPTR_SIZE*8;
+const MI_BIN_FULL: usize = MI_BIN_HUGE + 1;
+
+const BLOCK_QUEUES_EMPTY: [BlockQueue; 74] = [
+    BlockQueue::new(     1*8),
+    BlockQueue::new(     1*8), BlockQueue::new(     2*8), BlockQueue::new(     3*8), BlockQueue::new(     4*8), BlockQueue::new(     5*8), BlockQueue::new(     6*8), BlockQueue::new(     7*8), BlockQueue::new(     8), /* 8 */ 
+    BlockQueue::new(    10*8), BlockQueue::new(    12*8), BlockQueue::new(    14*8), BlockQueue::new(    16*8), BlockQueue::new(    20*8), BlockQueue::new(    24*8), BlockQueue::new(    28*8), BlockQueue::new(    32), /* 16 */ 
+    BlockQueue::new(    40*8), BlockQueue::new(    48*8), BlockQueue::new(    56*8), BlockQueue::new(    64*8), BlockQueue::new(    80*8), BlockQueue::new(    96*8), BlockQueue::new(   112*8), BlockQueue::new(   128), /* 24 */ 
+    BlockQueue::new(   160*8), BlockQueue::new(   192*8), BlockQueue::new(   224*8), BlockQueue::new(   256*8), BlockQueue::new(   320*8), BlockQueue::new(   384*8), BlockQueue::new(   448*8), BlockQueue::new(   512), /* 32 */ 
+    BlockQueue::new(   640*8), BlockQueue::new(   768*8), BlockQueue::new(   896*8), BlockQueue::new(  1024*8), BlockQueue::new(  1280*8), BlockQueue::new(  1536*8), BlockQueue::new(  1792*8), BlockQueue::new(  2048), /* 40 */ 
+    BlockQueue::new(  2560*8), BlockQueue::new(  3072*8), BlockQueue::new(  3584*8), BlockQueue::new(  4096*8), BlockQueue::new(  5120*8), BlockQueue::new(  6144*8), BlockQueue::new(  7168*8), BlockQueue::new(  8192), /* 48 */ 
+    BlockQueue::new( 10240*8), BlockQueue::new( 12288*8), BlockQueue::new( 14336*8), BlockQueue::new( 16384*8), BlockQueue::new( 20480*8), BlockQueue::new( 24576*8), BlockQueue::new( 28672*8), BlockQueue::new( 32768), /* 56 */ 
+    BlockQueue::new( 40960*8), BlockQueue::new( 49152*8), BlockQueue::new( 57344*8), BlockQueue::new( 65536*8), BlockQueue::new( 81920*8), BlockQueue::new( 98304*8), BlockQueue::new(114688*8), BlockQueue::new(131072), /* 64 */ 
+    BlockQueue::new(163840*8), BlockQueue::new(196608*8), BlockQueue::new(229376*8), BlockQueue::new(262144*8), BlockQueue::new(327680*8), BlockQueue::new(393216*8), BlockQueue::new(458752*8), BlockQueue::new(524288), /* 72 */ 
+    BlockQueue::new(MI_LARGE_OBJ_WSIZE_MAX + 1  /* 655360, Huge queue */),
+];
+
 
 pub struct FreeListAllocator<VM: VMBinding> {
     pub tls: VMThread,
     space: &'static MarkSweepSpace<VM>,
     plan: &'static dyn Plan<VM = VM>,
-    blocks_direct: Vec<Address>,
-    blocks: Vec<Address>,
+    blocks: Vec<BlockQueue>, // = pages in mimalloc
   }
+  #[derive(Clone, Copy, Debug)]
+pub struct BlockQueue {
+    first: Address,
+    size: usize,
+}
+
+impl BlockQueue {
+    const fn new(size: usize) -> BlockQueue {
+        BlockQueue {
+            first: unsafe { Address::zero() },
+            size,
+        }
+    }
+}
   
 #[derive(Clone, Copy, Debug)]
 struct BlockData {
@@ -46,13 +75,10 @@ impl<VM: VMBinding> Allocator<VM> for FreeListAllocator<VM> {
     }
 
     fn alloc(&mut self, size: usize, align: usize, offset: isize) -> Address {
-        // let mut s = 0;
-        // loop {
-        //     eprintln!("size = {}, bin = {}", s, FreeListAllocator::<VM>::_mi_bin(s));
-        //     s += 1;
-        //     if s > MI_LARGE_OBJ_SIZE_MAX {panic!()}
-        // }
-        let block_data_address = self.blocks_direct[size - 1];
+        trace!("Free List Allocator: allocation request for {} bytes", size);
+        let bin = FreeListAllocator::<VM>::mi_bin(size);
+        let block_queue = &self.blocks[bin as usize];
+        let block_data_address = block_queue.first;
         if unsafe { block_data_address == Address::zero() } {
             // no block for this size, go to slow path
             return self.alloc_slow_once(size, align, offset);
@@ -68,7 +94,10 @@ impl<VM: VMBinding> Allocator<VM> for FreeListAllocator<VM> {
     }
 
     fn alloc_slow_once(&mut self, size: usize, align: usize, offset: isize) -> Address {
-        let block = self.acquire_block_for_size(size + size_of::<Address>());
+        let bin = FreeListAllocator::<VM>::mi_bin(size);
+        let req_size = self.blocks[bin as usize].size;
+        // let block_queue = self.blocks.get_mut(bin as usize).unwrap();
+        let block = self.acquire_block_for_size(size);
         let block_data_address = block + BYTES_IN_BLOCK - size_of::<BlockData>();
         let cell = FreeListAllocator::<VM>::attempt_alloc_to_block(block_data_address);
         trace!("Free list allocator: slow alloc to {}", cell);
@@ -88,8 +117,8 @@ impl<VM: VMBinding> FreeListAllocator<VM> {
             tls,
             space,
             plan,
-            blocks_direct: vec![unsafe{ Address::zero() }; 256],
-            blocks: vec![unsafe{ Address::zero() }; 256],
+            blocks: BLOCK_QUEUES_EMPTY.to_vec(),
+            //vec![unsafe{ Address::zero() }; MI_BIN_FULL + 1],
         };
         allocator
     }
@@ -97,19 +126,21 @@ impl<VM: VMBinding> FreeListAllocator<VM> {
     pub fn acquire_block_for_size(&mut self, size: usize) -> Address {
         let block = self.acquire_block();
         let block_data_address = block + BYTES_IN_BLOCK - size_of::<BlockData>();
+        // let size = block_queue.size;
         let mut old_cell = block;
         let mut new_cell = block + size;
         let final_cell = loop {
             unsafe {
-                (new_cell + size - size_of::<Address>()).store::<Address>(old_cell);
+                new_cell.store::<Address>(old_cell);
                 // trace!("Store {} at {}", old_cell, new_cell + size - size_of::<Address>());
             }
             old_cell = new_cell;
             new_cell = old_cell + size;
             if new_cell + size >= block_data_address {break old_cell};
         };
+        let block_queue = self.blocks.get_mut(FreeListAllocator::<VM>::mi_bin(size) as usize).unwrap();
         let block_data = BlockData {
-            next: self.blocks_direct[ size - 1 - size_of::<Address>()],
+            next: block_queue.first,
             free: final_cell,
             size,
         };
@@ -118,8 +149,8 @@ impl<VM: VMBinding> FreeListAllocator<VM> {
             block_data_address.store::<BlockData>(block_data);
         };
 
-        self.blocks_direct[size - 1 - size_of::<Address>()] = block_data_address;
-
+        // self.blocks[size - 1 - size_of::<Address>()] = block_data_address;
+        block_queue.first = block_data_address;
         trace!("Constructed free list for block starting at {}", block);
         block
     }
@@ -138,7 +169,7 @@ impl<VM: VMBinding> FreeListAllocator<VM> {
             let block_data = unsafe { block_data_address.load::<BlockData>() };
             return FreeListAllocator::<VM>::attempt_alloc_to_block(block_data_address);
         };
-        let next_cell = unsafe { (cell + block_data.size - size_of::<Address>()).load::<Address>() };
+        let next_cell = unsafe { cell.load::<Address>() };
         // trace!("Load {} from {}", next_cell, (cell + block_data.size - size_of::<Address>()));
         block_data.free = next_cell;
         unsafe { block_data_address.store::<BlockData>(block_data) };
@@ -162,16 +193,16 @@ impl<VM: VMBinding> FreeListAllocator<VM> {
     }
 
 
-    fn _mi_wsize_from_size(size: usize) -> usize {
+    fn mi_wsize_from_size(size: usize) -> usize {
         // Align a byte size to a size in machine words
         // i.e. byte size == `wsize*sizeof(void*)`
         // adapted from _mi_wsize_from_size in mimalloc
         (size + size_of::<u32>() - 1) / size_of::<u32>()
     }
 
-    fn _mi_bin(size: usize) -> u8 {
+    fn mi_bin(size: usize) -> u8 {
         // adapted from _mi_bin in mimalloc
-        let mut wsize: usize = FreeListAllocator::<VM>::_mi_wsize_from_size(size);
+        let mut wsize: usize = FreeListAllocator::<VM>::mi_wsize_from_size(size);
         let bin: u8;
         if wsize <= 1 {
             bin = 1;
