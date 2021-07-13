9b3f056a42c1b0afce70130e6b76fed6936a7353
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-07-13 13:10:40 +1000

    Add vm_release call

--- src/scheduler/gc_work.rs
+++ src/scheduler/gc_work.rs
@@ -120,6 +120,7 @@ impl<P: Plan, W: CopyContext + WorkerLocal> Release<P, W> {
 impl<P: Plan, W: CopyContext + WorkerLocal> GCWork<P::VM> for Release<P, W> {
     fn do_work(&mut self, worker: &mut GCWorker<P::VM>, mmtk: &'static MMTK<P::VM>) {
         trace!("Release Global");
+        <P::VM as VMBinding>::VMCollection::vm_release();
         // We assume this is the only running work packet that accesses plan at the point of execution
         #[allow(clippy::cast_ref_to_mut)]
         let plan_mut: &mut P = unsafe { &mut *(self.plan as *const _ as *mut _) };
