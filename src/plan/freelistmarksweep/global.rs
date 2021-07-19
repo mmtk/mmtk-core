f15294c6529d4644a03ff4a1ad9cc20feb212734
paigereeves <paigereeves006@gmail.com>
2021-07-19 17:26:38 +1000

    add thread free list, define free, refactor metadataspec access, add flms plan

--- src/plan/freelistmarksweep/global.rs
+++ src/plan/freelistmarksweep/global.rs
@@ -1,15 +1,15 @@
 use crate::mmtk::MMTK;
+use crate::plan::global::{BasePlan, NoCopy};
 use crate::plan::freelistmarksweep::mutator::ALLOCATOR_MAPPING;
-use crate::plan::global::{BasePlan, CommonPlan, NoCopy};
+use crate::plan::AllocationSemantics;
 use crate::plan::Plan;
 use crate::plan::PlanConstraints;
-use crate::plan::{AllocationSemantics, GcStatus};
 use crate::policy::immortalspace::ImmortalSpace;
 use crate::policy::marksweepspace::MarkSweepSpace;
-use crate::policy::space::{CommonSpace, Space, SpaceOptions};
-use crate::scheduler::gc_work::{EndOfGC, Prepare, Release, StopMutators};
-use crate::scheduler::{GCWorkScheduler, GCWorkerLocalPtr};
-use crate::scheduler::{GCWorkerLocal, WorkBucketStage};
+use crate::policy::space::Space;
+use crate::scheduler::GCWorkerLocal;
+use crate::scheduler::GCWorkerLocalPtr;
+use crate::scheduler::MMTkScheduler;
 use crate::util::alloc::allocators::AllocatorSelector;
 use crate::util::heap::layout::heap_layout::Mmapper;
 use crate::util::heap::layout::heap_layout::VMMap;
@@ -17,22 +17,17 @@ use crate::util::heap::layout::vm_layout_constants::{HEAP_END, HEAP_START};
 use crate::util::heap::HeapMeta;
 #[allow(unused_imports)]
 use crate::util::heap::VMRequest;
-use crate::util::metadata::side_metadata::{
-    SideMetadataContext, SideMetadataSanity, SideMetadataSpec, LOCAL_SIDE_METADATA_BASE_ADDRESS,
-};
+use crate::util::metadata::side_metadata::{LOCAL_SIDE_METADATA_BASE_ADDRESS, SideMetadataContext, SideMetadataSanity, SideMetadataSpec, metadata_address_range_size};
 use crate::util::opaque_pointer::*;
 use crate::util::options::UnsafeOptionsWrapper;
-#[cfg(feature = "sanity")]
-use crate::util::sanity::sanity_checker::ScheduleSanityGC;
 use crate::vm::VMBinding;
 use enum_map::EnumMap;
 use std::sync::Arc;
 
-use super::gc_work::FLMSProcessEdges;
-
 pub struct FreeListMarkSweep<VM: VMBinding> {
-    pub common: CommonPlan<VM>,
+    pub base: BasePlan<VM>,
     pub ms_space: MarkSweepSpace<VM>,
+    pub im_space: ImmortalSpace<VM>,
 }
 
 pub const FLMS_CONSTRAINTS: PlanConstraints = PlanConstraints::default();
@@ -58,61 +53,49 @@ impl<VM: VMBinding> Plan for FreeListMarkSweep<VM> {
         &mut self,
         heap_size: usize,
         vm_map: &'static VMMap,
-        scheduler: &Arc<GCWorkScheduler<VM>>,
+        scheduler: &Arc<MMTkScheduler<VM>>,
     ) {
-        self.common.gc_init(heap_size, vm_map, scheduler);
+        self.base.gc_init(heap_size, vm_map, scheduler);
 
         // FIXME correctly initialize spaces based on options
         self.ms_space.init(&vm_map);
+        self.im_space.init(&vm_map);
     }
 
     fn collection_required(&self, space_full: bool, space: &dyn Space<Self::VM>) -> bool {
-        self.common.base.collection_required(self, space_full, space)
+        self.base.collection_required(self, space_full, space)
     }
 
     fn base(&self) -> &BasePlan<VM> {
-        &self.common.base
+        &self.base
     }
 
-    fn prepare(&mut self, tls: VMWorkerThread) {
-        self.common.prepare(tls, true);
-        self.ms_space.reset();
+    fn prepare(&mut self, _tls: VMWorkerThread) {
+        unreachable!()
     }
 
     fn release(&mut self, tls: VMWorkerThread) {
-        self.ms_space.block_level_sweep();
-        // self.ms_space.eager_sweep(tls);
+        self.ms_space.sweep(tls);
     }
 
     fn get_allocator_mapping(&self) -> &'static EnumMap<AllocationSemantics, AllocatorSelector> {
         &*ALLOCATOR_MAPPING
     }
 
-    fn schedule_collection(&'static self, scheduler: &GCWorkScheduler<VM>) {
-        self.base().set_collection_kind();
-        self.base().set_gc_status(GcStatus::GcPrepare);
-        // Stop & scan mutators (mutator scanning can happen before STW)
-        scheduler.work_buckets[WorkBucketStage::Unconstrained]
-            .add(StopMutators::<FLMSProcessEdges<VM>>::new());
-        // Prepare global/collectors/mutators
-        scheduler.work_buckets[WorkBucketStage::Prepare]
-            .add(Prepare::<Self, NoCopy<VM>>::new(self));
-        // Release global/collectors/mutators
-        scheduler.work_buckets[WorkBucketStage::Release]
-            .add(Release::<Self, NoCopy<VM>>::new(self));
-        // Resume mutators
-        #[cfg(feature = "sanity")]
-        scheduler.work_buckets[WorkBucketStage::Final]
-            .add(ScheduleSanityGC::<Self, NoCopy<VM>>::new(self));
-        scheduler.set_finalizer(Some(EndOfGC));
+    fn schedule_collection(&'static self, _scheduler: &MMTkScheduler<VM>) {
+        unreachable!("GC triggered in freelistmarksweep")
     }
 
     fn get_pages_used(&self) -> usize {
-        self.common.get_pages_used() + self.ms_space.reserved_pages()
+        self.im_space.reserved_pages() + self.ms_space.reserved_pages()
     }
 
-    fn common(&self) -> &CommonPlan<VM> {
-        &self.common
+    fn handle_user_collection_request(&self, _tls: VMMutatorThread, _force: bool) {
+        println!("Warning: User attempted a collection request, but it is not supported in FreeListMarkSweep. The request is ignored.");
+    }
+
+    fn poll(&self, space_full: bool, space: &dyn Space<Self::VM>) -> bool {
+        false
     }
 }
 
@@ -121,60 +104,98 @@ impl<VM: VMBinding> FreeListMarkSweep<VM> {
         vm_map: &'static VMMap,
         mmapper: &'static Mmapper,
         options: Arc<UnsafeOptionsWrapper>,
-        scheduler: Arc<GCWorkScheduler<VM>>,
     ) -> Self {
         #[cfg(not(feature = "freelistmarksweep_lock_free"))]
         let mut heap = HeapMeta::new(HEAP_START, HEAP_END);
         #[cfg(feature = "freelistmarksweep_lock_free")]
         let heap = HeapMeta::new(HEAP_START, HEAP_END);
+        let side_metadata_next = SideMetadataSpec {
+            is_global: false,
+            offset: LOCAL_SIDE_METADATA_BASE_ADDRESS.as_usize(),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
+        let side_metadata_free = SideMetadataSpec {
+            is_global: false,
+            offset: metadata_address_range_size(&side_metadata_next),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
+        let side_metadata_size = SideMetadataSpec {
+            is_global: false,
+            offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
+        let side_metadata_local_free = SideMetadataSpec {
+            is_global: false,
+            offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free) + metadata_address_range_size(&side_metadata_size),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
+        let side_metadata_thread_free = SideMetadataSpec {
+            is_global: false,
+            offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free) + metadata_address_range_size(&side_metadata_size) + metadata_address_range_size(&side_metadata_local_free),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
+        // let side_metadata_tls = SideMetadataSpec {
+        //     is_global: false,
+        //     offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free) + metadata_address_range_size(&side_metadata_size) + metadata_address_range_size(&side_metadata_local_free) + metadata_address_range_size(&side_metadata_thread_free),
+        //     log_num_of_bits: 6,
+        //     log_min_obj_size: 16,
+        // };
+        let local_specs = {
+            vec![
+                side_metadata_next,
+                side_metadata_free,
+                side_metadata_size,
+                side_metadata_local_free,
+                side_metadata_thread_free,
+            ]
+        };
         let ms_space = MarkSweepSpace::new(
             "MSspace",
             true,
             VMRequest::discontiguous(),
-            // local_specs.clone(),
+            local_specs.clone(),
             vm_map,
             mmapper,
             &mut heap,
-            scheduler,
         );
         let global_specs = SideMetadataContext::new_global_specs(&[]);
 
-        // let im_space = ImmortalSpace::new(
-        //     "IMspace",
-        //     true,
-        //     VMRequest::discontiguous(),
-        //     global_specs.clone(),
-        //     vm_map,
-        //     mmapper,
-        //     &mut heap,
-        //     &FLMS_CONSTRAINTS,
-        // );
-
-        let common =             CommonPlan::new(
+        let im_space = ImmortalSpace::new(
+            "IMspace",
+            true,
+            VMRequest::discontiguous(),
+            global_specs.clone(),
             vm_map,
             mmapper,
-            options,
-            heap,
+            &mut heap,
             &FLMS_CONSTRAINTS,
-            global_specs,
         );
 
         let res = FreeListMarkSweep {
-            common,
+            im_space,
             ms_space,
+            base: BasePlan::new(
+                vm_map,
+                mmapper,
+                options,
+                heap,
+                &FLMS_CONSTRAINTS,
+                global_specs,
+            ),
         };
 
         let mut side_metadata_sanity_checker = SideMetadataSanity::new();
-        res.common
+        res.base
             .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
         res.ms_space
             .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
-        // res.im_space
-        //     .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
+        res.im_space
+            .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
         res
-    }
-
-    pub fn ms_space(&self) -> &MarkSweepSpace<VM> {
-        &self.ms_space
     }
 }
--- src/plan/freelistmarksweep/mutator.rs
+++ src/plan/freelistmarksweep/mutator.rs
@@ -1,13 +1,10 @@
 use crate::plan::barriers::NoBarrier;
-use crate::plan::freelistmarksweep::FreeListMarkSweep;
 use crate::plan::mutator_context::Mutator;
 use crate::plan::mutator_context::MutatorConfig;
+use crate::plan::freelistmarksweep::FreeListMarkSweep;
 use crate::plan::AllocationSemantics as AllocationType;
 use crate::plan::Plan;
-use crate::policy::marksweepspace::MarkSweepSpace;
 use crate::util::alloc::allocators::{AllocatorSelector, Allocators};
-use crate::util::alloc::Allocator;
-use crate::util::alloc::FreeListAllocator;
 use crate::util::{VMMutatorThread, VMWorkerThread};
 use crate::vm::VMBinding;
 use enum_map::enum_map;
@@ -16,28 +13,12 @@ use enum_map::EnumMap;
 lazy_static! {
     pub static ref ALLOCATOR_MAPPING: EnumMap<AllocationType, AllocatorSelector> = enum_map! {
         AllocationType::Default | AllocationType::Code | AllocationType::ReadOnly => AllocatorSelector::FreeList(0),
-        AllocationType::Los | AllocationType::Immortal | AllocationType::LargeCode => AllocatorSelector::BumpPointer(0),
+        AllocationType::Los | AllocationType::Immortal => AllocatorSelector::BumpPointer(0),
     };
 }
-pub fn flms_mutator_prepare<VM: VMBinding>(mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
 
-}
-
-pub fn flms_mutator_release<VM: VMBinding>(mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
-    let free_list_allocator = unsafe {
-        mutator
-            .allocators
-            .get_allocator_mut(mutator.config.allocator_mapping[AllocationType::Default])
-    }
-    .downcast_mut::<FreeListAllocator<VM>>()
-    .unwrap();
-    free_list_allocator.rebind(
-        mutator
-            .plan
-            .downcast_ref::<FreeListMarkSweep<VM>>()
-            .unwrap()
-            .ms_space(),
-    )
+pub fn freelistmarksweep_mutator_noop<VM: VMBinding>(_mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
+    unreachable!();
 }
 
 pub fn create_freelistmarksweep_mutator<VM: VMBinding>(
@@ -46,25 +27,16 @@ pub fn create_freelistmarksweep_mutator<VM: VMBinding>(
 ) -> Mutator<VM> {
     let config = MutatorConfig {
         allocator_mapping: &*ALLOCATOR_MAPPING,
-        space_mapping: box vec![
-            (
-                AllocatorSelector::FreeList(0),
-                &plan
-                    .downcast_ref::<FreeListMarkSweep<VM>>()
-                    .unwrap()
-                    .ms_space,
-            ),
-            (
-                AllocatorSelector::BumpPointer(0),
-                plan
-                    .downcast_ref::<FreeListMarkSweep<VM>>()
-                    .unwrap()
-                    .common()
-                    .get_immortal(),
-            ),
-        ],
-        prepare_func: &flms_mutator_prepare,
-        release_func: &flms_mutator_release,
+        space_mapping: box vec![(
+            AllocatorSelector::FreeList(0),
+            &plan.downcast_ref::<FreeListMarkSweep<VM>>().unwrap().ms_space,
+        ),
+        (
+            AllocatorSelector::BumpPointer(0),
+            &plan.downcast_ref::<FreeListMarkSweep<VM>>().unwrap().im_space,
+        )],
+        prepare_func: &freelistmarksweep_mutator_noop,
+        release_func: &freelistmarksweep_mutator_noop,
     };
     Mutator {
         allocators: Allocators::<VM>::new(mutator_tls, plan, &config.space_mapping),
