472e54402f71cf60c7594a3d59231720786ee06c
paigereeves <paigereeves006@gmail.com>
2021-07-20 13:28:43 +1000

    tls metadata on the side, some progress on marksweep eager gc

--- src/plan/freelistmarksweep/global.rs
+++ src/plan/freelistmarksweep/global.rs
@@ -1,13 +1,14 @@
 use crate::mmtk::MMTK;
 use crate::plan::global::{BasePlan, NoCopy};
 use crate::plan::freelistmarksweep::mutator::ALLOCATOR_MAPPING;
-use crate::plan::AllocationSemantics;
+use crate::plan::{AllocationSemantics, GcStatus};
 use crate::plan::Plan;
 use crate::plan::PlanConstraints;
 use crate::policy::immortalspace::ImmortalSpace;
 use crate::policy::marksweepspace::MarkSweepSpace;
 use crate::policy::space::Space;
-use crate::scheduler::GCWorkerLocal;
+use crate::scheduler::gc_work::{EndOfGC, Prepare, Release, StopMutators};
+use crate::scheduler::{GCWorkerLocal, WorkBucketStage};
 use crate::scheduler::GCWorkerLocalPtr;
 use crate::scheduler::MMTkScheduler;
 use crate::util::alloc::allocators::AllocatorSelector;
@@ -20,9 +21,13 @@ use crate::util::heap::VMRequest;
 use crate::util::metadata::side_metadata::{LOCAL_SIDE_METADATA_BASE_ADDRESS, SideMetadataContext, SideMetadataSanity, SideMetadataSpec, metadata_address_range_size};
 use crate::util::opaque_pointer::*;
 use crate::util::options::UnsafeOptionsWrapper;
+#[cfg(feature = "sanity")]
+use crate::util::sanity::sanity_checker::ScheduleSanityGC;
 use crate::vm::VMBinding;
 use enum_map::EnumMap;
 use std::sync::Arc;
+
+use super::gc_work::FLMSProcessEdges;
 
 pub struct FreeListMarkSweep<VM: VMBinding> {
     pub base: BasePlan<VM>,
@@ -71,31 +76,38 @@ impl<VM: VMBinding> Plan for FreeListMarkSweep<VM> {
     }
 
     fn prepare(&mut self, _tls: VMWorkerThread) {
-        unreachable!()
+        self.im_space.prepare();
     }
 
     fn release(&mut self, tls: VMWorkerThread) {
-        self.ms_space.sweep(tls);
+        self.ms_space.eager_sweep(tls);
     }
 
     fn get_allocator_mapping(&self) -> &'static EnumMap<AllocationSemantics, AllocatorSelector> {
         &*ALLOCATOR_MAPPING
     }
 
-    fn schedule_collection(&'static self, _scheduler: &MMTkScheduler<VM>) {
-        unreachable!("GC triggered in freelistmarksweep")
+    fn schedule_collection(&'static self, scheduler: &MMTkScheduler<VM>) {
+        self.base().set_collection_kind();
+        self.base().set_gc_status(GcStatus::GcPrepare);
+        // Stop & scan mutators (mutator scanning can happen before STW)
+        scheduler.work_buckets[WorkBucketStage::Unconstrained]
+            .add(StopMutators::<FLMSProcessEdges<VM>>::new());
+        // Prepare global/collectors/mutators
+        scheduler.work_buckets[WorkBucketStage::Prepare]
+            .add(Prepare::<Self, NoCopy<VM>>::new(self));
+        // Release global/collectors/mutators
+        scheduler.work_buckets[WorkBucketStage::Release]
+            .add(Release::<Self, NoCopy<VM>>::new(self));
+        // Resume mutators
+        #[cfg(feature = "sanity")]
+        scheduler.work_buckets[WorkBucketStage::Final]
+            .add(ScheduleSanityGC::<Self, NoCopy<VM>>::new(self));
+        scheduler.set_finalizer(Some(EndOfGC));
     }
 
     fn get_pages_used(&self) -> usize {
         self.im_space.reserved_pages() + self.ms_space.reserved_pages()
-    }
-
-    fn handle_user_collection_request(&self, _tls: VMMutatorThread, _force: bool) {
-        println!("Warning: User attempted a collection request, but it is not supported in FreeListMarkSweep. The request is ignored.");
-    }
-
-    fn poll(&self, space_full: bool, space: &dyn Space<Self::VM>) -> bool {
-        false
     }
 }
 
@@ -139,12 +151,12 @@ impl<VM: VMBinding> FreeListMarkSweep<VM> {
             log_num_of_bits: 6,
             log_min_obj_size: 16,
         };
-        // let side_metadata_tls = SideMetadataSpec {
-        //     is_global: false,
-        //     offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free) + metadata_address_range_size(&side_metadata_size) + metadata_address_range_size(&side_metadata_local_free) + metadata_address_range_size(&side_metadata_thread_free),
-        //     log_num_of_bits: 6,
-        //     log_min_obj_size: 16,
-        // };
+        let side_metadata_tls = SideMetadataSpec {
+            is_global: false,
+            offset: metadata_address_range_size(&side_metadata_next) + metadata_address_range_size(&side_metadata_free) + metadata_address_range_size(&side_metadata_size) + metadata_address_range_size(&side_metadata_local_free) + metadata_address_range_size(&side_metadata_thread_free),
+            log_num_of_bits: 6,
+            log_min_obj_size: 16,
+        };
         let local_specs = {
             vec![
                 side_metadata_next,
@@ -152,6 +164,7 @@ impl<VM: VMBinding> FreeListMarkSweep<VM> {
                 side_metadata_size,
                 side_metadata_local_free,
                 side_metadata_thread_free,
+                side_metadata_tls,
             ]
         };
         let ms_space = MarkSweepSpace::new(
