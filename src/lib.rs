306e6b91e6591dac5783d86e1a73e6b06792dcc8
paigereeves <paigereeves006@gmail.com>
2021-08-01 20:16:52 +1000

    begin using chunkmap from immix

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@
 #![feature(const_panic)]
 #![feature(const_fn_union)]
 #![feature(step_trait)]
+#![feature(const_generics)]
+#![feature(const_fn_union)]
 #![feature(step_trait_ext)]
 // TODO: We should fix missing docs for public items and turn this on (Issue #309).
 // #![deny(missing_docs)]
