093da769a71067dcd4f37db6f453213e7dace660
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-08-09 20:57:19 +1000

    Add Immix GC (#382)

--- src/plan/transitive_closure.rs
+++ src/plan/transitive_closure.rs
@@ -1,7 +1,11 @@
 //! The fundamental mechanism for performing a transitive closure over an object graph.
 
+use std::mem;
+
 use crate::scheduler::gc_work::ProcessEdgesWork;
+use crate::scheduler::{GCWorker, WorkBucketStage};
 use crate::util::{Address, ObjectReference};
+use crate::MMTK;
 
 /// This trait is the fundamental mechanism for performing a
 /// transitive closure over an object graph.
@@ -20,5 +24,59 @@ impl<T: ProcessEdgesWork> TransitiveClosure for T {
     #[inline]
     fn process_node(&mut self, object: ObjectReference) {
         ProcessEdgesWork::process_node(self, object);
+    }
+}
+
+/// A transitive closure visitor to collect all the edges of an object.
+pub struct ObjectsClosure<'a, E: ProcessEdgesWork> {
+    mmtk: &'static MMTK<E::VM>,
+    buffer: Vec<Address>,
+    worker: &'a mut GCWorker<E::VM>,
+}
+
+impl<'a, E: ProcessEdgesWork> ObjectsClosure<'a, E> {
+    pub fn new(
+        mmtk: &'static MMTK<E::VM>,
+        buffer: Vec<Address>,
+        worker: &'a mut GCWorker<E::VM>,
+    ) -> Self {
+        Self {
+            mmtk,
+            buffer,
+            worker,
+        }
+    }
+}
+
+impl<'a, E: ProcessEdgesWork> TransitiveClosure for ObjectsClosure<'a, E> {
+    #[inline(always)]
+    fn process_edge(&mut self, slot: Address) {
+        if self.buffer.is_empty() {
+            self.buffer.reserve(E::CAPACITY);
+        }
+        self.buffer.push(slot);
+        if self.buffer.len() >= E::CAPACITY {
+            let mut new_edges = Vec::new();
+            mem::swap(&mut new_edges, &mut self.buffer);
+            self.worker.add_work(
+                WorkBucketStage::Closure,
+                E::new(new_edges, false, self.mmtk),
+            );
+        }
+    }
+    fn process_node(&mut self, _object: ObjectReference) {
+        unreachable!()
+    }
+}
+
+impl<'a, E: ProcessEdgesWork> Drop for ObjectsClosure<'a, E> {
+    #[inline(always)]
+    fn drop(&mut self) {
+        let mut new_edges = Vec::new();
+        mem::swap(&mut new_edges, &mut self.buffer);
+        self.worker.add_work(
+            WorkBucketStage::Closure,
+            E::new(new_edges, false, self.mmtk),
+        );
     }
 }
---
+++ src/policy/immix/mod.rs
@@ -0,0 +1,45 @@
+pub mod block;
+pub mod chunk;
+pub mod defrag;
+pub mod immixspace;
+pub mod line;
+
+pub use immixspace::*;
+
+use crate::{
+    policy::immix::block::Block,
+    util::metadata::side_metadata::{SideMetadataOffset, SideMetadataSpec},
+};
+
+use self::chunk::ChunkMap;
+
+use super::mallocspace::metadata::ACTIVE_PAGE_METADATA_SPEC;
+
+/// Mark/sweep memory for block-level only
+pub const BLOCK_ONLY: bool = false;
+
+/// Opportunistic copying
+pub const DEFRAG: bool = true;
+
+/// Mark lines when scanning objects.
+/// Otherwise, do it at mark time.
+pub const MARK_LINE_AT_SCAN_TIME: bool = true;
+
+macro_rules! validate {
+    ($x: expr) => { assert!($x, stringify!($x)) };
+    ($x: expr => $y: expr) => { if $x { assert!($y, stringify!($x implies $y)) } };
+}
+
+fn validate_features() {
+    // Block-only immix cannot do defragmentation
+    validate!(DEFRAG => !BLOCK_ONLY);
+    // Number of lines in a block should not exceed BlockState::MARK_MARKED
+    assert!(Block::LINES / 2 <= u8::MAX as usize - 2);
+}
+
+/// The start of immix side metadata is after the last MallocSpace side metadata.
+const IMMIX_LOCAL_SIDE_METADATA_BASE_OFFSET: SideMetadataOffset =
+    SideMetadataOffset::layout_after(&ACTIVE_PAGE_METADATA_SPEC);
+
+/// Immix's Last local side metadata. Used to calculate `LOCAL_SIDE_METADATA_VM_BASE_OFFSET`.
+pub const LAST_LOCAL_SIDE_METADATA: SideMetadataSpec = ChunkMap::ALLOC_TABLE;
