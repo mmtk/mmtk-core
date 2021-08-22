b84920d546ce48889cccb4a1e2b675a0c594c19f
paigereeves <paigereeves006@gmail.com>
2021-08-22 13:35:47 +1000

    added assertions and removed dead code. null block bug in acquire_block_for_size not fixed.

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -410,6 +410,7 @@ pub trait Space<VM: VMBinding>: 'static + SFT + Sync + Downcast {
             pr.clear_request(pages_reserved);
             drop(lock); // drop the lock before block
             VM::VMCollection::block_for_gc(VMMutatorThread(tls)); // We have checked that this is mutator
+            eprintln!("gc");
             unsafe { Address::zero() }
         } else {
             debug!("Collection not required");
@@ -490,9 +491,7 @@ pub trait Space<VM: VMBinding>: 'static + SFT + Sync + Downcast {
                     unsafe { Address::zero() }
                 }
             }
-        };
-        // eprintln!("done acquiring space");
-        a
+        }
     }
 
     fn address_in_space(&self, start: Address) -> bool {
