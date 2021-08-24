3a1f08e032d7386875f3fb23b0f712d2da5f1dfa
YiluoWei <39235878+YiluoWei@users.noreply.github.com>
2021-08-25 01:52:31 +1000

    make GLOBAL_SIDE_METADATA_BASE_ADDRESS public

--- src/util/metadata/side_metadata/constants.rs
+++ src/util/metadata/side_metadata/constants.rs
@@ -17,7 +17,7 @@ use super::metadata_address_range_size;
 pub(crate) const GLOBAL_SIDE_METADATA_BASE_ADDRESS: Address =
     unsafe { Address::from_usize(BYTES_IN_CHUNK) };
 #[cfg(target_pointer_width = "64")]
-pub(crate) const GLOBAL_SIDE_METADATA_BASE_ADDRESS: Address =
+pub const GLOBAL_SIDE_METADATA_BASE_ADDRESS: Address =
     unsafe { Address::from_usize(0x0000_0600_0000_0000usize) };
 
 pub(crate) const GLOBAL_SIDE_METADATA_BASE_OFFSET: SideMetadataOffset =
