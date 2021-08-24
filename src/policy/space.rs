7b33375bf6d5af9fa8077e95ed2f11c9bc3d1323
paigereeves <paigereeves006@gmail.com>
2021-08-24 18:15:44 +1000

    remove big in alloc_slow_once, use is_zero

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -410,7 +410,6 @@ pub trait Space<VM: VMBinding>: 'static + SFT + Sync + Downcast {
             pr.clear_request(pages_reserved);
             drop(lock); // drop the lock before block
             VM::VMCollection::block_for_gc(VMMutatorThread(tls)); // We have checked that this is mutator
-            eprintln!("gc");
             unsafe { Address::zero() }
         } else {
             debug!("Collection not required");
