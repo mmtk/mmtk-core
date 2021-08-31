d3da11410804b1c31e898e93da2334efdecb9705
Zixian Cai <u5937495@anu.edu.au>
2021-08-31 12:10:04 +1000

    Fix comment

--- src/policy/lockfreeimmortalspace.rs
+++ src/policy/lockfreeimmortalspace.rs
@@ -139,7 +139,7 @@ impl<VM: VMBinding> Space<VM> for LockFreeImmortalSpace<VM> {
     }
 
     /// We have to override the default implementation because
-    /// LockFreeImmortalSpace doesn't use side metadata
+    /// LockFreeImmortalSpace doesn't put metadata in a common space
     fn verify_side_metadata_sanity(&self, side_metadata_sanity_checker: &mut SideMetadataSanity) {
         side_metadata_sanity_checker
             .verify_metadata_context(std::any::type_name::<Self>(), &self.metadata)
