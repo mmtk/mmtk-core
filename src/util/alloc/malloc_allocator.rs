f64449f948a3821bd2a55728b3560a366b1e1761
Yiluo Wei <yiluow@rat.moma>
2021-09-15 01:40:52 +1000

    add align offset malloc

--- src/util/alloc/malloc_allocator.rs
+++ src/util/alloc/malloc_allocator.rs
@@ -35,7 +35,8 @@ impl<VM: VMBinding> Allocator<VM> for MallocAllocator<VM> {
     fn alloc_slow_once(&mut self, size: usize, align: usize, offset: isize) -> Address {
         // TODO: We currently ignore the offset field. This is wrong.
         // assert!(offset == 0);
-        assert!(align <= 16);
+        // assert!(align <= 16);
+        assert!(offset >= 0);
 
         #[cfg(feature = "analysis")]
         {
@@ -56,7 +57,7 @@ impl<VM: VMBinding> Allocator<VM> for MallocAllocator<VM> {
             }
         }
 
-        let ret = self.space.alloc(self.tls, size);
+        let ret = self.space.alloc(self.tls, size, align, offset);
 
         trace!(
             "MallocSpace.alloc size = {}, align = {}, offset = {}, res = {}",
--- src/util/malloc.rs
+++ src/util/malloc.rs
@@ -14,4 +14,64 @@ pub use hoard_sys::{calloc, free, malloc_usable_size};
     feature = "malloc_mimalloc",
     feature = "malloc_hoard",
 )))]
-pub use libc::{calloc, free, malloc_usable_size};
+pub use libc::{calloc, free, malloc_usable_size, posix_memalign};
+use crate::util::Address;
+use crate::util::constants::BYTES_IN_ADDRESS;
+use crate::util::memory;
+
+pub fn alloc(size: usize) -> Address {
+    let raw = unsafe { calloc(1, size) };
+    let address = Address::from_mut_ptr(raw);
+    address
+}
+
+#[cfg(not(any(
+    feature = "malloc_jemalloc",
+    feature = "malloc_mimalloc",
+    feature = "malloc_hoard",
+)))]
+pub fn align_alloc(size: usize, align: usize) -> Address {
+    let mut ptr = 0 as usize as *mut libc::c_void;
+    let ptr_ptr = std::ptr::addr_of_mut!(ptr);
+    let result = unsafe { posix_memalign(ptr_ptr, align, size) };
+    if result != 0 {
+        return Address::ZERO;
+    }
+    let address = Address::from_mut_ptr(ptr);
+    memory::zero(address, size);
+    address
+}
+
+#[cfg(not(any(
+    feature = "malloc_jemalloc",
+    feature = "malloc_mimalloc",
+    feature = "malloc_hoard",
+)))]
+pub fn align_offset_alloc(size: usize, align: usize, offset: isize) -> Address {
+    let actual_size = size + align + BYTES_IN_ADDRESS;
+    let address = align_alloc(actual_size, align);
+    if address.is_zero() {
+        return address;
+    }
+    let mod_offset = offset as usize % align;
+    let mut result = address + align - mod_offset;
+    if mod_offset + BYTES_IN_ADDRESS > align {
+        result += align;
+    }
+    let malloc_res_ptr: *mut usize = (result - BYTES_IN_ADDRESS).to_mut_ptr();
+    unsafe { *malloc_res_ptr = address.as_usize() };
+    result
+}
+
+pub fn offset_malloc_usable_size(address: Address) -> usize {
+    // let address = Address.from_mut_ptr(ptr);
+    let malloc_res_ptr: *mut usize = (address - BYTES_IN_ADDRESS).to_mut_ptr();
+    let malloc_res = unsafe { *malloc_res_ptr } as *mut libc::c_void;
+    unsafe { malloc_usable_size(malloc_res) }
+}
+
+pub fn offset_free(address: Address) {
+    let malloc_res_ptr: *mut usize = (address - BYTES_IN_ADDRESS).to_mut_ptr();
+    let malloc_res = unsafe { *malloc_res_ptr } as *mut libc::c_void;
+    unsafe { free(malloc_res) };
+}
