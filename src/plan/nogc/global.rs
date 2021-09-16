bb969b8733b9af2433517d229c218baf5f3c228f
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-09-16 13:31:02 +1000

    Fix nogc_lock_free

--- src/plan/nogc/global.rs
+++ src/plan/nogc/global.rs
@@ -111,7 +111,7 @@ impl<VM: VMBinding> NoGC<VM> {
         #[cfg(not(feature = "nogc_lock_free"))]
         let mut heap = HeapMeta::new(HEAP_START, HEAP_END);
         #[cfg(feature = "nogc_lock_free")]
-        let heap = HeapMeta::new(HEAP_START, HEAP_END);
+        let mut heap = HeapMeta::new(HEAP_START, HEAP_END);
 
         let global_specs = SideMetadataContext::new_global_specs(&[]);
 
