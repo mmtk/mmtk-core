12e0c6aa9c24990c561a1e115c77be4644e23556
Kunal Sareen <kunal.sareen@anu.edu.au>
2021-09-30 21:24:54 +1000

    Don't trace to-space objects for GenCopy

--- src/plan/generational/copying/gc_work.rs
+++ src/plan/generational/copying/gc_work.rs
@@ -97,17 +97,8 @@ impl<VM: VMBinding> ProcessEdgesWork for GenCopyMatureProcessEdges<VM> {
         }
         // Evacuate mature objects
         if self.gencopy().tospace().in_space(object) {
-            return self
-                .gencopy()
-                .tospace()
-                .trace_object::<Self, GenCopyCopyContext<VM>>(
-                    self,
-                    object,
-                    super::global::ALLOC_SS,
-                    unsafe { self.worker().local::<GenCopyCopyContext<VM>>() },
-                );
-        }
-        if self.gencopy().fromspace().in_space(object) {
+            return object;
+        } else if self.gencopy().fromspace().in_space(object) {
             return self
                 .gencopy()
                 .fromspace()
@@ -118,6 +109,7 @@ impl<VM: VMBinding> ProcessEdgesWork for GenCopyMatureProcessEdges<VM> {
                     unsafe { self.worker().local::<GenCopyCopyContext<VM>>() },
                 );
         }
+
         self.gencopy()
             .gen
             .trace_object_full_heap::<Self, GenCopyCopyContext<VM>>(self, object, unsafe {
