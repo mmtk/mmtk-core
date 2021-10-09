3226e5fb3c75f04ce61a065850d464ed9b0cabe5
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-09 21:32:00 +1100

    cleanup

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -141,11 +141,6 @@ impl<'a> SFTMap<'a> {
     }
 
     pub fn get(&self, address: Address) -> &'a dyn SFT {
-        debug_assert!(
-            address.chunk_index() < self.sft.len(),
-            "Invalid address {:?}",
-            address
-        );
         let res = self.sft[address.chunk_index()];
         if DEBUG_SFT {
             trace!(
