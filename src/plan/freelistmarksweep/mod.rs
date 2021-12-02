b980c60a9081d18bcc0642108e6d39b2b25f2857
paigereeves <paigereeves006@gmail.com>
2021-12-02 11:43:17 +1100

    remove freelistmarksweep plan

--- src/plan/freelistmarksweep/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Plan: nogc (allocation-only)
-
-mod gc_work;
-pub(super) mod global;
-pub(super) mod mutator;
-
-pub use self::global::FreeListMarkSweep;
-pub use self::global::FLMS_CONSTRAINTS;
--- src/plan/marksweep/global.rs
+++ src/plan/marksweep/global.rs
@@ -1,40 +1,47 @@
 use crate::mmtk::MMTK;
-use crate::plan::global::BasePlan;
-use crate::plan::global::CommonPlan;
-use crate::plan::global::GcStatus;
-use crate::plan::global::NoCopy;
-use crate::plan::marksweep::gc_work::{MSProcessEdges, MSSweepChunks};
 use crate::plan::marksweep::mutator::ALLOCATOR_MAPPING;
-use crate::plan::AllocationSemantics;
+use crate::plan::global::{BasePlan, CommonPlan, NoCopy};
 use crate::plan::Plan;
 use crate::plan::PlanConstraints;
-use crate::policy::mallocspace::metadata::ACTIVE_CHUNK_METADATA_SPEC;
 use crate::policy::mallocspace::MallocSpace;
-use crate::policy::space::Space;
-use crate::scheduler::gc_work::*;
-use crate::scheduler::*;
+use crate::policy::mallocspace::metadata::ACTIVE_CHUNK_METADATA_SPEC;
+use crate::plan::{AllocationSemantics, GcStatus};
+use crate::policy::immortalspace::ImmortalSpace;
+use crate::policy::marksweepspace::MarkSweepSpace;
+use crate::policy::space::{CommonSpace, Space, SpaceOptions};
+use crate::scheduler::gc_work::{EndOfGC, Prepare, Release, StopMutators, ProcessWeakRefs};
+use crate::scheduler::{GCWorkScheduler, GCWorkerLocalPtr};
+use crate::scheduler::{GCWorkerLocal, WorkBucketStage};
 use crate::util::alloc::allocators::AllocatorSelector;
-#[cfg(not(feature = "global_alloc_bit"))]
+use crate::util::alloc::free_list_allocator::BYTES_IN_BLOCK;
 use crate::util::alloc_bit::ALLOC_SIDE_METADATA_SPEC;
-#[cfg(feature = "analysis")]
-use crate::util::analysis::GcHookWork;
 use crate::util::heap::layout::heap_layout::Mmapper;
 use crate::util::heap::layout::heap_layout::VMMap;
 use crate::util::heap::layout::vm_layout_constants::{HEAP_END, HEAP_START};
 use crate::util::heap::HeapMeta;
-use crate::util::metadata::side_metadata::{SideMetadataContext, SideMetadataSanity};
+#[allow(unused_imports)]
+use crate::util::heap::VMRequest;
+use crate::util::metadata::side_metadata::{
+    SideMetadataContext, SideMetadataSanity, SideMetadataSpec, LOCAL_SIDE_METADATA_BASE_ADDRESS,
+};
+use crate::util::opaque_pointer::*;
 use crate::util::options::UnsafeOptionsWrapper;
 #[cfg(feature = "sanity")]
-use crate::util::sanity::sanity_checker::*;
-use crate::util::VMWorkerThread;
+use crate::util::sanity::sanity_checker::ScheduleSanityGC;
 use crate::vm::VMBinding;
+use enum_map::EnumMap;
 use std::sync::Arc;
 
-use enum_map::EnumMap;
+use super::gc_work::{MSProcessEdges};
 
+#[cfg(feature="malloc")]
+use super::gc_work::MSSweepChunks;
 pub struct MarkSweep<VM: VMBinding> {
     common: CommonPlan<VM>,
+    #[cfg(feature="malloc")]
     ms: MallocSpace<VM>,
+    #[cfg(not(feature="malloc"))]
+    ms: MarkSweepSpace<VM>,
 }
 
 pub const MS_CONSTRAINTS: PlanConstraints = PlanConstraints {
@@ -42,8 +49,15 @@ pub const MS_CONSTRAINTS: PlanConstraints = PlanConstraints {
     gc_header_bits: 2,
     gc_header_words: 0,
     num_specialized_scans: 1,
+    max_non_los_default_alloc_bytes: BYTES_IN_BLOCK,
+    max_non_los_copy_bytes: BYTES_IN_BLOCK,
+    needs_linear_scan: crate::util::constants::SUPPORT_CARD_SCANNING || crate::util::constants::LAZY_SWEEP,
+    needs_concurrent_workers: false,
+    generate_gc_trace: false,
     may_trace_duplicate_edges: true,
-    ..PlanConstraints::default()
+    needs_forward_after_liveness: false,
+    needs_log_bit: false,
+    barrier: crate::BarrierSelector::NoBarrier,
 };
 
 impl<VM: VMBinding> Plan for MarkSweep<VM> {
@@ -56,7 +70,14 @@ impl<VM: VMBinding> Plan for MarkSweep<VM> {
         scheduler: &Arc<GCWorkScheduler<VM>>,
     ) {
         self.common.gc_init(heap_size, vm_map, scheduler);
+
+        // FIXME correctly initialize spaces based on options
+        self.ms.init(&vm_map);
     }
+
+
+
+
 
     fn schedule_collection(&'static self, scheduler: &GCWorkScheduler<VM>) {
         self.base().set_collection_kind();
@@ -67,14 +88,15 @@ impl<VM: VMBinding> Plan for MarkSweep<VM> {
         // Prepare global/collectors/mutators
         scheduler.work_buckets[WorkBucketStage::Prepare]
             .add(Prepare::<Self, NoCopy<VM>>::new(self));
-        scheduler.work_buckets[WorkBucketStage::Prepare].add(MSSweepChunks::<VM>::new(self));
-        scheduler.work_buckets[WorkBucketStage::RefClosure]
-            .add(ProcessWeakRefs::<MSProcessEdges<VM>>::new());
+            #[cfg(feature="malloc")]
+            {
+            scheduler.work_buckets[WorkBucketStage::Prepare].add(MSSweepChunks::<VM>::new(self));
+            scheduler.work_buckets[WorkBucketStage::RefClosure]
+                .add(ProcessWeakRefs::<MSProcessEdges<VM>>::new());
+            }
         // Release global/collectors/mutators
         scheduler.work_buckets[WorkBucketStage::Release]
             .add(Release::<Self, NoCopy<VM>>::new(self));
-        #[cfg(feature = "analysis")]
-        scheduler.work_buckets[WorkBucketStage::Unconstrained].add(GcHookWork);
         // Resume mutators
         #[cfg(feature = "sanity")]
         scheduler.work_buckets[WorkBucketStage::Final]
@@ -88,11 +110,13 @@ impl<VM: VMBinding> Plan for MarkSweep<VM> {
 
     fn prepare(&mut self, tls: VMWorkerThread) {
         self.common.prepare(tls, true);
-        // Dont need to prepare for MallocSpace
+        #[cfg(not(feature="malloc"))]
+        self.ms.reset();
     }
 
     fn release(&mut self, tls: VMWorkerThread) {
-        trace!("Marksweep: Release");
+        #[cfg(not(feature="malloc"))]
+        self.ms.block_level_sweep();
         self.common.release(tls, true);
     }
 
@@ -100,9 +124,6 @@ impl<VM: VMBinding> Plan for MarkSweep<VM> {
         self.base().collection_required(self, space_full, space)
     }
 
-    fn get_collection_reserve(&self) -> usize {
-        0
-    }
 
     fn get_pages_used(&self) -> usize {
         self.common.get_pages_used() + self.ms.reserved_pages()
@@ -136,21 +157,50 @@ impl<VM: VMBinding> MarkSweep<VM> {
         vm_map: &'static VMMap,
         mmapper: &'static Mmapper,
         options: Arc<UnsafeOptionsWrapper>,
+        scheduler: Arc<GCWorkScheduler<VM>>,
     ) -> Self {
-        let heap = HeapMeta::new(HEAP_START, HEAP_END);
+        let mut heap = HeapMeta::new(HEAP_START, HEAP_END);
         // if global_alloc_bit is enabled, ALLOC_SIDE_METADATA_SPEC will be added to
         // SideMetadataContext by default, so we don't need to add it here.
         #[cfg(feature = "global_alloc_bit")]
         let global_metadata_specs =
-            SideMetadataContext::new_global_specs(&[ACTIVE_CHUNK_METADATA_SPEC]);
+            SideMetadataContext::new_global_specs(&[]);
         // if global_alloc_bit is NOT enabled,
         // we need to add ALLOC_SIDE_METADATA_SPEC to SideMetadataContext here.
         #[cfg(not(feature = "global_alloc_bit"))]
         let global_metadata_specs = SideMetadataContext::new_global_specs(&[
             ALLOC_SIDE_METADATA_SPEC,
-            ACTIVE_CHUNK_METADATA_SPEC,
         ]);
+        
+        #[cfg(not(feature = "malloc"))]
+        let res = {
+            let ms = MarkSweepSpace::new(
+                "MSspace",
+                true,
+                VMRequest::discontiguous(),
+                // local_specs.clone(),
+                vm_map,
+                mmapper,
+                &mut heap,
+                scheduler,
+            );
 
+            let common =             CommonPlan::new(
+                vm_map,
+                mmapper,
+                options,
+                heap,
+                &MS_CONSTRAINTS,
+                global_metadata_specs,
+            );
+
+            MarkSweep {
+                common,
+                ms,
+            }
+        };
+
+        #[cfg(feature = "malloc")]
         let res = MarkSweep {
             ms: MallocSpace::new(global_metadata_specs.clone()),
             common: CommonPlan::new(
@@ -163,20 +213,27 @@ impl<VM: VMBinding> MarkSweep<VM> {
             ),
         };
 
-        // Use SideMetadataSanity to check if each spec is valid. This is also needed for check
-        // side metadata in extreme_assertions.
-        {
-            let mut side_metadata_sanity_checker = SideMetadataSanity::new();
-            res.common
-                .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
-            res.ms
-                .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
-        }
-
+        let mut side_metadata_sanity_checker = SideMetadataSanity::new();
+        res.common
+            .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
+        res.ms
+            .verify_side_metadata_sanity(&mut side_metadata_sanity_checker);
         res
     }
 
+    #[cfg(feature="malloc")]
     pub fn ms_space(&self) -> &MallocSpace<VM> {
         &self.ms
+    }
+
+    #[cfg(not(feature="malloc"))]
+    pub fn ms_space(&self) -> &MarkSweepSpace<VM> {
+        &self.ms
+    }
+
+    /// Verify side metadata specs used in the spaces in Gen.
+    pub fn verify_side_metadata_sanity(&self, sanity: &mut SideMetadataSanity) {
+        self.ms.verify_side_metadata_sanity(sanity);
+        self.common.verify_side_metadata_sanity(sanity);
     }
 }
