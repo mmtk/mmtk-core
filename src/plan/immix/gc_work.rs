15ee83e61744b635d681239c9af460eac290ee0f
Yi Lin <qinsoon@gmail.com>
2022-01-04 14:27:06 +1100

    Add back the ImmixProcessEdges from master

--- src/plan/immix/gc_work.rs
+++ src/plan/immix/gc_work.rs
@@ -13,11 +13,86 @@ pub(in crate::plan) enum TraceKind {
     Defrag,
 }
 
+// pub(super) struct ImmixProcessEdges<VM: VMBinding, const KIND: TraceKind> {
+//     // Use a static ref to the specific plan to avoid overhead from dynamic dispatch or
+//     // downcast for each traced object.
+//     plan: &'static Immix<VM>,
+//     mmtk_process_edges: MMTkProcessEdges<VM>,
+// }
+
+// impl<VM: VMBinding, const KIND: TraceKind> ImmixProcessEdges<VM, KIND> {
+//     fn immix(&self) -> &'static Immix<VM> {
+//         self.plan
+//     }
+// }
+
+// impl<VM: VMBinding, const KIND: TraceKind> ProcessEdgesWork for ImmixProcessEdges<VM, KIND> {
+//     type VM = VM;
+
+//     const OVERWRITE_REFERENCE: bool = crate::policy::immix::DEFRAG;
+
+//     fn new(edges: Vec<Address>, roots: bool, mmtk: &'static MMTK<VM>) -> Self {
+//         let plan = mmtk.plan.downcast_ref::<Immix<VM>>().unwrap();
+//         Self { plan, mmtk_process_edges: MMTkProcessEdges::new(edges, roots, mmtk) }
+//     }
+
+//     #[cold]
+//     fn flush(&mut self) {
+//         self.mmtk_process_edges.flush()
+//     }
+
+//     /// Trace  and evacuate objects.
+//     #[inline(always)]
+//     fn trace_object(&mut self, object: ObjectReference) -> ObjectReference {
+//         if object.is_null() {
+//             return object;
+//         }
+//         if self.immix().immix_space.in_space(object) {
+//             if KIND == TraceKind::Fast {
+//                 self.immix().immix_space.fast_trace_object(self, object)
+//             } else {
+//                 self.immix().immix_space.trace_object(
+//                     self,
+//                     object,
+//                     CopySemantics::DefaultCopy,
+//                     self.worker(),
+//                 )
+//             }
+//         } else {
+//             self.mmtk_process_edges.trace_object(object)
+//         }
+//     }
+
+//     #[inline]
+//     fn process_edge(&mut self, slot: Address) {
+//         let object = unsafe { slot.load::<ObjectReference>() };
+//         let new_object = self.trace_object(object);
+//         if KIND == TraceKind::Defrag && Self::OVERWRITE_REFERENCE {
+//             unsafe { slot.store(new_object) };
+//         }
+//     }
+// }
+
+// impl<VM: VMBinding, const KIND: TraceKind> Deref for ImmixProcessEdges<VM, KIND> {
+//     type Target = ProcessEdgesBase<VM>;
+//     #[inline]
+//     fn deref(&self) -> &Self::Target {
+//         &self.mmtk_process_edges.base
+//     }
+// }
+
+// impl<VM: VMBinding, const KIND: TraceKind> DerefMut for ImmixProcessEdges<VM, KIND> {
+//     #[inline]
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         &mut self.mmtk_process_edges.base
+//     }
+// }
+
 pub(super) struct ImmixProcessEdges<VM: VMBinding, const KIND: TraceKind> {
     // Use a static ref to the specific plan to avoid overhead from dynamic dispatch or
     // downcast for each traced object.
     plan: &'static Immix<VM>,
-    mmtk_process_edges: MMTkProcessEdges<VM>,
+    base: ProcessEdgesBase<VM>,
 }
 
 impl<VM: VMBinding, const KIND: TraceKind> ImmixProcessEdges<VM, KIND> {
@@ -32,13 +107,22 @@ impl<VM: VMBinding, const KIND: TraceKind> ProcessEdgesWork for ImmixProcessEdge
     const OVERWRITE_REFERENCE: bool = crate::policy::immix::DEFRAG;
 
     fn new(edges: Vec<Address>, roots: bool, mmtk: &'static MMTK<VM>) -> Self {
-        let plan = mmtk.plan.downcast_ref::<Immix<VM>>().unwrap();
-        Self { plan, mmtk_process_edges: MMTkProcessEdges::new(edges, roots, mmtk) }
+        let base = ProcessEdgesBase::new(edges, roots, mmtk);
+        let plan = base.plan().downcast_ref::<Immix<VM>>().unwrap();
+        Self { plan, base }
     }
 
     #[cold]
     fn flush(&mut self) {
-        self.mmtk_process_edges.flush()
+        if self.nodes.is_empty() {
+            return;
+        }
+        let scan_objects_work = crate::policy::immix::ScanObjectsAndMarkLines::<Self>::new(
+            self.pop_nodes(),
+            false,
+            &self.immix().immix_space,
+        );
+        self.new_scan_work(box scan_objects_work);
     }
 
     /// Trace  and evacuate objects.
@@ -59,7 +143,9 @@ impl<VM: VMBinding, const KIND: TraceKind> ProcessEdgesWork for ImmixProcessEdge
                 )
             }
         } else {
-            self.mmtk_process_edges.trace_object(object)
+            self.immix()
+                .common
+                .trace_object::<Self>(self, object)
         }
     }
 
@@ -77,14 +163,14 @@ impl<VM: VMBinding, const KIND: TraceKind> Deref for ImmixProcessEdges<VM, KIND>
     type Target = ProcessEdgesBase<VM>;
     #[inline]
     fn deref(&self) -> &Self::Target {
-        &self.mmtk_process_edges.base
+        &self.base
     }
 }
 
 impl<VM: VMBinding, const KIND: TraceKind> DerefMut for ImmixProcessEdges<VM, KIND> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.mmtk_process_edges.base
+        &mut self.base
     }
 }
 
