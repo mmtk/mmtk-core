c301c678dc04be4b43836836a0c0992ad52ef2a4
Yi Lin <qinsoon@gmail.com>
2022-01-31 11:44:40 +1100

    Clean up some commented code

--- src/plan/generational/copying/gc_work.rs
+++ src/plan/generational/copying/gc_work.rs
@@ -8,61 +8,6 @@ use crate::vm::*;
 use crate::MMTK;
 use std::ops::{Deref, DerefMut};
 
-// pub struct GenCopyMatureProcessEdges<VM: VMBinding> {
-//     plan: &'static GenCopy<VM>,
-//     base: ProcessEdgesBase<VM>,
-// }
-
-// impl<VM: VMBinding> GenCopyMatureProcessEdges<VM> {
-//     fn gencopy(&self) -> &'static GenCopy<VM> {
-//         self.plan
-//     }
-// }
-
-// impl<VM: VMBinding> ProcessEdgesWork for GenCopyMatureProcessEdges<VM> {
-//     type VM = VM;
-
-//     fn new(edges: Vec<Address>, roots: bool, mmtk: &'static MMTK<VM>) -> Self {
-//         let base = ProcessEdgesBase::new(edges, roots, mmtk);
-//         let plan = base.plan().downcast_ref::<GenCopy<VM>>().unwrap();
-//         Self { plan, base }
-//     }
-//     #[inline]
-//     fn trace_object(&mut self, object: ObjectReference) -> ObjectReference {
-//         if object.is_null() {
-//             return object;
-//         }
-//         // Evacuate mature objects; don't trace objects if they are in to-space
-//         if self.gencopy().tospace().in_space(object) {
-//             return object;
-//         } else if self.gencopy().fromspace().in_space(object) {
-//             return self.gencopy().fromspace().trace_object::<Self>(
-//                 self,
-//                 object,
-//                 CopySemantics::Mature,
-//                 self.worker(),
-//             );
-//         }
-
-//         self.gencopy()
-//             .gen
-//             .trace_object_full_heap::<Self>(self, object, self.worker())
-//     }
-// }
-
-// impl<VM: VMBinding> Deref for GenCopyMatureProcessEdges<VM> {
-//     type Target = ProcessEdgesBase<VM>;
-//     fn deref(&self) -> &Self::Target {
-//         &self.base
-//     }
-// }
-
-// impl<VM: VMBinding> DerefMut for GenCopyMatureProcessEdges<VM> {
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         &mut self.base
-//     }
-// }
-
 use crate::scheduler::gc_work::MMTkProcessEdges;
 
 pub struct GenCopyNurseryGCWorkContext<VM: VMBinding>(std::marker::PhantomData<VM>);
--- src/plan/marksweep/gc_work.rs
+++ src/plan/marksweep/gc_work.rs
@@ -15,50 +15,6 @@ use std::sync::atomic::Ordering;
 
 use super::MarkSweep;
 
-// pub struct MSProcessEdges<VM: VMBinding> {
-//     plan: &'static MarkSweep<VM>,
-//     base: ProcessEdgesBase<VM>,
-// }
-
-// impl<VM: VMBinding> ProcessEdgesWork for MSProcessEdges<VM> {
-//     type VM = VM;
-
-//     const OVERWRITE_REFERENCE: bool = false;
-//     fn new(edges: Vec<Address>, roots: bool, mmtk: &'static MMTK<VM>) -> Self {
-//         let base = ProcessEdgesBase::new(edges, roots, mmtk);
-//         let plan = base.plan().downcast_ref::<MarkSweep<VM>>().unwrap();
-//         Self { plan, base }
-//     }
-
-//     #[inline]
-//     fn trace_object(&mut self, object: ObjectReference) -> ObjectReference {
-//         if object.is_null() {
-//             return object;
-//         }
-//         trace!("Tracing object {}", object);
-//         if self.plan.ms_space().in_space(object) {
-//             self.plan.ms_space().trace_object::<Self>(self, object)
-//         } else {
-//             self.plan.common().trace_object::<Self>(self, object)
-//         }
-//     }
-// }
-
-// impl<VM: VMBinding> Deref for MSProcessEdges<VM> {
-//     type Target = ProcessEdgesBase<VM>;
-//     #[inline]
-//     fn deref(&self) -> &Self::Target {
-//         &self.base
-//     }
-// }
-
-// impl<VM: VMBinding> DerefMut for MSProcessEdges<VM> {
-//     #[inline]
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         &mut self.base
-//     }
-// }
-
 /// Simple work packet that just sweeps a single chunk
 pub struct MSSweepChunk<VM: VMBinding> {
     ms: &'static MallocSpace<VM>,
