59455d5ab8adc222d1ff32a11d346ca523f08570
Yi Lin <qinsoon@gmail.com>
2022-05-18 11:01:00 +1000

    Add some documentation to ReferenceGlue

--- src/vm/reference_glue.rs
+++ src/vm/reference_glue.rs
@@ -3,8 +3,18 @@ use crate::util::ObjectReference;
 use crate::util::VMWorkerThread;
 use crate::vm::VMBinding;
 
-/// VM-specific methods for reference processing.
+/// VM-specific methods for reference processing, including weak references, and finalizers.
+/// We handle weak references and finalizers differently:
+/// * for weak references, we assume they are implemented as normal reference objects with a
+///   referent that is actually weakly reachable. This trait provides a few methods to access
+///   the referent of such an reference object.
+/// * for finalizers, we provide a `Finalizable` trait, and require bindings to specify a type
+///   that implements `Finalizable`. When the binding registers or pops a finalizable object
+///   from MMTk, the specified type is used for the finalizable objects. For most languages,
+///   they can just use `ObjectReference` for the finalizable type, meaning that they are registering
+///   and popping a normal object reference as finalizable objects.
 pub trait ReferenceGlue<VM: VMBinding> {
+    /// The type of finalizable objects. This type is used when the binding registers and pops finalizable objects.
     type FinalizableType: Finalizable;
 
     /// Weak and soft references always clear the referent
@@ -18,20 +28,20 @@ pub trait ReferenceGlue<VM: VMBinding> {
         });
     }
 
-    /// Get the referent from a reference.
+    /// Get the referent from a weak reference object.
     ///
     /// Arguments:
     /// * `object`: The object reference.
     fn get_referent(object: ObjectReference) -> ObjectReference;
 
-    /// Set the referent in a reference.
+    /// Set the referent in a weak reference object.
     ///
     /// Arguments:
     /// * `reff`: The object reference for the reference.
     /// * `referent`: The referent object reference.
     fn set_referent(reff: ObjectReference, referent: ObjectReference);
 
-    /// For reference types, if the referent is cleared during GC, the reference
+    /// For weak reference types, if the referent is cleared during GC, the reference
     /// will be added to a queue, and MMTk will call this method to inform
     /// the VM about the changes for those references. This method is used
     /// to implement Java's ReferenceQueue.
@@ -41,11 +51,20 @@ pub trait ReferenceGlue<VM: VMBinding> {
     fn enqueue_references(references: &[ObjectReference], tls: VMWorkerThread);
 }
 
+/// A finalizable object for MMTk. MMTk needs to know the actual object reference in the type,
+/// while a binding can use this type to store some runtime information about finalizable objects.
+/// For example, for bindings that allows multiple finalizer methods with one object, they can define
+/// the type as a tuple of `(object, finalize method)`, and register different finalizer methods to MMTk
+/// for the same object.
 pub trait Finalizable: std::fmt::Debug + Send {
+    /// Load the object reference.
     fn load_reference(&self) -> ObjectReference;
+    /// Store the object reference.
     fn set_reference(&mut self, object: ObjectReference);
 }
 
+/// This provides an implementation of `Finalizable` for `ObjectReference`. Most bindings
+/// should be able to use `ObjectReference` as `ReferenceGlue::FinalizableType`.
 impl Finalizable for ObjectReference {
     #[inline(always)]
     fn load_reference(&self) -> ObjectReference {
