a72d3d7ca64bd42c96bab729212439dd343bd65d
Yi Lin <qinsoon@gmail.com>
2022-08-17 13:23:42 +1000

    Add comments for space features, and add doc for vm_trace_object()

--- src/vm/active_plan.rs
+++ src/vm/active_plan.rs
@@ -72,7 +72,24 @@ pub trait ActivePlan<VM: VMBinding> {
     /// Return the total count of mutators.
     fn number_of_mutators() -> usize;
 
-    /// The fallback for object tracing.
+    /// The fallback for object tracing. MMTk generally expects to find an object in one of MMTk's spaces (if it is allocated by MMTK),
+    /// and apply the corresponding policy to trace the object. Tracing in MMTk means identifying whether we have encountered this object in the
+    /// current GC. For example, for mark sweep, we will check if an object is marked, and if it is not yet marked, mark and enqueue the object
+    /// for later scanning. For copying policies, copying also happens in this step. For example for MMTk's copying space, we will
+    /// copy an object if it is in 'from space', and enqueue the copied object for later scanning.
+    ///
+    /// If a binding would like to trace objects that are not allocated by MMTk and are not in any MMTk space, they can override this method.
+    /// They should check whether the object is encountered before in this current GC. If not, they should record the object as encountered themselves,
+    /// and enqueue the object reference to the object queue provided by the argument. If a binding moves objects, they should do the copying in the method,
+    /// and enqueue the new object reference instead.
+    ///
+    /// The method should return the new object reference if the method moves the object, otherwise return the original object reference.
+    ///
+    /// Arguments:
+    /// * `queue`: The object queue. If an object is encountered for the first time in this GC, we expect the implementation to call `queue.enqueue()`
+    ///            for the object. If the object is moved during the tracing, the new object reference (after copying) should be enqueued instead.
+    /// * `object`: The object to trace.
+    /// * `worker`: The GC worker that is doing this tracing. This is used to copy object (see [vm::ObjectModel::copy])
     fn vm_trace_object<Q: ObjectQueue>(
         _queue: &mut Q,
         object: ObjectReference,
