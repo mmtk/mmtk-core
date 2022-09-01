a6d1692da41a4cd079e6ce8dd2db189fb7b788c1
qinsoon <qinsoon@users.noreply.github.com>
2022-09-01 01:24:39 +0000

    deploy: d0183aa549a2967588eda7555fcc1addcafc7d21

---
+++ docs/external/util/malloc/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["calloc",""],["counted_calloc",""],["counted_malloc",""],["free",""],["free_with_size",""],["malloc",""],["realloc",""],["realloc_with_old_size",""]],"mod":[["malloc_ms_util","Using malloc as mark sweep free-list allocator"]]});
---
+++ docs/internal/util/alloc/immix_allocator/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["ImmixAllocator","Immix allocator"]]});
