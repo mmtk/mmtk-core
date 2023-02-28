b3299a64523935bb93cd013d0830447b135e8669
qinsoon <qinsoon@users.noreply.github.com>
2023-02-28 06:58:47 +0000

    deploy: 08fd8ab0cfaa240a6e9a401afaec963ec0de8be5

---
+++ docs/full-doc/plan/sticky/immix/global/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":[["STICKY_IMMIX_CONSTRAINTS",""]],"struct":[["StickyImmix",""]]};
---
+++ docs/full-doc/plan/sticky/immix/mutator/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["create_stickyimmix_mutator",""],["stickyimmix_mutator_prepare",""],["stickyimmix_mutator_release",""]]};
