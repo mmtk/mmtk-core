94c5063c8687cfad7dea28a07d329a408d78a647
Yi Lin <qinsoon@gmail.com>
2023-03-21 05:19:03 +0000

    Remove vm_map/mmapper from plan

--- src/plan/global.rs
+++ src/plan/global.rs
@@ -185,9 +185,6 @@ pub trait Plan: 'static + Sync + Downcast {
     ) -> Option<&dyn crate::plan::generational::global::GenerationalPlan<VM = Self::VM>> {
         None
     }
-    fn mmapper(&self) -> &'static dyn Mmapper {
-        self.base().mmapper
-    }
     fn options(&self) -> &Options {
         &self.base().options
     }
@@ -381,8 +378,7 @@ pub struct BasePlan<VM: VMBinding> {
     pub cur_collection_attempts: AtomicUsize,
     pub gc_requester: Arc<GCRequester<VM>>,
     pub stats: Stats,
-    mmapper: &'static dyn Mmapper,
-    pub vm_map: &'static dyn Map,
+    // pub vm_map: &'static dyn Map,
     pub options: Arc<Options>,
     pub heap: HeapMeta,
     pub gc_trigger: Arc<GCTrigger<VM>>,
@@ -540,10 +536,9 @@ impl<VM: VMBinding> BasePlan<VM> {
             cur_collection_attempts: AtomicUsize::new(0),
             gc_requester: Arc::new(GCRequester::new()),
             stats,
-            mmapper: args.global_args.mmapper,
             heap: args.global_args.heap,
             gc_trigger: args.global_args.gc_trigger,
-            vm_map: args.global_args.vm_map,
+            // vm_map: args.global_args.vm_map,
             options: args.global_args.options,
             #[cfg(feature = "sanity")]
             inside_sanity: AtomicBool::new(false),
