d0960fd842986619476c0339dbb448a2d4066c72
Kunshan Wang <wks1986@gmail.com>
2023-04-13 19:57:11 +0800

    Fix compilation when vo_bit is not enabled

--- src/plan/markcompact/global.rs
+++ src/plan/markcompact/global.rs
@@ -180,15 +180,15 @@ impl<VM: VMBinding> Plan for MarkCompact<VM> {
 
 impl<VM: VMBinding> MarkCompact<VM> {
     pub fn new(args: CreateGeneralPlanArgs<VM>) -> Self {
-        // if vo_bit is enabled, ALLOC_SIDE_METADATA_SPEC will be added to
+        // if vo_bit is enabled, VO_BIT_SIDE_METADATA_SPEC will be added to
         // SideMetadataContext by default, so we don't need to add it here.
         #[cfg(feature = "vo_bit")]
         let global_side_metadata_specs = SideMetadataContext::new_global_specs(&[]);
         // if vo_bit is NOT enabled,
-        // we need to add ALLOC_SIDE_METADATA_SPEC to SideMetadataContext here.
+        // we need to add VO_BIT_SIDE_METADATA_SPEC to SideMetadataContext here.
         #[cfg(not(feature = "vo_bit"))]
         let global_side_metadata_specs =
-            SideMetadataContext::new_global_specs(&[ALLOC_SIDE_METADATA_SPEC]);
+            SideMetadataContext::new_global_specs(&[VO_BIT_SIDE_METADATA_SPEC]);
 
         let mut plan_args = CreateSpecificPlanArgs {
             global_args: args,
