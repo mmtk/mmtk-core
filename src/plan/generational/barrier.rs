2e5564ae0447073fcaafe485dcd67aa0849fcfca
Yi Lin <qinsoon@gmail.com>
2023-05-04 00:45:15 +0000

    Minor update

--- src/plan/generational/barrier.rs
+++ src/plan/generational/barrier.rs
@@ -82,11 +82,12 @@ impl<VM: VMBinding, P: GenerationalPlanExt<VM> + PlanTraceObject<VM>> BarrierSem
     }
 
     fn memory_region_copy_slow(&mut self, _src: VM::VMMemorySlice, dst: VM::VMMemorySlice) {
-        // Only enqueue array slices in mature spaces
+        // Check if the destination object/slice is in nursery space.
         let dst_in_nursery = match dst.object() {
             Some(obj) => self.plan.is_object_in_nursery(obj),
             None => self.plan.is_address_in_nursery(dst.start()),
         };
+        // Only enqueue array slices in mature spaces
         if !dst_in_nursery {
             // enqueue
             debug_assert_eq!(
