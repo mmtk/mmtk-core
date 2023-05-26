43e91932dfaf3598fb4413a3a228d7e470d9e300
Yi Lin <qinsoon@gmail.com>
2023-05-26 01:31:44 +0000

    Changes to tests so panic will not poison the lock

--- vmbindings/dummyvm/src/tests/barrier_slow_path_assertion.rs
+++ vmbindings/dummyvm/src/tests/barrier_slow_path_assertion.rs
@@ -5,56 +5,57 @@
 
 use crate::object_model::OBJECT_REF_OFFSET;
 use crate::{api::*, edges};
-use crate::tests::fixtures::SerialFixture;
-use crate::tests::fixtures::MutatorFixture;
+use crate::tests::fixtures::MMTKSingleton;
+use crate::tests::fixtures::FixtureContent;
 use atomic::Atomic;
 use mmtk::util::{Address, ObjectReference};
+use mmtk::util::{VMThread, VMMutatorThread};
 use mmtk::vm::edge_shape::SimpleEdge;
 use mmtk::AllocationSemantics;
 
 lazy_static! {
-    static ref MUTATOR: SerialFixture<MutatorFixture> = SerialFixture::new();
+    static ref MMTK_SINGLETON: MMTKSingleton = MMTKSingleton::create();
 }
 
 #[test]
-#[should_panic]
+#[should_panic(expected = "object bit is unset")]
 fn test_assertion_barrier_invalid_ref() {
-    MUTATOR.with_fixture(|fixture| {
-        // Allocate
-        let size = 24;
-        let addr = mmtk_alloc(fixture.mutator, size, 8, 0, AllocationSemantics::Default);
-        let objref: ObjectReference = ObjectReference::from_raw_address(addr.add(OBJECT_REF_OFFSET));
-        mmtk_post_alloc(fixture.mutator, objref, size, AllocationSemantics::Default);
-        // Create an edge
-        let mut slot: Atomic<ObjectReference> = Atomic::new(objref);
-        let edge = SimpleEdge::from_address(Address::from_ref(&mut slot));
-        // Create an invalid object reference (offset 8 bytes on the original object ref), and invoke barrier slowpath with it
-        // The invalid object ref has no VO bit, and the assertion should fail.
-        let invalid_objref = ObjectReference::from_raw_address(objref.to_raw_address() + 8usize);
-        unsafe {
-            let mu = &mut *fixture.mutator;
-            mu.barrier
-                .object_reference_write_slow(invalid_objref, edges::DummyVMEdge::Simple(edge), objref);
-        }
-    })
+    let mutator = mmtk_bind_mutator(VMMutatorThread(VMThread::UNINITIALIZED));
+
+    // Allocate
+    let size = 24;
+    let addr = mmtk_alloc(mutator, size, 8, 0, AllocationSemantics::Default);
+    let objref: ObjectReference = ObjectReference::from_raw_address(addr.add(OBJECT_REF_OFFSET));
+    mmtk_post_alloc(mutator, objref, size, AllocationSemantics::Default);
+    // Create an edge
+    let mut slot: Atomic<ObjectReference> = Atomic::new(objref);
+    let edge = SimpleEdge::from_address(Address::from_ref(&mut slot));
+    // Create an invalid object reference (offset 8 bytes on the original object ref), and invoke barrier slowpath with it
+    // The invalid object ref has no VO bit, and the assertion should fail.
+    let invalid_objref = ObjectReference::from_raw_address(objref.to_raw_address() + 8usize);
+    unsafe {
+        let mu = &mut *mutator;
+        mu.barrier
+            .object_reference_write_slow(invalid_objref, edges::DummyVMEdge::Simple(edge), objref);
+    }
 }
 
 #[test]
 fn test_assertion_barrier_valid_ref() {
-    MUTATOR.with_fixture(|fixture| {
-        // Allocate
-        let size = 24;
-        let addr = mmtk_alloc(fixture.mutator, size, 8, 0, AllocationSemantics::Default);
-        let objref: ObjectReference = ObjectReference::from_raw_address(addr.add(OBJECT_REF_OFFSET));
-        mmtk_post_alloc(fixture.mutator, objref, size, AllocationSemantics::Default);
-        // Create an edge
-        let mut slot: Atomic<ObjectReference> = Atomic::new(objref);
-        let edge = SimpleEdge::from_address(Address::from_ref(&mut slot));
-        // Invoke barrier slowpath with the valid object ref
-        unsafe {
-            let mu = &mut *fixture.mutator;
-            mu.barrier
-                .object_reference_write_slow(objref, edges::DummyVMEdge::Simple(edge), objref);
-        }
-    })
+    let mutator = mmtk_bind_mutator(VMMutatorThread(VMThread::UNINITIALIZED));
+
+    // Allocate
+    let size = 24;
+    let addr = mmtk_alloc(mutator, size, 8, 0, AllocationSemantics::Default);
+    let objref: ObjectReference = ObjectReference::from_raw_address(addr.add(OBJECT_REF_OFFSET));
+    mmtk_post_alloc(mutator, objref, size, AllocationSemantics::Default);
+    // Create an edge
+    let mut slot: Atomic<ObjectReference> = Atomic::new(objref);
+    let edge = SimpleEdge::from_address(Address::from_ref(&mut slot));
+    // Invoke barrier slowpath with the valid object ref
+    unsafe {
+        let mu = &mut *mutator;
+        mu.barrier
+            .object_reference_write_slow(objref, edges::DummyVMEdge::Simple(edge), objref);
+    }
 }
