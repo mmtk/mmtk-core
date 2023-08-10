a37fb5604cedf0e015782eceeebc824f924a1b2b
Eduardo Souza <ledusou@gmail.com>
2023-08-10 06:12:18 +0000

    Fix warning in tests

--- tests/test_roots_work_factory.rs
+++ tests/test_roots_work_factory.rs
@@ -70,11 +70,11 @@ impl RootsWorkFactory<Address> for MockFactory {
         unimplemented!();
     }
 
-    fn create_immovable_process_edge_roots_work(&mut self, edges: Vec<Address>) {
+    fn create_immovable_process_edge_roots_work(&mut self, _edges: Vec<Address>) {
         unimplemented!();
     }
 
-    fn create_immovable_process_node_roots_work(&mut self, nodes: Vec<ObjectReference>) {
+    fn create_immovable_process_node_roots_work(&mut self, _nodes: Vec<ObjectReference>) {
         unimplemented!();
     }
 }
