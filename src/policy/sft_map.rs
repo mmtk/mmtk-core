f10a10d540ec64cf7d11541af9df185a2262bf0d
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-14 14:36:13 +1000

    cargo fmt

--- src/policy/sft_map.rs
+++ src/policy/sft_map.rs
@@ -1,5 +1,4 @@
 use super::sft::*;
-use crate::util::heap::layout::vm_layout_constants::VM_LAYOUT_CONSTANTS;
 use crate::util::metadata::side_metadata::SideMetadataSpec;
 use crate::util::Address;
 
@@ -67,6 +66,7 @@ pub(crate) fn create_sft_map() -> Box<dyn SFTMap> {
             // 64-bit malloc mark sweep needs a chunk-based SFT map, but the sparse map is not suitable for 64bits.
             Box::new(dense_chunk_map::SFTDenseChunkMap::new())
         } else if #[cfg(target_pointer_width = "64")] {
+            use crate::util::heap::layout::vm_layout_constants::VM_LAYOUT_CONSTANTS;
             if VM_LAYOUT_CONSTANTS.force_use_contiguous_spaces() {
                 Box::new(space_map::SFTSpaceMap::new())
             } else {
