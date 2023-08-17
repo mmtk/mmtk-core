2b8c14daca33df8e713d4741964ebb86556c92fa
Zixian Cai <u5937495@anu.edu.au>
2023-08-17 23:14:12 +1000

    Fix cargo doc warnings

--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -7,9 +7,10 @@
 /// descendants) may have the same policy (eg there could be numerous
 /// instances of CopySpace, each with different roles). Spaces are
 /// defined in terms of a unique region of virtual memory, so no two
-/// space instances ever share any virtual memory.<p>
+/// space instances ever share any virtual memory.
+/// 
 /// In addition to tracking virtual memory use and the mapping to
-/// policy, spaces also manage memory consumption (<i>used</i> virtual
+/// policy, spaces also manage memory consumption (*used* virtual
 /// memory).
 pub mod space;
 
