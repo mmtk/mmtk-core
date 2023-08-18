4eabcda13a1e42a61505488ac1f4eaa7bf77d6ed
Kunshan Wang <wks1986@gmail.com>
2023-08-18 20:35:26 +0800

    Use sudo

--- .github/scripts/ci-replace-mmtk-dep.sh
+++ .github/scripts/ci-replace-mmtk-dep.sh
@@ -2,6 +2,6 @@
 
 echo "Replacing mmtk-core dependency for binding"
 
-apt-get update
-apt-get install python-tomlkit
+sudo apt-get update -y
+sudo apt-get install python-tomlkit
 python $(dirname "$0")/replace-mmtk-dep.py "$@"
