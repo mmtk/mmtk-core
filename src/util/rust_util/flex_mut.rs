0fc150ca1a331142eabdcc8ed0885b2bec660d8c
Yi Lin <qinsoon@gmail.com>
2023-09-29 02:27:57 +0000

    Add comments for ArcFlexMut

--- src/util/rust_util/flex_mut.rs
+++ src/util/rust_util/flex_mut.rs
@@ -13,6 +13,17 @@ macro_rules! to_trait_object {
     }};
 }
 
+/// `ArcFlexMut` is a replacement for `UnsafeCell` for a shared reference in situations where 1.
+/// their mutability is managed by the programmer, 2. mutability is hard to reason about statically,
+/// 3. using locks or `RefCell`/`AtomicRefCell` is not plausible for the sake of performance, and
+/// 4. the shared reference could be both statically typed and a dyn ref to a trait object, depending
+/// on where it is used.
+/// `ArcFlexMut` does not guarantee thread safety, and it does not provide any actual locking.
+/// It provides methods for acquiring a read or write guard, and can optionally check if there is
+/// any possible data race. Without the checks, in a release build, `ArcFlexMut` should perform
+/// as efficient as `UnsafeCell`.
+/// We currently use this type for [`crate::policy::space::Space`]s.
+#[repr(transparent)]
 pub struct ArcFlexMut<T>
 where
     T: ?Sized,
@@ -21,6 +32,7 @@ where
 }
 
 impl<T> ArcFlexMut<T> {
+    /// Create a shared reference to the object.
     pub fn new(v: T) -> Self {
         Self {
             inner: Arc::new(peace_lock::RwLock::new(v)),
@@ -28,42 +40,20 @@ impl<T> ArcFlexMut<T> {
     }
 }
 
-impl<T> ArcFlexMut<T> {
-    pub fn into_dyn_space<VM: VMBinding>(self) -> ArcFlexMut<dyn Space<VM>>
-    where
-        T: 'static + Space<VM>,
-    {
-        to_trait_object!(self, dyn Space<VM>)
-    }
-}
-
 impl<T: ?Sized> ArcFlexMut<T> {
+    /// Acquire a read guard to get immutable access to the data. It is allowed to have a reader when there is no writer.
+    /// If the feature `check_flex_mut` is enabled, the method will panic if the rule is violated.
     pub fn read(&self) -> ArcFlexMutReadGuard<'_, T> {
         ArcFlexMutReadGuard {
             inner: self.inner.read(),
         }
     }
 
+    /// Acquire a write guard to get mutable access to the data. It is allowed to have a writer when there is no other writer or reader.
+    /// If the feature `check_flex_mut` is enabled, the method will panic if the rule is violated.
     pub fn write(&self) -> ArcFlexMutWriteGuard<'_, T> {
         ArcFlexMutWriteGuard {
             inner: self.inner.write(),
-        }
-    }
-}
-
-impl<T: 'static + Downcast + ?Sized> ArcFlexMut<T> {
-    fn can_downcast<S: 'static>(&self) -> bool {
-        let lock = self.inner.read();
-        (*lock).as_any().downcast_ref::<S>().is_some()
-    }
-
-    pub fn downcast<S: 'static>(self) -> ArcFlexMut<S> {
-        if self.can_downcast::<S>() {
-            let raw = Arc::into_raw(self.inner);
-            let new_inner = unsafe { Arc::from_raw(raw as *const peace_lock::RwLock<S>) };
-            ArcFlexMut { inner: new_inner }
-        } else {
-            panic!("Failed to downcast")
         }
     }
 }
@@ -79,6 +69,43 @@ where
     }
 }
 
+// For types that implements `Downcast`, we can turn the shared reference into a reference of a concrete type.
+
+impl<T: 'static + Downcast + ?Sized> ArcFlexMut<T> {
+    /// Is it allowed to downcast to the given type?
+    fn can_downcast<S: 'static>(&self) -> bool {
+        let lock = self.inner.read();
+        (*lock).as_any().downcast_ref::<S>().is_some()
+    }
+
+    /// Downcast the shared reference into a shared reference of a concrete type. The new reference share
+    /// the count and the lock with the old consumed reference.
+    pub fn downcast<S: 'static>(self) -> ArcFlexMut<S> {
+        if self.can_downcast::<S>() {
+            let raw = Arc::into_raw(self.inner);
+            let new_inner = unsafe { Arc::from_raw(raw as *const peace_lock::RwLock<S>) };
+            ArcFlexMut { inner: new_inner }
+        } else {
+            panic!("Failed to downcast")
+        }
+    }
+}
+
+// Methods to turn the shared reference into a shared reference of a trait object.
+// The references points to the same object with the same count.
+// This impl block is a workaround to implement the functionality specifically for
+// `dyn Space`, as I can't find a way to implement this using generics.
+
+impl<T> ArcFlexMut<T> {
+    pub fn into_dyn_space<VM: VMBinding>(self) -> ArcFlexMut<dyn Space<VM>>
+    where
+        T: 'static + Space<VM>,
+    {
+        to_trait_object!(self, dyn Space<VM>)
+    }
+}
+
+/// Read guard for ArcFlexMut
 pub struct ArcFlexMutReadGuard<'a, T>
 where
     T: ?Sized,
@@ -98,6 +125,7 @@ where
     }
 }
 
+/// Write guard for ArcFlexMut
 pub struct ArcFlexMutWriteGuard<'a, T>
 where
     T: ?Sized,
