f20e3c33ef910702f231d2591c70ee267c0041c6
Yi Lin <qinsoon@gmail.com>
2023-10-26 02:11:06 +0000

    Remove tag in comments

--- src/policy/space.rs
+++ src/policy/space.rs
@@ -46,7 +46,7 @@ pub struct SpaceAllocFail;
 /// descendants) may have the same policy (eg there could be numerous
 /// instances of CopySpace, each with different roles). Spaces are
 /// defined in terms of a unique region of virtual memory, so no two
-/// space instances ever share any virtual memory.<p>
+/// space instances ever share any virtual memory.
 ///
 /// In addition to tracking virtual memory use and the mapping to
 /// policy, spaces also manage memory consumption (used virtual
