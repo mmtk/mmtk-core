18f7bb610f9aac268518951f718c4b19122e12d4
Kunshan Wang <wks1986@gmail.com>
2023-12-28 23:05:05 +0800

    WIP: Rework GC triggering

--- src/plan/gc_requester.rs
+++ src/plan/gc_requester.rs
@@ -6,12 +6,11 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 
 struct RequestSync {
-    request_count: isize,
-    last_request_count: isize,
+    /// Is GC scheduled (but not finished)?
+    gc_scheduled: bool,
 }
 
-/// GC requester.  This object allows other threads to request (trigger) GC,
-/// and the GC coordinator thread waits for GC requests using this object.
+/// This data structure lets mutators trigger GC, and may schedule collection when appropriate.
 pub struct GCRequester<VM: VMBinding> {
     request_sync: Mutex<RequestSync>,
     request_flag: AtomicBool,
@@ -23,8 +22,7 @@ impl<VM: VMBinding> GCRequester<VM> {
     pub fn new(scheduler: Arc<GCWorkScheduler<VM>>) -> Self {
         GCRequester {
             request_sync: Mutex::new(RequestSync {
-                request_count: 0,
-                last_request_count: -1,
+                gc_scheduled: true,
             }),
             request_flag: AtomicBool::new(false),
             scheduler,
@@ -38,22 +36,37 @@ impl<VM: VMBinding> GCRequester<VM> {
         }
 
         let mut guard = self.request_sync.lock().unwrap();
+        // Note: This is the double-checked locking algorithm.
+        // The load has the `Relaxed` order instead of `Acquire` because we only use the flag to
+        // remove successive requests, but we don't use it to synchronize other data fields.
         if !self.request_flag.load(Ordering::Relaxed) {
             self.request_flag.store(true, Ordering::Relaxed);
-            guard.request_count += 1;
 
-            self.schedule_collection();
+            self.try_schedule_collection(&mut *guard);
         }
     }
 
     pub fn clear_request(&self) {
-        let guard = self.request_sync.lock().unwrap();
+        let _guard = self.request_sync.lock().unwrap();
         self.request_flag.store(false, Ordering::Relaxed);
-        drop(guard);
     }
 
-    fn schedule_collection(&self) {
-        // Add a ScheduleCollection work packet.  It is the seed of other work packets.
-        self.scheduler.work_buckets[WorkBucketStage::Unconstrained].add(ScheduleCollection);
+    pub fn on_gc_finished(&self) {
+        let mut guard = self.request_sync.lock().unwrap();
+        guard.gc_scheduled = false;
+
+        self.try_schedule_collection(&mut *guard);
+    }
+
+    fn try_schedule_collection(&self, sync: &mut RequestSync) {
+        if self.request_flag.load(Ordering::Relaxed) && !sync.gc_scheduled {
+            // Add a ScheduleCollection work packet.  It is the seed of other work packets.
+            self.scheduler.work_buckets[WorkBucketStage::Unconstrained].add(ScheduleCollection);
+
+            sync.gc_scheduled = true;
+
+            // Note: We do not clear `request_flag` now.  It will be cleared by `clear_request`
+            // after all mutators have stopped.
+        }
     }
 }
