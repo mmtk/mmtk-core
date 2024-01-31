a8d31f2366e0f2257f9415df99dfa1fe9c4f76e3
Eduardo Souza <ledusou@gmail.com>
2024-01-31 23:46:26 +0000

    Fixing heapsize and disabling GC for alloc benchmark

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -2,11 +2,24 @@ use criterion::Criterion;
 
 use mmtk::memory_manager;
 use mmtk::util::test_util::fixtures::*;
+use mmtk::util::test_util::mock_method::*;
+use mmtk::util::test_util::mock_vm::{write_mockvm, MockVM};
 use mmtk::AllocationSemantics;
 
 pub fn bench(c: &mut Criterion) {
-    // Setting a larger heap so we won't trigger GC, but we should disable GC if we can
-    let mut fixture = MutatorFixture::create_with_heapsize(usize::MAX);
+    // Setting a larger heap (1TB) so we won't trigger GC, but we should disable GC if we can
+    let mut fixture = MutatorFixture::create_with_heapsize(1 << 40);
+    {
+        write_mockvm(|mock| {
+            *mock = {
+                MockVM {
+                    is_collection_enabled: MockMethod::new_fixed(Box::new(|_| false)),
+                    ..MockVM::default()
+                }
+            }
+        });
+    }
+
     c.bench_function("alloc", |b| {
         b.iter(|| {
             let _addr =
