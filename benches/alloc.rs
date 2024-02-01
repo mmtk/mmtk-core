e9660c7ffca6a0d7fb5efb159482d89053f5d203
Eduardo Souza <ledusou@gmail.com>
2024-02-01 00:27:05 +0000

    Fixing code/comment in alloc benchmark

--- benches/alloc.rs
+++ benches/alloc.rs
@@ -7,8 +7,8 @@ use mmtk::util::test_util::mock_vm::{write_mockvm, MockVM};
 use mmtk::AllocationSemantics;
 
 pub fn bench(c: &mut Criterion) {
-    // Setting a larger heap (1TB) so we won't trigger GC, but we should disable GC if we can
-    let mut fixture = MutatorFixture::create_with_heapsize(1usize << 40);
+    // Setting a larger heap, although the GC should be disabled in the MockVM
+    let mut fixture = MutatorFixture::create_with_heapsize(1 << 30);
     {
         write_mockvm(|mock| {
             *mock = {
