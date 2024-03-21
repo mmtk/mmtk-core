d1453cce26784a7b11b5f3d1f3e17878728c7364
Yi Lin <qinsoon@gmail.com>
2024-03-21 04:31:39 +0000

    Still compile raw_memory_freelist and its tests for 32 bits

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -73,7 +73,6 @@ mod freelist;
 mod int_array_freelist;
 /// Implementation of GenericFreeList backed by raw memory, allocated
 /// on demand direct from the OS (via mmap).
-#[cfg(target_pointer_width = "64")]
 mod raw_memory_freelist;
 
 pub use self::address::Address;
