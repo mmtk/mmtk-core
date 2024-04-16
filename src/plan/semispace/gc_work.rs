a2b36f42742bed40da731ccc4ece294891668525
Kunshan Wang <wks1986@gmail.com>
2024-04-16 14:15:03 +0800

    Rename associated types in GCWorkContext (#1116)
    This PR renames the associated types `ProcessEdgesWorkType` to
    `DefaultProcessEdges`, and `TPProcessEdges` to `PinningProcessEdges`.
    
    The old name `TPProcessEdges` was very confusing because that type alone
    is not sufficient to implement transitive pinning roots. We rename it to
    `PinningProcessEdges`, removing "transitive" from its name. We also
    renamed `ProcessEdgesWorkType` to `DefaultProcessEdges` for symmetry. We
    also updated comments to clarify the purposes of those type members.
    
    We also updated the type parameters of
    `ProcessEdgesWorkRootsWorkFactory<VM, DPE, PPE>` and
    `ProcessRootNode<VM, R2OPE, O2OPE>` to clarify their purposes, at the
    expense of being slightly more verbose.

--- src/plan/semispace/gc_work.rs
+++ src/plan/semispace/gc_work.rs
@@ -7,6 +7,6 @@ pub struct SSGCWorkContext<VM: VMBinding>(std::marker::PhantomData<VM>);
 impl<VM: VMBinding> crate::scheduler::GCWorkContext for SSGCWorkContext<VM> {
     type VM = VM;
     type PlanType = SemiSpace<VM>;
-    type ProcessEdgesWorkType = PlanProcessEdges<Self::VM, SemiSpace<VM>, DEFAULT_TRACE>;
-    type TPProcessEdges = UnsupportedProcessEdges<VM>;
+    type DefaultProcessEdges = PlanProcessEdges<Self::VM, SemiSpace<VM>, DEFAULT_TRACE>;
+    type PinningProcessEdges = UnsupportedProcessEdges<VM>;
 }
