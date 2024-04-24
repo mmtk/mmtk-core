990980858be221db5572e0e362b986e5294dc025
Kunshan Wang <wks1986@gmail.com>
2024-04-24 17:11:20 +0800

    Fix link

--- src/util/apiutils.rs
+++ src/util/apiutils.rs
@@ -10,7 +10,7 @@ use super::{Address, ObjectReference};
 /// Note: The Rust ABI currently doesn't guarantee the encoding of `None` even if the `T` in
 /// `Option<T>` is eligible for null pointer optimization.  Transmuting a `None` value of
 /// `Option<ObjectReference>` to `usize` still has undefined behavior.
-/// See: https://doc.rust-lang.org/std/option/index.html#representation
+/// See: <https://doc.rust-lang.org/std/option/index.html#representation>
 ///
 /// It is intended for passing an `Option<ObjectReference>` values to and from native programs
 /// (usually C or C++) that have null pointers.
