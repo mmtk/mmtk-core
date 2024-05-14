729496c12a7b83ccfebc29ecfd6e4b6630e1a888
Kunshan Wang <wks1986@gmail.com>
2024-05-14 17:51:33 +0800

    address, vo_bit and util

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -35,7 +35,7 @@ pub mod test_util;
 /// An analysis framework for collecting data and profiling in GC.
 #[cfg(feature = "analysis")]
 pub(crate) mod analysis;
-/// Logging edges to check duplicated edges in GC.
+/// Logging slots to check duplicated edges in GC.
 #[cfg(feature = "extreme_assertions")]
 pub(crate) mod slot_logger;
 /// Non-generic refs to generic types of `<VM>`.
