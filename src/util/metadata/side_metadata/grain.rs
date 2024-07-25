2f4e38062cac479a06b4a95030cda509ddb1542d
Kunshan Wang <wks1986@gmail.com>
2024-07-25 22:11:26 +0800

    Fix stupid mistakes and add tests

--- src/util/metadata/side_metadata/grain.rs
+++ src/util/metadata/side_metadata/grain.rs
@@ -7,19 +7,15 @@
 //! The `std::simd` module is still a nightly feature as of Rust 1.79.  When it stablizes, we can
 //! allow the granularity to be a SIMD vector, too.
 
-use num_traits::Num;
+use num_traits::{Num, PrimInt};
 
-use crate::util::{constants::BITS_IN_BYTE, Address};
+use crate::util::{
+    constants::{BITS_IN_BYTE, LOG_BITS_IN_BYTE},
+    Address,
+};
 
 /// Offset of bits in a grain.
 type BitOffset = usize;
-
-/// Bit address within a grain.
-#[derive(Debug, Clone, Copy)]
-pub struct BitAddress {
-    pub addr: Address,
-    pub bit: BitOffset,
-}
 
 #[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Granularity {
@@ -33,16 +29,13 @@ impl Granularity {
 
     pub const fn of_log_bits(log_bits: usize) -> Self {
         Self {
-            log_bytes: log_bits >> BITS_IN_BYTE,
+            log_bytes: log_bits >> LOG_BITS_IN_BYTE,
         }
     }
 
-    pub fn of_type<T: Num>() -> Self {
+    pub const fn of_type<T: Num>() -> Self {
         let bytes = std::mem::size_of::<T>();
         let log_bytes = bytes.ilog2() as usize;
-
-        debug_assert_eq!(1 << log_bytes, bytes, "Not power-of-two size: {bytes}");
-
         Self::of_log_bytes(log_bytes)
     }
 
@@ -51,7 +44,7 @@ impl Granularity {
     }
 
     pub const fn log_bits(&self) -> usize {
-        self.log_bytes() << BITS_IN_BYTE
+        self.log_bytes() + LOG_BITS_IN_BYTE as usize
     }
 
     pub const fn bytes(&self) -> usize {
@@ -63,13 +56,20 @@ impl Granularity {
     }
 }
 
+/// Bit address within a grain.
+#[derive(Debug, Clone, Copy)]
+pub struct BitAddress {
+    pub addr: Address,
+    pub bit: BitOffset,
+}
+
 impl BitAddress {
     pub fn is_grain_aligned(&self, granularity: Granularity) -> bool {
         self.bit == 0 && self.addr.is_aligned_to(granularity.bytes())
     }
 
     pub fn is_normalized(&self, granularity: Granularity) -> bool {
-        self.addr.is_aligned_to(granularity.bytes()) && self.bit < granularity.bytes()
+        self.addr.is_aligned_to(granularity.bytes()) && self.bit < granularity.bits()
     }
 
     pub fn normalize(&self, granularity: Granularity) -> Self {
@@ -86,7 +86,7 @@ impl BitAddress {
 
     pub fn align_up_to_grain(&self, granularity: Granularity) -> Address {
         debug_assert!(self.is_normalized(granularity));
-        if self.bit == 0 {
+        if self.bit > 0 {
             (self.addr + 1usize).align_up(granularity.bytes())
         } else {
             self.addr.align_up(granularity.bytes())
@@ -94,7 +94,20 @@ impl BitAddress {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+pub trait AddressToBitAddress {
+    fn with_bit_offset(&self, offset: BitOffset) -> BitAddress;
+}
+
+impl AddressToBitAddress for Address {
+    fn with_bit_offset(&self, offset: BitOffset) -> BitAddress {
+        BitAddress {
+            addr: *self,
+            bit: offset,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum VisitRange {
     WholeGrain {
         start: Address,
@@ -105,6 +118,25 @@ pub enum VisitRange {
         bit_start: BitOffset,
         bit_end: BitOffset,
     },
+}
+
+pub fn bit_mask<T: PrimInt>(granularity: Granularity, bit_start: usize, bit_end: usize) -> T {
+    debug_assert!(
+        bit_start < bit_end,
+        "bit_start ({bit_start}) must be less than bit_end ({bit_end})"
+    );
+    debug_assert!(
+        bit_end <= granularity.bits(),
+        "Bit offset too high.  Granularity is {} bits, bit_end is {}",
+        granularity.bits(),
+        bit_end
+    );
+
+    if bit_end == granularity.bits() {
+        !T::zero() << bit_start
+    } else {
+        (!T::zero() << bit_start) & !(!T::zero() << bit_end)
+    }
 }
 
 pub fn break_range(
@@ -121,13 +153,21 @@ pub fn break_range(
         "{end:?} is not normalized for {granularity:?}"
     );
 
+    warn!("break_range: {granularity:?} {start:?} {end:?}");
+
     if start.addr == end.addr {
-        // The start and the end are in the same grain.  Yield only one SubGrain range.
-        return vec![VisitRange::SubGrain {
-            addr: start.addr,
-            bit_start: start.bit,
-            bit_end: end.bit,
-        }];
+        // The start and the end are in the same grain.
+        if start.bit == end.bit {
+            // Empty.
+            return vec![];
+        } else {
+            // Yield only one SubGrain range.
+            return vec![VisitRange::SubGrain {
+                addr: start.addr,
+                bit_start: start.bit,
+                bit_end: end.bit,
+            }];
+        }
     }
 
     // Start with a sub-grain range if the start is not aligned.
@@ -156,4 +196,214 @@ pub fn break_range(
         .into_iter()
         .flatten()
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn mk_addr(addr: usize) -> Address {
+        unsafe { Address::from_usize(addr) }
+    }
+
+    fn mk_ba(addr: usize, bit: usize) -> BitAddress {
+        BitAddress {
+            addr: mk_addr(addr),
+            bit,
+        }
+    }
+
+    const G64BIT: Granularity = Granularity::of_log_bytes(3);
+
+    #[test]
+    fn test_empty_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        for bit in 0..g.bits() {
+            let ba = mk_ba(base, bit);
+            let result = break_range(g, ba, ba);
+            assert!(
+                result.is_empty(),
+                "Not empty. bit: {bit}, result: {result:?}"
+            );
+        }
+    }
+
+    #[test]
+    fn test_subgrain_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        for bit0 in 0..g.bits() {
+            let ba0 = mk_ba(base, bit0);
+            for bit1 in (bit0 + 1)..g.bits() {
+                let ba1 = mk_ba(base, bit1);
+                let result = break_range(g, ba0, ba1);
+                assert_eq!(
+                    result,
+                    vec![VisitRange::SubGrain {
+                        addr: mk_addr(base),
+                        bit_start: bit0,
+                        bit_end: bit1
+                    }],
+                    "Not equal.  bit0: {bit0}, bit1: {bit1}",
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_end_grain_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        let ba1 = mk_ba(base + g.bytes(), 0);
+        for bit0 in 1..g.bits() {
+            let ba0 = mk_ba(base, bit0);
+            let result = break_range(g, ba0, ba1);
+            assert_eq!(
+                result,
+                vec![VisitRange::SubGrain {
+                    addr: mk_addr(base),
+                    bit_start: bit0,
+                    bit_end: g.bits()
+                }],
+                "Not equal.  bit0: {bit0}",
+            );
+        }
+    }
+
+    #[test]
+    fn test_adjacent_grain_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        for bit0 in 1..g.bits() {
+            let ba0 = mk_ba(base, bit0);
+            for bit1 in 1..g.bits() {
+                let ba1 = mk_ba(base + g.bytes(), bit1);
+                let result = break_range(g, ba0, ba1);
+                assert_eq!(
+                    result,
+                    vec![
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base),
+                            bit_start: bit0,
+                            bit_end: g.bits(),
+                        },
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base + g.bytes()),
+                            bit_start: 0,
+                            bit_end: bit1,
+                        },
+                    ],
+                    "Not equal.  bit0: {bit0}, bit1: {bit1}",
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_left_and_whole_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        for bit0 in 1..g.bits() {
+            let ba0 = mk_ba(base, bit0);
+            for word1 in 2..8 {
+                let ba1 = mk_ba(base + word1 * g.bytes(), 0);
+                let result = break_range(g, ba0, ba1);
+                assert_eq!(
+                    result,
+                    vec![
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base),
+                            bit_start: bit0,
+                            bit_end: g.bits(),
+                        },
+                        VisitRange::WholeGrain {
+                            start: mk_addr(base + g.bytes()),
+                            end: mk_addr(base + word1 * g.bytes()),
+                        },
+                    ],
+                    "Not equal.  bit0: {bit0}, word1: {word1}",
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_whole_and_right_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        for word0 in 1..8 {
+            let ba0 = mk_ba(base - word0 * g.bytes(), 0);
+            for bit1 in 1..g.bits() {
+                let ba1 = mk_ba(base, bit1);
+                let result = break_range(g, ba0, ba1);
+                assert_eq!(
+                    result,
+                    vec![
+                        VisitRange::WholeGrain {
+                            start: mk_addr(base - word0 * g.bytes()),
+                            end: mk_addr(base),
+                        },
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base),
+                            bit_start: 0,
+                            bit_end: bit1,
+                        },
+                    ],
+                    "Not equal.  word0: {word0}, bit1: {bit1}",
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_whole_range() {
+        let g = G64BIT;
+        let base = 0x1000;
+        let ba0 = mk_ba(base, 0);
+        let ba1 = mk_ba(base + 42 * g.bytes(), 0);
+        let result = break_range(g, ba0, ba1);
+        assert_eq!(
+            result,
+            vec![VisitRange::WholeGrain {
+                start: mk_addr(base),
+                end: mk_addr(base + 42 * g.bytes()),
+            },],
+        );
+    }
+
+    #[test]
+    fn test_left_whole_right_range() {
+        let g = G64BIT;
+        let base0 = 0x1000;
+        let base1 = 0x2000;
+
+        for bit0 in 1..g.bits() {
+            let ba0 = mk_ba(base0 - g.bytes(), bit0);
+            for bit1 in 1..g.bits() {
+                let ba1 = mk_ba(base1, bit1);
+                let result = break_range(g, ba0, ba1);
+                assert_eq!(
+                    result,
+                    vec![
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base0 - g.bytes()),
+                            bit_start: bit0,
+                            bit_end: g.bits(),
+                        },
+                        VisitRange::WholeGrain {
+                            start: mk_addr(base0),
+                            end: mk_addr(base1),
+                        },
+                        VisitRange::SubGrain {
+                            addr: mk_addr(base1),
+                            bit_start: 0,
+                            bit_end: bit1,
+                        },
+                    ],
+                    "Not equal.  bit0: {bit0}, bit1: {bit1}",
+                );
+            }
+        }
+    }
 }
