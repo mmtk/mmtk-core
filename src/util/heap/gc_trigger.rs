b800ae294ccdca2634260cbdab3be965288d25a6
Kunshan Wang <wks1986@gmail.com>
2024-07-25 10:01:17 +0800

    Minor change

--- src/util/heap/gc_trigger.rs
+++ src/util/heap/gc_trigger.rs
@@ -76,21 +76,20 @@ impl<VM: VMBinding> GCTrigger<VM> {
             .policy
             .is_gc_required(space_full, space.map(|s| SpaceStats::new(s)), plan)
         {
+            info!(
+                "[POLL] {}{} ({}/{} pages)",
+                if let Some(space) = space {
+                    format!("{}: ", space.get_name())
+                } else {
+                    "".to_string()
+                },
+                "Triggering collection",
+                plan.get_reserved_pages(),
+                plan.get_total_pages(),
+            );
+
             if !plan.constraints().collects_garbage {
-                unreachable!("User ran out of space and the plan does not support collecting garbage.");
-            }
-            else {
-                info!(
-                    "[POLL] {}{} ({}/{} pages)",
-                    if let Some(space) = space {
-                        format!("{}: ", space.get_name())
-                    } else {
-                        "".to_string()
-                    },
-                    "Triggering collection",
-                    plan.get_reserved_pages(),
-                    plan.get_total_pages(),
-                );
+                panic!("User ran out of space and the plan does not support collecting garbage.");
             }
 
             self.gc_requester.request();
