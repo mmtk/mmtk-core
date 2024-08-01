b4ed92905fb1218692170374cb6ee5b23b239a5e
Kunshan Wang <wks1986@gmail.com>
2024-08-01 13:08:56 +0800

    Extra assertions for mutators and epilogues
    This commit adds assertions about the number of mutators and the list of
    mutators returned from the VM binding.
    
    Also asserts that epilogues are not silently dropped before they are
    executed.  It could happen if a buggy VM binding returns a number of
    mutators that does not match the actual list of mutators.
    
    This should detect bugs like https://github.com/mmtk/mmtk-ruby/issues/84

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -44,6 +44,7 @@ pub mod test_util;
 /// An analysis framework for collecting data and profiling in GC.
 #[cfg(feature = "analysis")]
 pub(crate) mod analysis;
+pub(crate) mod epilogue;
 /// Non-generic refs to generic types of `<VM>`.
 pub(crate) mod erase_vm;
 /// Finalization implementation.
