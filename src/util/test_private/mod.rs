6a9320b996a096506b8fdb19f73d7b67b23430b6
Kunshan Wang <wks1986@gmail.com>
2024-08-06 17:26:37 +0800

    Inline test_private functions

--- src/util/test_private/mod.rs
+++ src/util/test_private/mod.rs
@@ -1,11 +1,19 @@
-//! This module exposes private items in mmtk-core for testing and benchmarking.
-//! They must not be used in production.
+//! This module exposes private items in mmtk-core for testing and benchmarking. They must not be
+//! used in production.
+//!
+//! # Notes on inlining
+//!
+//! In mmtk-core, we refrain from inserting inlining hints manually.  But we use `#[inline(always)]`
+//! in this module explicitly because the functions here are simple wrappers of private functions,
+//! and the compiler usually fails to make the right decision given that those functions are not
+//! used often, and we don't compile the benchmarks using feedback-directed optimizations.
 
 use crate::util::metadata::side_metadata::SideMetadataSpec;
 
 use super::Address;
 
 /// Expose `zero_meta_bits` when running `cargo bench`.
+#[inline(always)]
 pub fn zero_meta_bits(
     meta_start_addr: Address,
     meta_start_bit: u8,
@@ -16,6 +24,7 @@ pub fn zero_meta_bits(
 }
 
 /// Expose `set_meta_bits` when running `cargo bench`.
+#[inline(always)]
 pub fn set_meta_bits(
     meta_start_addr: Address,
     meta_start_bit: u8,
