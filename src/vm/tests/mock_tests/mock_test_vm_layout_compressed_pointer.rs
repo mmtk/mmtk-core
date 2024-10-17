d87613fd70885b9827aabb645758d493e14b304e
Yi Lin <qinsoon@gmail.com>
2024-10-17 15:40:44 +1300

    Figure out what address range is available for testing

--- src/vm/tests/mock_tests/mock_test_vm_layout_compressed_pointer.rs
+++ src/vm/tests/mock_tests/mock_test_vm_layout_compressed_pointer.rs
@@ -4,6 +4,7 @@ use super::mock_test_prelude::*;
 use super::mock_test_vm_layout_default::test_with_vm_layout;
 use crate::util::conversions::*;
 use crate::util::heap::vm_layout::VMLayout;
+use crate::util::heap::vm_layout::BYTES_IN_CHUNK;
 use crate::util::Address;
 
 // This test only run on 64bits.
@@ -13,21 +14,18 @@ fn test_vm_layout_compressed_pointer() {
     with_mockvm(
         default_setup,
         || {
-            let start = if cfg!(target_os = "macos") {
-                // Impossible to map 0x4000_0000 on maocOS. SO choose a different address.
-                0x60_0000_0000
-            } else {
-                0x4000_0000
-            };
             let heap_size = 1024 * 1024;
-            let end = match start + heap_size {
+            let start = crate::util::memory::find_usable_address(heap_size, BYTES_IN_CHUNK)
+                .expect("Cannot find usable address range");
+            println!("Use {} as heap start", start);
+            let end = match start.as_usize() + heap_size {
                 end if end <= (4usize << 30) => 4usize << 30,
                 end if end <= (32usize << 30) => 32usize << 30,
-                _ => start + (32usize << 30),
+                _ => start.as_usize() + (32usize << 30),
             };
             let layout = VMLayout {
                 log_address_space: 35,
-                heap_start: chunk_align_down(unsafe { Address::from_usize(start) }),
+                heap_start: start,
                 heap_end: chunk_align_up(unsafe { Address::from_usize(end) }),
                 log_space_extent: 31,
                 force_use_contiguous_spaces: false,
