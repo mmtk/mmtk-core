86ce27081379169d7eddeca6eaa4e8f1c23dcbea
Kunshan Wang <wks1986@gmail.com>
2024-11-05 10:16:29 +0800

    Make env_logger an optional dependency
    Now the built-in env_logger is guarded behind a Cargo feature
    "builtin_env_logger".  It is a default feature, but can be disabled
    in Cargo.toml by setting `dependencies.mmtk.default-features = false`.
    In this way, VM bindings that want to implement its own logger can
    remove the env_logger crate from its dependencies.

--- src/util/logger.rs
+++ src/util/logger.rs
@@ -1,9 +1,16 @@
-use log::{self, SetLoggerError};
+use log::SetLoggerError;
 
 /// Attempt to init a env_logger for MMTk.
+/// Does nothing if the "builtin_env_logger" feature is disabled.
 pub fn try_init() -> Result<(), SetLoggerError> {
-    env_logger::try_init_from_env(
-        // By default, use info level logging.
-        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
-    )
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "builtin_env_logger")] {
+            env_logger::try_init_from_env(
+                // By default, use info level logging.
+                env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+            )
+        } else {
+            Ok(())
+        }
+    }
 }
