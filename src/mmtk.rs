6f85255ec3cd05f1b4226cd6ff167e30f44c46b1
Yi Lin <qinsoon@gmail.com>
2025-02-04 03:29:00 +0000

    Add a no_mangle function that prints object info from the space

--- src/mmtk.rs
+++ src/mmtk.rs
@@ -597,3 +597,8 @@ impl<VM: VMBinding> MMTK<VM> {
             .initialize_object_metadata(object, false)
     }
 }
+
+#[no_mangle]
+pub fn mmtk_debug_print_object_info(object: crate::util::ObjectReference) {
+    println!("{}", SFT_MAP.get_checked(object.to_raw_address()).debug_get_object_info(object))
+}
