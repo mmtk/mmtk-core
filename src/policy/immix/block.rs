f603c6be305119b1a373e12260cb192b21bdfa87
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-04-10 03:15:23 +0000

    Remove unused import

--- src/policy/immix/block.rs
+++ src/policy/immix/block.rs
@@ -1,7 +1,6 @@
 use super::defrag::Histogram;
 use super::line::Line;
 use super::ImmixSpace;
-use crate::policy::space::Space;
 use crate::util::constants::*;
 use crate::util::heap::blockpageresource::BlockPool;
 use crate::util::heap::chunk_map::Chunk;
