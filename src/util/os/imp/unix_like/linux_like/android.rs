d9b40daff7ab9bf25334e11c866814ddbee837f5
Yi Lin <qinsoon@gmail.com>
2026-02-13 17:18:37 +1300

    OS interface (#1439)
    This PR addresses https://github.com/mmtk/mmtk-core/issues/1420.
    
    This PR is based on top of https://github.com/mmtk/mmtk-core/pull/1418,
    and includes all the changes for Windows (for testing purpose). It is
    likely that Windows support will be removed from this PR, and will be
    merged separately.
    
    This PR does not try to refactor our malloc interface -- I am not sure
    if malloc should be included in the OS interface or not.
    
    This PR consolidates the current multiple mmap functions (such as
    dzmmap, mmap_fixed, mmap_noreplace, mmap_noreserve, etc), and use
    `MmapStrategy` to specify the expected mmap behavior.
    
    ---------
    
    Co-authored-by: sepcnt <30561671+sepcnt@users.noreply.github.com>

---
+++ src/util/os/imp/unix_like/linux_like/android.rs
@@ -0,0 +1,65 @@
+use crate::util::address::Address;
+use crate::util::os::imp::unix_like::linux_like::linux_common;
+use crate::util::os::imp::unix_like::unix_common;
+use crate::util::os::*;
+
+use std::io::Result;
+
+/// Android implementation of the `OS` trait.
+pub struct Android;
+
+impl OSMemory for Android {
+    fn dzmmap(
+        start: Address,
+        size: usize,
+        strategy: MmapStrategy,
+        annotation: &MmapAnnotation<'_>,
+    ) -> Result<Address> {
+        linux_common::dzmmap(start, size, strategy, annotation)
+    }
+
+    fn munmap(start: Address, size: usize) -> Result<()> {
+        unix_common::munmap(start, size)
+    }
+
+    fn set_memory_access(start: Address, size: usize, prot: MmapProtection) -> Result<()> {
+        unix_common::mprotect(start, size, prot)
+    }
+
+    fn is_mmap_oom(os_errno: i32) -> bool {
+        unix_common::is_mmap_oom(os_errno)
+    }
+
+    fn panic_if_unmapped(start: Address, size: usize) {
+        linux_common::panic_if_unmapped(start, size)
+    }
+}
+
+impl OSProcess for Android {
+    type ProcessIDType = unix_common::ProcessIDType;
+    type ThreadIDType = unix_common::ThreadIDType;
+
+    fn get_process_memory_maps() -> Result<String> {
+        linux_common::get_process_memory_maps()
+    }
+
+    fn get_process_id() -> Result<Self::ProcessIDType> {
+        unix_common::get_process_id()
+    }
+
+    fn get_thread_id() -> Result<Self::ThreadIDType> {
+        unix_common::get_thread_id()
+    }
+
+    fn get_total_num_cpus() -> CoreNum {
+        linux_common::get_total_num_cpus()
+    }
+
+    fn bind_current_thread_to_core(core_id: CoreId) {
+        linux_common::bind_current_thread_to_core(core_id)
+    }
+
+    fn bind_current_thread_to_cpuset(core_ids: &[CoreId]) {
+        linux_common::bind_current_thread_to_cpuset(core_ids)
+    }
+}
