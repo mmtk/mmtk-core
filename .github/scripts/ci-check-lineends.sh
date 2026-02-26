a9d0f3519bba171bbc6e88a01fd7d6faeb23711e
Kunshan Wang <wks1986@gmail.com>
2026-02-26 16:50:45 +0800

    Comments

--- .github/scripts/ci-check-lineends.sh
+++ .github/scripts/ci-check-lineends.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# This scripts is executed during CI style tests to check line ends.
-# In this project, text files use UNIX line ends, and must have a newline character at the end of the file.
+# This is a driver script for check-lineends.py
+# It finds text files in the project tree and checks/fixes its line ends.
 #
+# The CI runs this script during style checking.
+#
+# Developers may also run this script directly.
+# It forwards all command line options to check-lineends.py.
+# This means if you add the '-f' option,
+# it will automatically fix the line ends of all files we concern.
+#
+#   ./.github/scripts/ci-check-lineends.sh -f
+#
+# In this project, text files use UNIX line ends, and must have a newline character at the end of the file.
 # Note that not having a newline character at the end of a file may have unexpected consequences.
 # For example, when concatenating multiple files,
 # the last line of a file will be joined with the first line of the next file.
 # The same may happen when including files using `#include` or `include!` directives in C or Rust.
-#
-# You may also run this script directly to check line ends.
-# It can also automatically fix the line ends for you if you add the -f option:
-#
-#   ./.github/scripts/ci-check-lineends.sh -f
-#
-# Other options will be forwarded to the check-lineends.py script, too.
 
 BAD_LINE_ENDS=0
 
+# TODO: When we introduce the '.gitattributes' file,
+# make sure the patterns here matches the patterns in '.gitattributes'.
+# Alternatively, find a way to automatically establish the list of files to check
+# from the contents of '.gitattributes'.
 FILES=$(find . -name 'target' -prune -o -type f -a '(' \
     -name '.gitignore' \
     -o -name '*.rs' \
