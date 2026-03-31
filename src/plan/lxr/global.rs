cb1ae9d18e7585640a46b9e78adf44334e00d0e3
Wenyu Zhao <wenyuzhaox@gmail.com>
2026-03-31 23:37:42 +1100

    optimiza

--- src/plan/lxr/global.rs
+++ src/plan/lxr/global.rs
@@ -886,9 +886,7 @@ impl<VM: VMBinding> LXR<VM> {
         scheduler.work_buckets[WorkBucketStage::RefForwarding].set_enabled(false);
         scheduler.work_buckets[WorkBucketStage::FinalizableForwarding].set_enabled(false);
         scheduler.work_buckets[WorkBucketStage::Compact].set_enabled(false);
-        if cfg!(feature = "jdk11") {
-            scheduler.work_buckets[WorkBucketStage::FixRelocations].set_enabled(false);
-        }
+        scheduler.work_buckets[WorkBucketStage::FixRelocations].set_enabled(false);
         if crate::args::LAZY_DECREMENTS
             && pause != Pause::Full
             && !cfg!(feature = "fragmentation_analysis")
