e8442a4d7a34323b6d52020da4efba8e74d126b2
Kunshan Wang <wks1986@gmail.com>
2026-09-04 17:12:07 +0800

    Remove undefined trace points

--- tools/tracing/timeline/visualize.py
+++ tools/tracing/timeline/visualize.py
@@ -245,15 +245,6 @@ class LogProcessor:
                         "pause": pause,
                     }
 
-                case "defrag_prepare":
-                    gc["args"] |= {
-                        "defrag_prepare": {
-                            "defrag_spill_threshold": int(args[0]),
-                            "available_clean_pages_for_defrag": int(args[1]),
-                            "new_available_clean_pages_for_defrag": int(args[2]),
-                        },
-                    }
-
                 case _:
                     processed_for_gc = False
         else:
